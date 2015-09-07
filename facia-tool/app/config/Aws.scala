387cd00ec9cc9a12cb89687b4405ac6874b557f2
lindseydew <lindsey.dew@guardian.co.uk>
2015-09-07 16:14:42 +0100

    send to sns feed

---
+++ facia-tool/app/config/Aws.scala
@@ -0,0 +1,33 @@
+package config
+
+import com.amazonaws.AmazonClientException
+import com.amazonaws.auth._
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import common.BadConfigurationException
+import play.api.{Logger, Play}
+import play.api.Play.current
+
+object aws {
+  def mandatoryCredentials: AWSCredentialsProvider = credentials.getOrElse(throw new BadConfigurationException("AWS credentials are not configured"))
+  val credentials: Option[AWSCredentialsProvider] = {
+    val provider = new AWSCredentialsProviderChain(
+      new ProfileCredentialsProvider("CMS Fronts")
+    )
+
+    // this is a bit of a convoluted way to check whether we actually have credentials.
+    // I guess in an ideal world there would be some sort of isConfigued() method...
+    try {
+      provider.getCredentials
+      Some(provider)
+    } catch {
+      case ex: AmazonClientException =>
+        Logger.error("amazon client exception")
+
+        // We really, really want to ensure that PROD is configured before saying a box is OK
+        if (Play.isProd) throw ex
+        // this means that on dev machines you only need to configure keys if you are actually going to use them
+        None
+    }
+  }
+
+}
