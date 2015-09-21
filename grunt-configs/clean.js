812b3f1b1b38165b6f47501a804d3f7ad3b6f29f
Fabio Crisci <fabio.crisci@guardian.co.uk>
2015-09-21 12:38:04 +0100

    Remove frontend steps from build

--- grunt-configs/clean.js
+++ grunt-configs/clean.js
@@ -1,32 +1,5 @@
 module.exports = function(grunt, options) {
     return {
-        js: [
-            options.staticTargetDir + 'javascripts',
-            options.staticHashDir + 'javascripts',
-            options.staticHashDir + 'bundles',
-            options.requirejsDir
-        ],
-        css: [
-            options.staticTargetDir + 'stylesheets',
-            options.staticHashDir + 'stylesheets'
-        ],
-        images: [
-            options.staticTargetDir + 'images',
-            options.staticHashDir + 'images'
-        ],
-        flash: [
-            options.staticTargetDir + 'flash',
-            options.staticHashDir + 'flash'
-        ],
-        fonts: [
-            options.staticTargetDir + 'fonts',
-            options.staticHashDir + 'fonts'
-        ],
-        assets: [
-            'common/conf/assets',
-            options.staticHashDir + 'assets'
-        ],
-        // Clean any pre-commit hooks in .git/hooks directory
         hooks: ['.git/hooks']
     };
 };
--- grunt-configs/shell.js
+++ grunt-configs/shell.js
@@ -1,13 +1,6 @@
+'use strict';
 module.exports = function(grunt, options) {
     return {
-        spriteGeneration: {
-            command: 'find . -name \'*.json\' -exec node spricon.js {} \\;',
-            options: {
-                execOptions: {
-                    cwd: 'tools/sprites'
-                }
-            }
-        },
         /**
          * Using this task to copy hooks, as Grunt's own copy task doesn't preserve permissions
          */
@@ -17,19 +10,8 @@ module.exports = function(grunt, options) {
                 failOnError: false
             }
         },
-
-        abTestInfo: {
-            command: 'node tools/ab-test-info/ab-test-info.js ' +
-                     'static/src/javascripts/projects/common/modules/experiments/tests ' +
-                     'static/abtests.json'
-        },
-
-        npmInstallFaciaTool: {
-            command: 'cd facia-tool/public && npm prune && npm install'
-        },
-
-        jspmInstallFaciaTool: {
-            command: './node_modules/.bin/jspm install',
+        npmInstall: {
+            command: 'npm prune && npm install',
             options: {
                 execOptions: {
                     cwd: 'facia-tool/public'
@@ -37,31 +19,8 @@ module.exports = function(grunt, options) {
             }
         },
 
-        npmInstall: {
-            command: 'npm prune && npm install'
-        },
-
-        jspmInstallStatic: {
-            command: './jspm install && ./jspm dl-loader && ./jspm clean',
-            options: {
-                execOptions: {
-                    cwd: './node_modules/.bin'
-                }
-            }
-        },
-
-        jspmBundleStatic: {
-            command:
-                'node ./bundle',
-            options: {
-                execOptions: {
-                    cwd: '.'
-                }
-            }
-        },
-
-        updateCanIUse: {
-          command: 'npm update caniuse-db'
+        jspmInstall: {
+            command: './node_modules/.bin/jspm install'
         }
     };
 };
