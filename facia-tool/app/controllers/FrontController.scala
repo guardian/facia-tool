5d58cf7e794227ed9af42aa25b56e646d9fabbbd
Fabio Crisci <fabio.crisci@guardian.co.uk>
2015-09-28 19:14:40 +0100

    unauthorized page

--- facia-tool/app/controllers/FrontController.scala
+++ facia-tool/app/controllers/FrontController.scala
@@ -77,7 +77,7 @@ object PermissionCheckAction extends ActionFilter[UserRequest] {
       None
     }
     else {
-      Some(Results.Forbidden("forbidden"))
+      Some(Results.Unauthorized(views.html.unauthorized()))
     })
   }
 }
