7e501e7f69bbd6559164d2cb831355cefcbbda4d
Fabio Crisci <fabio.crisci@guardian.co.uk>
2015-09-30 10:31:26 +0100

    Better cache clean

--- upload-riffraff.sh
+++ upload-riffraff.sh
@@ -8,7 +8,4 @@ set -o pipefail
 if [ "${JS_STEPS}" = "false" ]
 then
     sbt ++$TRAVIS_SCALA_VERSION -jvm-opts travis/jvmopts "project facia-tool" riffRaffUpload
-    # Tricks to avoid unnecessary cache updates
-    find $HOME/.sbt -name "*.lock" | xargs rm
-    find $HOME/.ivy2 -name "ivydata-*.properties" | xargs rm
 fi
