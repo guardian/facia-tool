b5667d7e1fef57206d7f747ed7ae5fd4905568a8
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2015-10-30 11:35:18 +0000

    send switches to client

---
+++ switchboard/switches.scala
@@ -0,0 +1,25 @@
+package switches
+
+import play.api.Logger
+import play.api.libs.json.{Json, JsValue}
+
+object switchManager {
+
+  var switches: Map[String, Boolean] = Map()
+
+  def updateSwitches(newSwitches: Map[String, Boolean] ): Unit = {
+    switches = newSwitches
+  }
+
+  def getStatus(switchName:String): Boolean = {
+    switches.get(switchName) match {
+        case Some(value) => value
+        case None => {
+          Logger.warn(s"No status found matching switch ${switchName}")
+          false
+        }
+    }
+  }
+
+  def getSwitchesAsJson(): JsValue = Json.toJson(switches)
+}
