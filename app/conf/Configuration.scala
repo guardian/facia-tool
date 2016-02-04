b1b98aee631206f0c02090933a1861d924c894cc
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-02-04 11:50:29 +0000

    add a new bucket

--- app/conf/Configuration.scala
+++ app/conf/Configuration.scala
@@ -82,6 +82,7 @@ object Configuration {
   object aws {
     lazy val region = getMandatoryString("aws.region")
     lazy val bucket = getMandatoryString("aws.bucket")
+    lazy val faciaBucket = getMandatoryString("aws.faciaBucket")
   }
 
   object facia {
