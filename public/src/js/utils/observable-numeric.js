c7b47d68a28f3b4d56a1ebb502136d6d1598c090
David Furey <david.furey@guardian.co.uk>
2017-02-09 17:16:28 +0000

    ES6 niceness

--- public/src/js/utils/observable-numeric.js
+++ public/src/js/utils/observable-numeric.js
@@ -3,12 +3,10 @@ import ko from 'knockout';
 export default function(initialValue) {
     var actual = ko.observable(initialValue);
     return ko.dependentObservable({
-        read: function ()
-        {
+        read() {
             return actual();
         },
-        write: function (newValue)
-        {
+        write(newValue) {
             var parsedValue = parseFloat(newValue);
             actual(isNaN(parsedValue) ? newValue : parsedValue);
         }
