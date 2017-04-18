0eef16324578da498a02d7d926a3dfd2decf29d8
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2017-04-18 14:16:33 +0100

    Use multiline strings

--- app/auth/PanDomainAuthActions.scala
+++ app/auth/PanDomainAuthActions.scala
@@ -33,13 +33,14 @@ trait PanDomainAuthActions extends AuthActions with PanDomainAuth with Results {
 
   override def invalidUserMessage(claimedAuth: AuthenticatedUser): String = {
     if( (claimedAuth.user.emailDomain == "guardian.co.uk") && !claimedAuth.multiFactor) {
-      s"${claimedAuth.user.email} is not valid for use with the Fronts Tool as you need to have two factor authentication enabled." +
-      s" Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request access to Composer CMS tools."
+      s"""${claimedAuth.user.email} is not valid for use with the Fronts Tool as you need to have two factor authentication enabled.
+      Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request access to Composer CMS tools."""
     } else if (!userInGroups(claimedAuth)) {
-      s"${claimedAuth.user.email} is not valid for use with the Fronts Tool as you need to be granted access to the editorial tools." +
-      s" Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request access to Composer CMS tools."
+      s"""${claimedAuth.user.email} is not valid for use with the Fronts Tool as you need to be granted access to the editorial tools.
+      Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request access to Composer CMS tools."""
     } else {
-      s"${claimedAuth.user.email} is not valid for use with the Fronts Tool. You need to use your Guardian Google account to login. Please sign in with your Guardian Google account first, then retry logging in."
+      s"""${claimedAuth.user.email} is not valid for use with the Fronts Tool. You need to use your Guardian Google account to login.
+      Please sign in with your Guardian Google account first, then retry logging in."""
     }
   }
 
