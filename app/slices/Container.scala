110492a7bedcc6cd08059a2127a4f2d9d03c1928
Diego Vazquez Nanini <diego@vazqueznanini.com>
2017-05-16 18:20:26 +0100

    Rename and defaults.js

--- app/slices/Container.scala
+++ app/slices/Container.scala
@@ -9,7 +9,7 @@ class Containers(val fixedContainers: FixedContainers) {
     ("dynamic/fast", Dynamic(DynamicFast)),
     ("dynamic/slow", Dynamic(DynamicSlow)),
     ("dynamic/package", Dynamic(DynamicPackage)),
-    ("dynamo/election", Dynamic(DynamicElection)),
+    ("dynamic/election", Dynamic(DynamicElection)),
     ("dynamic/slow-mpu", Dynamic(DynamicSlowMPU)),
     ("nav/list", NavList),
     ("nav/media-list", NavMediaList),
