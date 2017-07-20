e786418788bc39723e7e1e143ebdf72534a21f4a
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-07-20 11:52:40 +0100

    Add the imageSrcThumb to thumbnail used in article meta

--- public/src/js/constants/article-meta-fields.js
+++ public/src/js/constants/article-meta-fields.js
@@ -53,6 +53,7 @@ export default Object.freeze([
                 width: 'imageSrcWidth',
                 height: 'imageSrcHeight',
                 origin: 'imageSrcOrigin',
+                thumb: 'imageSrcThumb',
                 options: {
                     minWidth: 400,
                     widthAspectRatio: 5,
@@ -63,7 +64,9 @@ export default Object.freeze([
         type: 'text'
     },
     {
-        key: 'imageSrcThumb'
+        key: 'imageSrcThumb',
+        label: 'image thumbnail',
+        type: 'text'
     },
     {
         key: 'imageSrcWidth',
