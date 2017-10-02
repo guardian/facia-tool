b342c6775bd023b4901a5fe9cb41bb8d6a7a2471
Richard Beddington <richard_beddington@33907.gnm.int>
2017-10-02 17:09:48 +0100

    Add comments re: keepQueryStringParam

--- public/src/js/utils/url-abs-path.js
+++ public/src/js/utils/url-abs-path.js
@@ -1,5 +1,12 @@
 var a = document.createElement('a');
 
+/*
+ * @param {string} url
+ * @param {bool} keepQueryString
+ * @description gets the absolute path, keepQueryString was added where campaign
+ * params where being removed. Because this helper is many other places keeping
+ * queryStrings is opt in
+ */
 export default function(url, keepQueryString) {
     if (typeof url === 'string') {
         // If necessary, add a leading slash to stop the browser resolving it against the current path
