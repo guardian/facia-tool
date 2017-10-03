f12996a994e4f007c889b4e65110a299b6a10c70
Richard Beddington <richard.beddington@gmail.com>
2017-10-03 14:29:16 +0100

    Fix missing query string on URLs pasted into clipboard (#256)
    * Fix missing query string on pasted URLs
    
    * Add comments re: keepQueryStringParam

--- public/src/js/utils/url-abs-path.js
+++ public/src/js/utils/url-abs-path.js
@@ -1,13 +1,22 @@
 var a = document.createElement('a');
 
-export default function(url) {
+/*
+ * @param {string} url
+ * @param {bool} keepQueryString
+ * @description gets the absolute path, keepQueryString was added where campaign
+ * params where being removed. Because this helper is many other places keeping
+ * queryStrings is opt in
+ */
+export default function(url, keepQueryString) {
     if (typeof url === 'string') {
         // If necessary, add a leading slash to stop the browser resolving it against the current path
         url = url.match(/^\//) || url.match(/^https?:\/\//) ? url : '/' + url;
 
         a.href = url;
 
+        var path = a.pathname.replace(/^\//, '');
+
         // Return the abspath without a leading slash, because ContentApi ids are formed like that
-        return a.pathname.replace(/^\//, '');
+        return keepQueryString ? path + a.search : path;
     }
 }
