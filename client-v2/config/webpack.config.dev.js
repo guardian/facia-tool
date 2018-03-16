6447aca001152937f926d8a1647f66abca39d961
RichieAHB <richard.beddington@guardian.co.uk>
2018-03-16 15:56:16 +0000

    Add a dev server

--- client-v2/config/webpack.config.dev.js
+++ client-v2/config/webpack.config.dev.js
@@ -1,6 +1,13 @@
+import path from 'path';
 import common from './webpack.config.common.js';
 
 export default {
   ...common,
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, '../'),
+    compress: true,
+    port: 9001,
+    publicPath: '/dist/'
+  }
 };
