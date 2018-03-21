85fa043888862b9404ae289bdccbdc092bfcbb6c
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-03-21 13:59:37 +0000

    add panda fetch

---
+++ client-v2/src/services/pandaFetch.js
@@ -0,0 +1,36 @@
+import { reEstablishSession } from 'panda-session';
+import getStore from '../util/storeAccessor';
+
+function checkStatus(res) {
+  if (res.status >= 200 && res.status < 300) {
+    return res;
+  }
+  throw res;
+}
+
+export default function pandaFetch(url, body) {
+  return new Promise((resolve, reject) =>
+    fetch(url, body)
+      .then(checkStatus)
+      .then(res => resolve(res))
+      .catch(err => {
+        if (err.status === 419) {
+          const store = getStore();
+          const { config: reauthUrl } = store.getState();
+
+          return reEstablishSession(reauthUrl, 5000).then(
+            () => {
+              fetch(url, body)
+                .then(checkStatus)
+                .then(res => resolve(res))
+                .catch(reAuthErr => reject(reAuthErr));
+            },
+            error => {
+              throw error;
+            }
+          );
+        }
+        return reject(err);
+      })
+  );
+}
