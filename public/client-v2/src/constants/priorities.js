38a2354c55ee0f85559fa8465f441330a54cb166
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-03-28 08:28:31 +0100

    add priorities constant

---
+++ public/client-v2/src/constants/priorities.js
@@ -0,0 +1,15 @@
+// @flow
+
+declare type Priorities = {
+  editorial: Object,
+  commercial: Object,
+  training: Object,
+  email: Object
+};
+
+export const priorities: Priorities = {
+  editorial: {},
+  commercial: {},
+  training: {},
+  email: {}
+};
