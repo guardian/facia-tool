d90a9424a54f2f4d566d626d7c03906e0281a321
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-04-04 15:42:47 +0100

    Fix errors in typing

--- public/client-v2/src/types/Fronts.js
+++ public/client-v2/src/types/Fronts.js
@@ -2,19 +2,19 @@
 
 type FrontConfig = {
   collections: Array<string>,
-  priority: ?string,
-  canonical: ?string,
-  group: ?string,
-  isHidden: ?boolean,
-  isImageDisplayed: ?boolean,
-  imageHeight: ?number,
-  imageWidth: ?number,
-  imageUrl: ?string,
-  onPageDescription: ?string,
-  description: ?string,
-  title: ?string,
-  webTitle: ?string,
-  navSection: ?string
+  priority?: string,
+  canonical?: string,
+  group?: string,
+  isHidden?: boolean,
+  isImageDisplayed?: boolean,
+  imageHeight?: number,
+  imageWidth?: number,
+  imageUrl?: string,
+  onPageDescription?: string,
+  description?: string,
+  title?: string,
+  webTitle?: string,
+  navSection?: string
 };
 
 type FrontDetail = FrontConfig & { id: string };
@@ -28,22 +28,22 @@ type Platform = 'Web' | 'Platform';
 type CollectionDetail = {
   displayName: string,
   type: string,
-  backfill: ?Object,
-  href: ?string,
-  groups: ?Array<string>,
-  metadata: ?Array<Object>,
-  platform: ?string,
-  uneditable: ?boolean,
-  showTags: ?boolean,
-  hideKickers: ?boolean,
-  excludedFromRss: ?boolean,
-  description: ?string,
-  showSections: ?boolean,
-  showDateHeader: ?boolean,
-  showLatestUpdate: ?boolean,
-  excludeFromRss: ?boolean,
-  hideShowMore: ?boolean,
-  platform: ?Platform
+  backfill?: Object,
+  href?: string,
+  groups?: Array<string>,
+  metadata?: Array<Object>,
+  platform?: string,
+  uneditable?: boolean,
+  showTags?: boolean,
+  hideKickers?: boolean,
+  excludedFromRss?: boolean,
+  description?: string,
+  showSections?: boolean,
+  showDateHeader?: boolean,
+  showLatestUpdate?: boolean,
+  excludeFromRss?: boolean,
+  hideShowMore?: boolean,
+  platform?: Platform
 };
 
 type Collection = {
@@ -52,7 +52,7 @@ type Collection = {
 
 type FrontsConfig = {
   fronts: Front,
-  collections: CollectionDetail
+  collections: Collection
 };
 
 type Priorities = {
