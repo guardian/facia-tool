abab075cff9729f15f54d0fc7b273fa57d2a73fc
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-04-04 11:46:22 +0100

    Add types for fronts, collections and priorities

---
+++ public/client-v2/src/types/Fronts.js
@@ -0,0 +1,73 @@
+// @flow
+
+type FrontConfig = {
+  collections: Array<string>,
+  priority: ?string,
+  canonical: ?string,
+  group: ?string,
+  isHidden: ?boolean,
+  isImageDisplayed: ?boolean,
+  imageHeight: ?number,
+  imageWidth: ?number,
+  imageUrl: ?string,
+  onPageDescription: ?string,
+  description: ?string,
+  title: ?string,
+  webTitle: ?string,
+  navSection: ?string
+};
+
+type FrontDetail = FrontConfig & { id: string };
+
+type Front = {
+  [string]: FrontConfig
+};
+
+type Platform = 'Web' | 'Platform';
+
+type CollectionDetail = {
+  displayName: string,
+  type: string,
+  backfill: ?Object,
+  href: ?string,
+  groups: ?Array<string>,
+  metadata: ?Array<Object>,
+  platform: ?string,
+  uneditable: ?boolean,
+  showTags: ?boolean,
+  hideKickers: ?boolean,
+  excludedFromRss: ?boolean,
+  description: ?string,
+  showSections: ?boolean,
+  showDateHeader: ?boolean,
+  showLatestUpdate: ?boolean,
+  excludeFromRss: ?boolean,
+  hideShowMore: ?boolean,
+  platform: ?Platform
+};
+
+type Collection = {
+  [string]: CollectionDetail
+};
+
+type FrontsConfig = {
+  fronts: Front,
+  collections: CollectionDetail
+};
+
+type Priorities = {
+  editorial: Object,
+  commercial: Object,
+  training: Object,
+  email: Object
+};
+
+export type {
+  FrontsConfig,
+  FrontConfig,
+  Front,
+  FrontDetail,
+  Collection,
+  CollectionDetail,
+  Priorities
+};
