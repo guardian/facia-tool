34be7be85fe31e85fdd9768b4ec4113bd1d3c897
Susie Coleman <susie.coleman@guardian.co.uk>
2018-04-10 10:50:56 +0100

    Update to how new json formatter library works.

--- app/updates/UpdateMessage.scala
+++ app/updates/UpdateMessage.scala
@@ -1,10 +1,10 @@
 package updates
 
 import com.gu.facia.client.models.{CollectionConfigJson, FrontJson, TrailMetaData}
-import julienrf.variants.Variants
 import org.joda.time.DateTime
 import play.api.libs.json._
 import services.ConfigAgent
+import julienrf.json.derived
 
 sealed trait UpdateMessage {
   def affectedFronts(configAgent: ConfigAgent): Set[String]
@@ -93,7 +93,7 @@ case class HandlingBreakingNewsTrail(id: String, breakingNewsTrail: ClientHydrat
 
 /* Macro - Watch out, this needs to be after the case classes */
 object UpdateMessage {
-  implicit val format: Format[UpdateMessage] = Variants.format[UpdateMessage]((__ \ "type").format[String])
+  implicit val format = derived.flat.oformat[UpdateMessage]((__ \ "type").format[String])
 }
 
 /* Kinesis messages */
