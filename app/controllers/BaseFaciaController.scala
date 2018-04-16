50765fadc38479da7241596fa9fa7865c3885e92
Susie Coleman <susie.coleman@guardian.co.uk>
2018-04-16 09:17:19 +0100

    Update contact details for Fronts access.

--- app/controllers/BaseFaciaController.scala
+++ app/controllers/BaseFaciaController.scala
@@ -58,8 +58,8 @@ abstract class BaseFaciaController(deps: BaseFaciaControllerComponents) extends 
   override def invalidUserMessage(claimedAuth: AuthenticatedUser): String = {
     if( (claimedAuth.user.emailDomain == "guardian.co.uk") && !claimedAuth.multiFactor)
       s"${claimedAuth.user.email} is not valid for use with the Fronts Tool as you need to have two factor authentication enabled." +
-        s" Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request access to Composer CMS tools."
-    else if (!userInGroups(claimedAuth)) s"${claimedAuth.user.email} is not a member of required google groups. Please contact the Helpdesk by emailing 34444@theguardian.com"
+        s" Please contact Central Production by emailing core.central.production@guardian.co.uk and request access to The Fronts Tool."
+    else if (!userInGroups(claimedAuth)) s"${claimedAuth.user.email} is not a member of required google groups. Please contact Central Production by emailing core.central.production@guardian.co.uk"
 
     else s"${claimedAuth.user.email} is not valid for use with the Fronts Tool. You need to use your Guardian Google account to login. Please sign in with your Guardian Google account first, then retry logging in."
 
