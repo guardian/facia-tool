8bb9d1cb5918395d7b309181a99c6c0936f8b4f4
RichieAHB <richard.beddington@guardian.co.uk>
2018-05-03 17:53:57 +0100

    lint fix

--- client-v2/src/util/paths.js
+++ client-v2/src/util/paths.js
@@ -1,7 +1,7 @@
 // @flow
 
 const getPaths = (uri: string) => {
-  const [_, path] = /https:\/\/www\.theguardian.com(.+)/.exec(uri);
+  const [, path] = /https:\/\/www\.theguardian.com(.+)/.exec(uri);
 
   return {
     ophan: `https://dashboard.ophan.co.uk/info?path=${path}`
