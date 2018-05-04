17eb837949669a1bde0a882640e07d508d47d036
Richard Beddington <richard.beddington@gmail.com>
2018-05-04 11:30:38 +0100

    Fix bad object creation (#312)

--- client-v2/src/services/pandaFetch.js
+++ client-v2/src/services/pandaFetch.js
@@ -5,20 +5,20 @@ import { reEstablishSession } from 'panda-session';
 const reauthUrl = '/auth';
 const pandaFetch = (
   url: string,
-  body: Object = {},
+  options: Object = {},
   count: number = 0
 ): Promise<Response> =>
   new Promise(
     async (resolve: (r: Response) => mixed, reject: (r: Response) => mixed) => {
       const res = await fetch(url, {
-        body,
+        ...options,
         credentials: 'same-origin'
       });
 
       if (res.status === 419 && count < 1) {
         await reEstablishSession(reauthUrl, 5000);
         try {
-          const res2 = await pandaFetch(url, body, count + 1);
+          const res2 = await pandaFetch(url, options, count + 1);
           return resolve(res2);
         } catch (e) {
           return reject(e);
