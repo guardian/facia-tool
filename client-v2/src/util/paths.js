d45ae7da9b2e5a8532e4763510ccca29d6ebdf21
RichieAHB <richard.beddington@guardian.co.uk>
2018-05-04 10:56:26 +0100

    Make path creator more resilient

--- client-v2/src/util/paths.js
+++ client-v2/src/util/paths.js
@@ -1,11 +1,23 @@
 // @flow
 
-const getPaths = (uri: string) => {
+const getPathFromUri = (uri: string): string | void => {
   const [, path] = /https:\/\/www\.theguardian.com(.+)/.exec(uri);
+  return path;
+};
 
-  return {
-    ophan: `https://dashboard.ophan.co.uk/info?path=${path}`
-  };
+const ophanURIFromPath = (path: string) =>
+  `https://dashboard.ophan.co.uk/info?path=${path}`;
+
+const getPaths = (uri: string) => {
+  const path = getPathFromUri(uri);
+
+  return path
+    ? {
+        ophan: ophanURIFromPath(path)
+      }
+    : {
+        ophan: null
+      };
 };
 
 export { getPaths };
