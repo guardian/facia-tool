4caf306cf7ba585c51186f592535c97b9ef166ad
Jonathon Herbert <jonathon_herbert@33568.gnm.int>
2018-05-14 09:24:37 +0100

    Added selectors for deriving Articles from ArticleFragments and ExternalArticles, getting articles in groups

--- client-v2/src/selectors/__tests__/shared.spec.js
+++ client-v2/src/selectors/__tests__/shared.spec.js
@@ -1,6 +1,24 @@
-import { getArticleFromArticleFragment } from '../shared';
+import {
+  createArticleFromArticleFragmentSelector,
+  externalArticleFromArticleFragmentSelector,
+  createArticlesInCollectionGroupSelector
+} from '../shared';
 
 const state = {
+  collections: {
+    c1: {
+      groups: ['group1', 'group2'],
+      articles: {
+        live: ['af1', 'af2']
+      }
+    },
+    c2: {
+      groups: ['group1'],
+      articles: {
+        draft: ['af3', 'af4']
+      }
+    }
+  },
   externalArticles: {
     ea1: {
       id: 'ea1',
@@ -10,26 +28,114 @@ const state = {
   articleFragments: {
     af1: {
       uuid: 'af1',
-      id: 1,
+      id: 'ea1',
       frontPublicationDate: 1,
       publishedBy: 'A. N. Author',
       meta: {
-        group: 1
+        group: '0'
+      }
+    },
+    afWithInvalidReference: {
+      uuid: 'afWithInvalidReference',
+      id: 'invalid',
+      meta: {}
+    },
+    af2: {
+      uuid: 'af2',
+      meta: {
+        group: '1'
+      }
+    },
+    af3: {
+      uuid: 'af3',
+      meta: {
+        group: '0'
+      }
+    },
+    af4: {
+      uuid: 'af4',
+      meta: {
+        group: '0'
       }
     }
   }
 };
 
 describe('Shared selectors', () => {
-  describe('getArticleFromArticleFragment', () => {
-    it('should derive an Article from an ArticleFragment and an ExternalArticle, using an ArticleFragment id', () => {
+  describe('createExternalArticleFromArticleFragmentSelector', () => {
+    it('should create a selector that returns an external article referenced by the given article', () => {
+      expect(externalArticleFromArticleFragmentSelector(state, 'af1')).toEqual(
+        state.externalArticles.ea1
+      );
+      expect(
+        externalArticleFromArticleFragmentSelector(state, 'invalid')
+      ).toEqual(null);
+    });
+  });
 
+  describe('createArticleFromArticleFragmentSelector', () => {
+    it('should derive an Article from an ArticleFragment and an ExternalArticle, using an ArticleFragment id', () => {
+      const selector = createArticleFromArticleFragmentSelector();
+      expect(selector(state, 'af1')).toEqual({
+        id: 'ea1',
+        headline: 'Example external article',
+        group: '0'
+      });
     });
     it('should handle a case where the referenced ArticleFragment does not exist', () => {
-
+      const selector = createArticleFromArticleFragmentSelector();
+      expect(selector(state, 'invalid')).toEqual(null);
     });
     it('should handle a case where the ExternalArticle referenced by the ArticleFragment does not exist', () => {
+      const selector = createArticleFromArticleFragmentSelector();
+      expect(selector(state, 'afWithInvalidReference')).toEqual(null);
+    });
+  });
 
+  describe('createArticlesInCollectionGroupSelector', () => {
+    it('should return a list of articles held by the given collection for the given display index', () => {
+      const selector = createArticlesInCollectionGroupSelector();
+      expect(
+        selector(state, {
+          collectionId: 'c1',
+          stage: 'live',
+          groupDisplayIndex: 0
+        })
+      ).toEqual(['af2']);
+      expect(
+        selector(state, {
+          collectionId: 'c1',
+          stage: 'live',
+          groupDisplayIndex: 1
+        })
+      ).toEqual(['af1']);
+      expect(
+        selector(state, {
+          collectionId: 'c2',
+          stage: 'draft',
+          groupDisplayIndex: 0
+        })
+      ).toEqual(['af3', 'af4']);
+    });
+    it('should return an empty array if the collection is not found', () => {
+      const selector = createArticlesInCollectionGroupSelector();
+      expect(
+        selector(state, {
+          collectionId: 'invalid',
+          stage: 'live',
+          groupDisplayIndex: 0
+        })
+      ).toEqual([]);
+    });
+    it('should return an empty array if the stage is not found', () => {
+      const selector = createArticlesInCollectionGroupSelector();
+      expect(
+        selector(state, {
+          collectionId: 'c1',
+          stage: 'invalid',
+          groupDisplayIndex: 0
+        })
+      ).toEqual([]);
     });
   });
 });
--- client-v2/src/selectors/shared.js
+++ client-v2/src/selectors/shared.js
@@ -2,35 +2,90 @@
 
 import { createSelector } from 'reselect';
 
-import type { ExternalArticle, ArticleFragment } from '../types/Shared';
+import type { ArticleFragment } from '../types/Shared';
 import type { State } from '../types/State';
 
-const getExternalArticles = (state: State) => state.externalArticles;
-const getArticleFragmentFromId = (state: State, id: string): ArticleFragment =>
+const externalArticlesSelector = (state: State) => state.externalArticles;
+
+const articleFragmentsSelector = (state: State) => state.articleFragments;
+
+const articleFragmentSelector = (state: State, id: string): ArticleFragment =>
   state.articleFragments[id];
-const getExternalArticleFromArticleFragmentId = (
+
+const externalArticleFromArticleFragmentSelector = (
   state: State,
   id: string
-): ExternalArticle | null => {
-  const articleFragment = getArticleFragmentFromId(state, id);
+) => {
+  const articleFragment = articleFragmentSelector(state, id);
+  const externalArticles = externalArticlesSelector(state);
   if (!articleFragment) {
     return null;
   }
-  const articleId = articleFragment.id.split('/')[2];
-  const externalArticles = getExternalArticles(state);
-  return externalArticles[articleId] || null;
+  return externalArticles[articleFragment.id] || null;
 };
 
-const getArticleFromArticleFragment = createSelector(
-  [getArticleFragmentFromId, getExternalArticleFromArticleFragmentId],
-  (articleFragment, externalArticle) => {
-    const group =
-      (articleFragment.meta && articleFragment.meta.group) || undefined;
-    return {
-      ...externalArticle,
-      ...{ group }
-    };
-  }
-);
+const createArticleFromArticleFragmentSelector = () =>
+  createSelector(
+    articleFragmentSelector,
+    externalArticleFromArticleFragmentSelector,
+    (articleFragment, externalArticle) => {
+      if (!articleFragment || !externalArticle) {
+        return null;
+      }
+      const group = articleFragment.meta && articleFragment.meta.group;
+      return {
+        ...externalArticle,
+        ...{ group }
+      };
+    }
+  );
 
-export { getExternalArticles, getArticleFromArticleFragment };
+const collectionsSelector = (state: State) => state.collections;
+
+const collectionIdSelector = (_, { collectionId }) => collectionId;
+
+const createCollectionSelector = () =>
+  createSelector(
+    collectionsSelector,
+    collectionIdSelector,
+    (collections, id) => collections[id]
+  );
+
+const groupDisplayIndexSelector = (
+  _,
+  { groupDisplayIndex }: { groupDisplayIndex: number }
+) => groupDisplayIndex;
+
+const stageSelector = (_, { stage }: { stage: string }) => stage;
+
+const createArticlesInCollectionGroupSelector = () => {
+  const collectionSelector = createCollectionSelector();
+  const defaultArray = [];
+  return createSelector(
+    articleFragmentsSelector,
+    collectionSelector,
+    groupDisplayIndexSelector,
+    stageSelector,
+    (articleFragments, collection, groupDisplayIndex, stage) => {
+      if (!collection || !collection.groups || !collection.articles[stage]) {
+        return defaultArray;
+      }
+      const numberOfGroups = collection.groups.length;
+      const groupNumber = numberOfGroups - groupDisplayIndex - 1;
+      return collection.articles[stage].filter(id => {
+        const articleFragment = articleFragments[id];
+        const articleGroup = articleFragment.meta.group
+          ? parseInt(articleFragment.meta.group, 10)
+          : 0;
+        return articleGroup === groupNumber;
+      });
+    }
+  );
+};
+
+export {
+  externalArticlesSelector,
+  externalArticleFromArticleFragmentSelector,
+  createArticleFromArticleFragmentSelector,
+  createArticlesInCollectionGroupSelector
+};
