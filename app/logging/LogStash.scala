19c1316b7c6d1bbeb9f437296c0f833d5d4a28eb
Jonathon Herbert <jonathon_herbert@33568.gnm.int>
2018-06-04 14:10:22 +0100

    LogStash now uses the logger from the Logging trait

--- app/logging/LogStash.scala
+++ app/logging/LogStash.scala
@@ -7,7 +7,7 @@ import com.gu.logback.appender.kinesis.KinesisAppender
 import conf.ApplicationConfiguration
 import net.logstash.logback.layout.LogstashLayout
 import org.slf4j.LoggerFactory
-import play.api.{LoggerLike, Logger => PlayLogger}
+import play.api.{LoggerLike}
 
 object LogStash {
 
@@ -55,9 +55,9 @@ object LogStash {
 
   def init(config: ApplicationConfiguration) = {
     if(config.logging.enabled) {
-      PlayLogger.info("LogConfig initializing")
+      logger.info("LogConfig initializing")
       (for {
-        lb <- asLogBack(PlayLogger)
+        lb <- asLogBack(logger)
       } yield {
         lb.info("Configuring Logback")
         val context = lb.getLoggerContext
@@ -80,9 +80,9 @@ object LogStash {
         )
         lb.addAppender(appender)
         lb.info("Configured Logback")
-      })getOrElse(PlayLogger.info("not running using logback"))
+      })getOrElse(logger.info("not running using logback"))
     } else {
-      PlayLogger.info("Logging disabled")
+      logger.info("Logging disabled")
     }
   }
 }
