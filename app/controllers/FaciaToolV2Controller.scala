16d8340a9d36fbb380ebc6b0a5b3e7dc44aa0381
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-06-06 08:11:54 +0100

    v2 endpont for updating collections

---
+++ app/controllers/FaciaToolV2Controller.scala
@@ -0,0 +1,56 @@
+package controllers
+
+import frontsapi.model.UpdateActions
+import logging.Logging
+import metrics.FaciaToolMetrics
+import model.Cached
+import permissions.BreakingNewsEditCollectionsCheck
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, AnyContent}
+import services._
+import updates._
+import util.Acl
+
+import scala.concurrent.{ExecutionContext, Future}
+
+
+class FaciaToolV2Controller(
+                           val acl: Acl,
+                           val structuredLogger: StructuredLogger,
+                           val faciaPress: FaciaPress,
+                           val updateActions: UpdateActions,
+                           val deps: BaseFaciaControllerComponents
+                         )(implicit ec: ExecutionContext)
+  extends BaseFaciaController(deps) with Logging {
+
+
+  def collectionEdits() = APIAuthAction { implicit request =>
+
+    FaciaToolMetrics.ApiUsageCount.increment()
+
+    val v2Update: Option[V2Update] = request.body.asJson.flatMap(jsValue => jsValue.asOpt[V2Update])
+    v2Update match {
+      case Some(update) => {
+          val identity = request.user
+
+          val shouldUpdateLive: Boolean = update.isLive
+
+          updateActions.v2UpdateCollection(update.id, update.collection, identity)
+
+          faciaPress.press(PressCommand(
+            Set(update.id),
+            live = update.isLive,
+            draft = (update.collection.draft.isEmpty && shouldUpdateLive) || !shouldUpdateLive)
+          )
+
+          structuredLogger.putLog(LogUpdate(V2CollectionUpdate(update.id, update.updateType), identity.email))
+
+          Ok(Json.toJson(update.collection)).as("application/json")
+
+      }
+      case None => BadRequest
+    }
+
+  }
+}
+
