0e8a4c0173518adc4473ba0dab77d07f1a715ad7
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-06-13 14:15:44 +0100

    Updates are always draft and don't log type

--- app/updates/V2Update.scala
+++ app/updates/V2Update.scala
@@ -3,7 +3,7 @@ package updates
 import com.gu.facia.client.models.CollectionJson
 import play.api.libs.json.{Format, Json}
 
-case class V2Update (id: String, updateType: String, collection: CollectionJson, isLive: Boolean)
+case class V2Update (id: String, collection: CollectionJson)
 
 object V2Update {
   implicit val jsonFormat: Format[V2Update] = Json.format[V2Update]
