c65bdcffe1a069886348f94e0efc5a380231679a
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-06-20 14:01:19 +0100

    Add a clipboard component

---
+++ client-v2/src/components/Clipboard.js
@@ -0,0 +1,14 @@
+// @flow
+
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Container = styled(`div`)`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+`;
+
+const Clipboard = () => <Container>Clipboard</Container>;
+
+export default Clipboard;
