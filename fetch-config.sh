8d1a99e484e5e6814dc9061d1bcb32ea75b128dc
susiecoleman <susiecoleman20@gmail.com>
2018-07-18 11:22:31 +0100

    Create correct directory and add sudo to give permission to alter content of /etc.

--- fetch-config.sh
+++ fetch-config.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-mkdir -p ~/etc/gu
-aws s3 cp s3://facia-private/facia-tool.application.secrets.local.conf /etc/gu/facia-tool.application.secrets.conf --profile cmsFronts
-aws s3 cp s3://facia-private/facia-tool.local.properties /etc/gu/facia-tool.properties --profile cmsFronts
+sudo mkdir -p /etc/gu
+sudo aws s3 cp s3://facia-private/facia-tool.application.secrets.local.conf /etc/gu/facia-tool.application.secrets.conf --profile cmsFronts
+sudo aws s3 cp s3://facia-private/facia-tool.local.properties /etc/gu/facia-tool.properties --profile cmsFronts
