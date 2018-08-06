d2f4eaf73dd95e594c1af23fd6ab3c547f631070
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-08-06 11:40:55 +0100

    Use export from shared schema

--- client-v2/src/util/clipboardSchema.js
+++ client-v2/src/util/clipboardSchema.js
@@ -2,18 +2,12 @@
 
 import { createType, build } from 'normalise-with-fields';
 import v4 from 'uuid/v4';
-import { postProcessArticleFragment } from 'shared/util/schema';
+import {
+  postProcessArticleFragment,
+  supportingArticles
+} from 'shared/util/schema';
 
 const articleFragments = createType('articleFragments', {
-  preProcess: af => ({
-    ...af,
-    uuid: v4()
-  }),
-  postProcess: postProcessArticleFragment,
-  idKey: 'uuid'
-});
-
-const supportingArticles = createType('articleFragments', {
   preProcess: af => ({
     ...af,
     uuid: v4()
