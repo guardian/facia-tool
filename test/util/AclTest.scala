a64050a6afbc843df4f6211e797b78a11a19f1bd
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-09-10 13:56:30 +0100

    More readable tests

--- test/util/AclTest.scala
+++ test/util/AclTest.scala
@@ -9,38 +9,71 @@ import permissions.{TrainingPermission, EmailPermission, EditorialPermission, Co
 
     "Allow access to commercial fronts if commercial permissions" in {
 
-      PermissionsChecker.check(AccessGranted, AccessDenied, AccessDenied, Set(CommercialPermission)) should be(AccessGranted)
+      PermissionsChecker.check(
+        hasCommercialPermissions = AccessGranted,
+        hasEditorialPermissions = AccessDenied,
+        hasTrainingPermissions = AccessDenied,
+        Set(CommercialPermission)
+      ) should be(AccessGranted)
     }
 
     "Allow access to editorial fronts if editorial permissions" in {
 
-      PermissionsChecker.check(AccessDenied, AccessGranted, AccessDenied, Set(EditorialPermission)) should be(AccessGranted)
-
+      PermissionsChecker.check(
+        hasCommercialPermissions = AccessDenied,
+        hasEditorialPermissions = AccessGranted,
+        hasTrainingPermissions = AccessDenied,
+        Set(EditorialPermission)
+      ) should be(AccessGranted)
     }
 
     "Allow access to email fronts if editorial permissions"  in {
-      PermissionsChecker.check(AccessDenied, AccessGranted, AccessDenied, Set(EmailPermission)) should be(AccessGranted)
+      PermissionsChecker.check(
+        hasCommercialPermissions = AccessDenied,
+        hasEditorialPermissions = AccessGranted,
+        hasTrainingPermissions = AccessDenied,
+        Set(EmailPermission)
+      ) should be(AccessGranted)
     }
 
     "Allow access to training fronts if training permissions" in {
 
-      PermissionsChecker.check(AccessDenied, AccessDenied, AccessGranted, Set(TrainingPermission)) should be(AccessGranted)
-
+      PermissionsChecker.check(
+        hasCommercialPermissions = AccessDenied,
+        hasEditorialPermissions = AccessDenied,
+        hasTrainingPermissions = AccessGranted,
+        Set(TrainingPermission)
+      ) should be(AccessGranted)
     }
 
     "Allow access to fronts shared between editorial and commercial if commercial permissions " in {
 
-      PermissionsChecker.check(AccessGranted, AccessDenied, AccessDenied, Set(EditorialPermission, CommercialPermission)) should be(AccessGranted)
+      PermissionsChecker.check(
+        hasCommercialPermissions = AccessGranted,
+        hasEditorialPermissions = AccessDenied,
+        hasTrainingPermissions = AccessDenied,
+        Set(EditorialPermission, CommercialPermission)
+      ) should be(AccessGranted)
     }
 
     "Allow access to fronts shared between editorial and commercial if editorial permissions " in {
 
-      PermissionsChecker.check(AccessDenied, AccessGranted, AccessDenied, Set(EditorialPermission, CommercialPermission)) should be(AccessGranted)
+      PermissionsChecker.check(
+        hasCommercialPermissions = AccessDenied,
+        hasEditorialPermissions = AccessGranted,
+        hasTrainingPermissions = AccessDenied,
+        Set(EditorialPermission, CommercialPermission)
+      ) should be(AccessGranted)
   }
 
     "Do not allow access if not access to any of the priorities " in {
 
-      PermissionsChecker.check(AccessDenied, AccessDenied, AccessGranted, Set(EditorialPermission, CommercialPermission)) should be(AccessDenied)
+      PermissionsChecker.check(
+        hasCommercialPermissions = AccessDenied,
+        hasEditorialPermissions = AccessDenied,
+        hasTrainingPermissions = AccessGranted,
+        Set(EditorialPermission, CommercialPermission)
+      ) should be(AccessDenied)
 
     }
 
