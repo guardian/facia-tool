6fc810b31e643e6daa4bcb54acf50b628ba30ecf
Jonathon Herbert <jonathon_herbert@34841.gnm.int>
2018-09-13 14:56:04 +0100

    Better comments and tests for deepGet

--- client-v2/src/shared/util/deepGet.js
+++ client-v2/src/shared/util/deepGet.js
@@ -1,19 +1,20 @@
 // @flow
 
+/**
+ * Safely get a nested property from an object.
+ *
+ * @param {Object} obj
+ * @param {string[]} properties
+ */
 export default function deepGet(obj: Object, properties: string[]) {
-  // If we have reached an undefined/null property
-  // then stop executing and return undefined.
   if (obj === undefined || obj === null) {
     return null;
   }
 
-  // If the path array has no more elements, we've reached
-  // the intended property and return its value.
   if (properties.length === 0) {
     return obj;
   }
 
-  // Prepare our found property and path array for recursion
   const foundSoFar = obj[properties[0]];
   const remainingProperties = properties.slice(1);
 
