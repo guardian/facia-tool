7b6f65476528c1d8d84f81d0a01b4ebcb007390c
vlbee <32312712+vlbee@users.noreply.github.com>
2018-10-04 16:08:17 +0100

    Add hover action icons and export index to shared/components/images

---
+++ client-v2/src/shared/images/icons-hover/index.js
@@ -0,0 +1,17 @@
+import View from './icon-hover-view.svg';
+import Ophan from './icon-hover-ophan.svg';
+import Copy from './icon-hover-copy.svg';
+import Paste from './icon-hover-paste-sublink.svg';
+import Clipboard from './icon-hover-clipboard.svg';
+import Delete from './icon-hover-delete.svg';
+
+const hoverActionIcons = {
+  view: View,
+  ophan: Ophan,
+  copy: Copy,
+  paste: Paste,
+  clipboard: Clipboard,
+  delete: Delete
+};
+
+export default hoverActionIcons;
