ee3540268715d783c4dc607b641653af42e1867f
Jonathon Herbert <jonathon_herbert@34841.gnm.int>
2018-10-15 17:38:43 +0100

    Began jest integration

--- client-v2/jest.config.js
+++ client-v2/jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
+  preset: 'ts-jest',
   transformIgnorePatterns: [
     '/node_modules/(?!panda-session|grid-util-js).+\\.js$'
   ],
   setupTestFrameworkScriptFile: './node_modules/jest-enzyme/lib/index.js',
-  setupFiles: ['./src/setupTest.js'],
+  setupFiles: ['./src/setupTest.ts'],
   moduleDirectories: ['<rootDir>/src', 'node_modules'],
-  testURL: 'http://localhost',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/mocks/fileMock.js'
--- client-v2/src/setupTest.ts
+++ client-v2/src/setupTest.ts
@@ -1,5 +1,3 @@
-
-
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
