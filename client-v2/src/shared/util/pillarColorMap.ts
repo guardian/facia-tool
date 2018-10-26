7707518bd4de9b7e53f9ccdd3f03ac9d2bbacbfd
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-10-26 12:47:53 +0100

    add new colour map

---
+++ client-v2/src/shared/util/pillarColorMap.ts
@@ -0,0 +1,24 @@
+const pillarColorMap = {
+  'pillar/news': '#c70000',
+  'pillar/sport': '#0084c6',
+  'pillar/opinion': '#c70000',
+  'pillar/lifestyle': '#bb3b80',
+  'pillar/arts': '#a1845c'
+} as { [pillar: string]: string };
+
+
+const notLiveColour = '#ff7f0f';
+const labsColour = '#65A897';
+const noPillarColour = '#221133';
+
+export const getPillarColor = (pillar: string | undefined, isLive: boolean) => {
+    if (!isLive) {
+      return notLiveColour;
+    }
+
+    if (!pillar) {
+      return noPillarColour;
+    }
+
+    return pillarColorMap[pillar];
+  };
