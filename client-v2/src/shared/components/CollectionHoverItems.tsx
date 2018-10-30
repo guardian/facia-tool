48c5b6fb76d1aa71c3d108546ccce11ead0c58ec
vlbee <32312712+vlbee@users.noreply.github.com>
2018-10-30 11:32:10 +0000

    Add HoverItem component to shared/component/Article

--- client-v2/src/shared/components/CollectionHoverItems.tsx
+++ client-v2/src/shared/components/CollectionHoverItems.tsx
@@ -1,28 +1,25 @@
 import styled from 'styled-components';
 
-const HoverActions = styled('div')`
+const HoverActionsAreaOverlay = styled('div')<{
+  isSnapLink?: boolean;
+}>`
   display: flex;
-  justify-content: space-between;
-  align-content: flex-end;
-  padding: 0 10px 8px;
+  justify-content: ${({ isSnapLink }) =>
+    isSnapLink ? 'flex-end' : 'space-between'};
+  padding: 4px 8px;
+`;
+
+const HideMetaDataOnToolTipDisplay = styled('div')<{
+  size?: string; // Article Component size
+}>`
+  background-color: #ededed;
+  position: absolute;
+  width: 70px;
+  height: ${({ size }) => (size === 'small' ? '90%' : '180%')};
+  margin: 2px;
   bottom: 0;
   left: 0;
-  opacity: 1;
-  position: absolute;
   right: 0;
-  visibility: hidden;
 `;
 
-const HoverActionsLeft = styled('div')`
-  display: flex;
-  justify-content: space-around;
-`;
-
-const HoverActionsRight = styled('div')`
-  display: flex;
-  justify-content: space-around;
-`;
-
-export { HoverActionsLeft, HoverActionsRight }
-
-export default HoverActions;
+export { HoverActionsAreaOverlay, HideMetaDataOnToolTipDisplay };
