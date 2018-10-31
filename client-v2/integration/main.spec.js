45eb53fad426768e32d62c1dbd2764023ee3a80b
RichieAHB <richard.beddington@guardian.co.uk>
2018-10-31 18:04:36 +0000

    Improve test

--- client-v2/integration/main.spec.js
+++ client-v2/integration/main.spec.js
@@ -7,17 +7,29 @@ fixture`Fronts edit`.page`http://localhost:3456/v2/editorial`
   .after(teardown);
 
 test('Drag and drop', async t => {
-  const topClipHeadline = await select('rich/debug', 'headline').textContent;
+  const topFrontHeadline = await select('rich/debug', 'headline').textContent;
   const topFeedHeadline = await select('feed-item', 'headline').textContent;
   const frontDropsCount = await select('rich/debug', '^drop-zone').count;
   await t
+    // drag from feed to front
     .setTestSpeed(0.5)
     .dragToElement(select('feed-item'), select('rich/debug', 'drop-zone:0'))
     .wait(1000)
     .expect(select('rich/debug', '^drop-zone').count)
     .eql(frontDropsCount + 2)
     .expect(select('rich/debug', 'headline').textContent)
-    .notEql(topClipHeadline)
+    .notEql(topFrontHeadline)
     .expect(select('rich/debug', 'headline').textContent)
-    .eql(topFeedHeadline);
+    .eql(topFeedHeadline)
+    // drag top to bottom in front
+    .dragToElement(
+      select('rich/debug', 'headline'),
+      select('rich/debug', '^drop-zone').nth(-1)
+    )
+    .expect(select('rich/debug', '^drop-zone').count)
+    .eql(frontDropsCount + 2)
+    .expect(select('rich/debug', 'headline').textContent)
+    .notEql(topFeedHeadline)
+    .expect(select('rich/debug', 'headline').textContent)
+    .eql(topFrontHeadline);
 });
