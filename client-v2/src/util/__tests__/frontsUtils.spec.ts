9a2c8043b4622c5f554a2949fd0ffac7460e2b84
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-11-15 09:17:01 +0000

    Generic fixture needs a generic name

--- client-v2/src/util/__tests__/frontsUtils.spec.ts
+++ client-v2/src/util/__tests__/frontsUtils.spec.ts
@@ -1,11 +1,11 @@
 import { getVisibilityStoryDetails } from '../frontsUtils';
-import { boostedArticleFragment, articleFragmentWithElementsThumbnail } from 'fixtures/articleFragment';
+import { boostedArticleFragment, articleFragment } from 'fixtures/articleFragment';
 
 describe('Front utilities', () => {
   describe('getVisibilityStoryDetails', () => {
     it('summarise stories correctly according to group ', () => {
       const groups = [];
-      const result = getVisibilityStoryDetails([[boostedArticleFragment], [articleFragmentWithElementsThumbnail, articleFragmentWithElementsThumbnail]]);
+      const result = getVisibilityStoryDetails([[boostedArticleFragment], [articleFragment, articleFragment]]);
       expect(result[0]).toEqual({group: 1, isBoosted: true});
       expect(result[1]).toEqual({group: 0, isBoosted: false});
       expect(result[2]).toEqual({group: 0, isBoosted: false});
