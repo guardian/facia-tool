5ce73b3c1a685b1d87acfaac9b42cd4fb076d7fd
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-12-06 15:20:03 +0000

    Use correct opinion color

--- client-v2/src/shared/util/getPillarColor.ts
+++ client-v2/src/shared/util/getPillarColor.ts
@@ -1,7 +1,7 @@
 const pillarColorMap = {
   'pillar/news': '#c70000',
   'pillar/sport': '#0084c6',
-  'pillar/opinion': '#c70000',
+  'pillar/opinion': '#e05e00',
   'pillar/lifestyle': '#bb3b80',
   'pillar/arts': '#a1845c'
 } as { [pillar: string]: string };
