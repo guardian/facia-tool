5964968dacc6a79ee66ce4f4dc55e77734bff108
Richard Beddington <richard.beddington@gmail.com>
2018-12-07 10:44:07 +0000

    Minimum viable collection overview (#496)
    * Minimum viable collection overview
    
    * PR fixes

---
+++ client-v2/src/components/FrontsEdit/CollectionOverview.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Collection, CollectionItemSets } from 'shared/types/Collection';
+import { State } from 'types/State';
+import {
+  createCollectionSelector,
+  selectSharedState,
+  createArticlesInCollectionSelector
+} from 'shared/selectors/shared';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { createCollectionId } from 'shared/components/Collection';
+
+interface FrontCollectionOverviewContainerProps {
+  collectionId: string;
+  browsingStage: CollectionItemSets;
+}
+
+type FrontCollectionOverviewProps = FrontCollectionOverviewContainerProps & {
+  collection: Collection | undefined;
+  articleCount: number;
+};
+
+const Container = styled.button`
+  align-items: center;
+  appearance: none;
+  background-color: #f6f6f6;
+  border: 1px solid #c4c4c4;
+  border-radius: 1.25em;
+  color: inherit;
+  cursor: pointer;
+  display: flex;
+  font-family: TS3TextSans;
+  font-size: 14px;
+  height: 2.5em;
+  margin-top: 0.75em;
+  padding: 0.25em 0.75em;
+  text-align: left;
+  text-decoration: none;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #ddd;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const TextLeft = styled.div`
+  flex: 1 1;
+  font-size: 14px;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+const TextRight = styled.div`
+  flex: 0 0 auto;
+  font-size: 14px;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+const Name = styled.span`
+  color: #333;
+  font-weight: 700;
+`;
+
+const CollectionOverview = ({
+  collection,
+  articleCount
+}: FrontCollectionOverviewProps) =>
+  collection ? (
+    <Container
+      onClick={e => {
+        e.preventDefault();
+        const el = document.getElementById(createCollectionId(collection));
+        if (el) {
+          el.scrollIntoView({
+            behavior: 'smooth',
+            inline: 'start',
+            block: 'start'
+          });
+        }
+      }}
+    >
+      <TextLeft>
+        <Name>{collection.displayName}</Name> {articleCount} items
+      </TextLeft>
+      <TextRight>{/* more metadata */}</TextRight>
+    </Container>
+  ) : null;
+
+const mapStateToProps = () => {
+  const collectionSelector = createCollectionSelector();
+  const articlesInCollectionSelector = createArticlesInCollectionSelector();
+  return (state: State, props: FrontCollectionOverviewContainerProps) => ({
+    collection: collectionSelector(selectSharedState(state), {
+      collectionId: props.collectionId
+    }),
+    articleCount: articlesInCollectionSelector(selectSharedState(state), {
+      collectionSet: props.browsingStage,
+      collectionId: props.collectionId
+    }).length
+  });
+};
+
+export default connect(mapStateToProps)(CollectionOverview);
--- client-v2/src/shared/components/layout/ContentContainer.ts
+++ client-v2/src/shared/components/layout/ContentContainer.ts
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
+interface ContentContainerProps {
+  setBack?: boolean;
+}
+
 export default styled('div')`
-  background-color: #f6f6f6;
+  background-color: ${({ setBack }: ContentContainerProps) =>
+    setBack ? 'transparent' : '#f6f6f6'};
   position: relative;
   padding: 0 10px 10px 10px;
   box-shadow: 0 -1px 0 #333;
-  border: 1px solid #c9c9c9;
+  border: ${({ setBack }: ContentContainerProps) =>
+    setBack ? 'none' : '1px solid #c9c9c9'};
   border-top: none;
   & + & {
     margin-top: 10px;
