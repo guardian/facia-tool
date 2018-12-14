123c1f1a0f67f94c1cbb89c325d58d898faef5cd
Jonathon Herbert <jonathon_herbert@34841.gnm.int>
2018-12-14 17:42:56 +0000

    Remove now-unnecessary theme

--- client-v2/src/components/util/__tests__/TextHighlighter.spec.tsx
+++ client-v2/src/components/util/__tests__/TextHighlighter.spec.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import { render, prettyDOM, cleanup } from 'react-testing-library';
 import TextHighlighter from 'components/util/TextHighlighter';
-import { ThemeProvider } from 'styled-components';
-import theme from 'shared/constants/theme';
 
 describe('TextHighlighter', () => {
   afterEach(cleanup);
   it('should render text with the parts that match the search string highlighted', () => {
     const { getAllByText } = render(
-      <ThemeProvider theme={theme}>
-        <TextHighlighter
-          originalString="An example string"
-          searchString="example"
-        />
-      </ThemeProvider>
+      <TextHighlighter
+        originalString="An example string"
+        searchString="example"
+      />
     );
     expect(getAllByText('example').length).toBe(1);
   });
   it('should find all instances of the search string', () => {
-    const { getAllByText, container } = render(
-      <ThemeProvider theme={theme}>
-        <TextHighlighter
-          originalString="An example string with two examples"
-          searchString="example"
-        />
-      </ThemeProvider>
+    const { getAllByText } = render(
+      <TextHighlighter
+        originalString="An example string with two examples"
+        searchString="example"
+      />
     );
     expect(getAllByText('example').length).toBe(2);
   });
