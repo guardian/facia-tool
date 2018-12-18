c459d800417d875b11716440d98f0258a54ce173
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-12-18 14:35:28 +0000

    styling for date range picker

--- client-v2/src/components/FrontsCAPIInterface/DateInput.tsx
+++ client-v2/src/components/FrontsCAPIInterface/DateInput.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'react-dates/initialize';
 import { DateRangePicker } from 'react-dates';
+import styled from 'styled-components';
 import moment from 'moment';
 
 interface CAPIDateInputProps {
@@ -13,6 +14,20 @@ interface CAPIDateInputState {
   focusedInput: 'startDate' | 'endDate' | null
 }
 
+const DatePickerContainer = styled('div')`
+  padding-top: 24px;
+  padding-left: 2px;
+
+`;
+
+const SearchTitle = styled('div')`
+  font-size: 16px;
+  font-weight: bold;
+  color: #121212;
+  margin-right: 3px;
+  float: left;
+  margin-bottom: 5px;
+`;
 
 export default class CAPIDateRangeInput extends React.Component<CAPIDateInputProps, CAPIDateInputState> {
 
@@ -25,25 +40,26 @@ export default class CAPIDateRangeInput extends React.Component<CAPIDateInputPro
     const { start, end, onDateChange } = this.props;
 
     return (
-      <div>
-      <DateRangePicker
-        displayFormat='DD/MM/YYYY'
-        minimumNights={0}
-        startDate={start}
-        startDateId="startDateId"
-        endDate={end}
-        endDateId="endDateId"
-        onDatesChange={ ({ startDate, endDate }) => {
-          onDateChange(startDate, endDate);
-        }}
-        initialVisibleMonth={() => moment().subtract(1, 'months')}
-        isOutsideRange={(day) => day.isAfter(moment())}
-        focusedInput={this.state.focusedInput}
-        onFocusChange={focusedInput => {
-          this.setState({ focusedInput })
-        }}
-      />
-      </div>
+      <DatePickerContainer>
+        <SearchTitle> Date Range: </SearchTitle>
+        <DateRangePicker
+          displayFormat='DD/MM/YYYY'
+          minimumNights={0}
+          startDate={start}
+          startDateId="startDateId"
+          endDate={end}
+          endDateId="endDateId"
+          onDatesChange={ ({ startDate, endDate }) => {
+            onDateChange(startDate, endDate);
+          }}
+          initialVisibleMonth={() => moment().subtract(1, 'months')}
+          isOutsideRange={(day) => day.isAfter(moment())}
+          focusedInput={this.state.focusedInput}
+          onFocusChange={focusedInput => {
+            this.setState({ focusedInput })
+          }}
+        />
+      </DatePickerContainer>
     );
   };
 }
