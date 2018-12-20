7f1d88628cf8f395158cfe15c35724f634b56c28
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-12-20 08:56:30 +0000

    Stage as a command line argument

--- scripts/editorial_backfills.py
+++ scripts/editorial_backfills.py
@@ -3,17 +3,15 @@ import json
 import datetime
 import io
 import time
+import sys
 
 session = boto3.Session(profile_name='cmsFronts')
 s3 = session.resource('s3')
 bucket = 'facia-tool-store'
-stage = 'CODE'
-configPath = stage + '/frontsapi/config/config.json'
-collectionPath = stage + '/frontsapi/collection/'
 fileName = 'config.json'
 
 
-def get_collection_json(collection_id):
+def get_collection_json(collection_id, collectionPath):
     obj = s3.Object(bucket, collectionPath + collection_id + '/collection.json')
     try:
         data = io.BytesIO()
@@ -30,75 +28,81 @@ def run():
     '''
 
     print 'Starting script'
-    no_backfill_fronts = list()
-    automated_fronts = list()
+    if len(sys.argv) > 1:
+        stage = sys.argv[1]
+        configPath = stage + '/frontsapi/config/config.json'
+        collectionPath = stage + '/frontsapi/collection/'
+        no_backfill_fronts = list()
+        automated_fronts = list()
 
-    s3.Object(bucket, configPath).download_file('./frontsconfig.json')
-    with open('frontsconfig.json') as data_file:
-        config = json.load(data_file)
+        s3.Object(bucket, configPath).download_file('./frontsconfig.json')
+        with open('frontsconfig.json') as data_file:
+            config = json.load(data_file)
 
-    fronts = (config['fronts'])
-    collections = (config['collections'])
+        fronts = (config['fronts'])
+        collections = (config['collections'])
 
-    for front_name in fronts:
+        for front_name in fronts:
 
-        front_config = fronts.get(front_name)
-        front_collections = front_config.get('collections')
+            front_config = fronts.get(front_name)
+            front_collections = front_config.get('collections')
 
-        if not 'priority' in front_config:
-            automated = True
-            fully_manual = True
-            last_updates = list()
+            if not 'priority' in front_config:
+                automated = True
+                fully_manual = True
+                last_updates = list()
 
-            for collection_id in front_collections:
-                collection = collections.get(collection_id)
-                edited_collection = get_collection_json(collection_id)
+                for collection_id in front_collections:
+                    collection = collections.get(collection_id)
+                    edited_collection = get_collection_json(collection_id, collectionPath)
 
-                if edited_collection:
-                    name = edited_collection.get('displayName')
-                    update = edited_collection.get('lastUpdated')
-                    if isinstance(update, int):
-                        last_updates.append(update)
-                    else:
-                        #Some old collections have last update time saved as string
-                        dt_obj = datetime.datetime.strptime(update, '%Y-%m-%dT%H:%M:%S.%fZ')
-                        last_updates.append(time.mktime(dt_obj.timetuple())*1000)
-
-                if 'backfill' in collection:
-                    fully_manual = False
-                    #For any collection with a backfill we also need to check if it contains
-                    #manually curated articles. If it does, it is not a fully automated collecion.
                     if edited_collection:
-                        live = edited_collection.get('live')
-                        draft = edited_collection.get('draft')
-                        if name == 'new':
-                            if live and len(live) > 0:
-                                if any("snap" not in item.get('id') for item in live):
-                                    automated = False
+                        name = edited_collection.get('displayName')
+                        update = edited_collection.get('lastUpdated')
+                        if isinstance(update, int):
+                            last_updates.append(update)
+                        else:
+                            #Some old collections have last update time saved as string
+                            dt_obj = datetime.datetime.strptime(update, '%Y-%m-%dT%H:%M:%S.%fZ')
+                            last_updates.append(time.mktime(dt_obj.timetuple())*1000)
 
-                            if draft and len(draft) > 0:
-                                if any("snap" not in item.get('id') for item in draft):
-                                    automated = False
+                    if 'backfill' in collection:
+                        fully_manual = False
+                        #For any collection with a backfill we also need to check if it contains
+                        #manually curated articles. If it does, it is not a fully automated collecion.
+                        if edited_collection:
+                            live = edited_collection.get('live')
+                            draft = edited_collection.get('draft')
+                            if name == 'new':
+                                if live and len(live) > 0:
+                                    if any("snap" not in item.get('id') for item in live):
+                                        automated = False
+
+                                if draft and len(draft) > 0:
+                                    if any("snap" not in item.get('id') for item in draft):
+                                        automated = False
+                    else:
+                        automated = False
+
+                if len(last_updates) > 0:
+                    last_front_update = max(last_updates)
+                    date = datetime.datetime.fromtimestamp(last_front_update/1000.0).strftime('%Y-%m-%d %H:%M:%S')
                 else:
-                    automated = False
+                    date = ''
 
-            if len(last_updates) > 0:
-                last_front_update = max(last_updates)
-                date = datetime.datetime.fromtimestamp(last_front_update/1000.0).strftime('%Y-%m-%d %H:%M:%S')
-            else:
-                date = ''
+                if automated:
+                    automated_fronts.append(front_name + ', ' + date)
+                if fully_manual:
+                    no_backfill_fronts.append(front_name + ', ' + date)
 
-            if automated:
-                automated_fronts.append(front_name + ', ' + date)
-            if fully_manual:
-                no_backfill_fronts.append(front_name + ', ' + date)
+        automated_json = json.dumps(automated_fronts, indent=4)
+        fautomated = open('automated_fronts.json', 'w')
+        fautomated.write(automated_json)
 
-    automated_json = json.dumps(automated_fronts, indent=4)
-    fautomated = open('automated_fronts.json', 'w')
-    fautomated.write(automated_json)
-
-    no_backfilled_json = json.dumps(no_backfill_fronts, indent=4)
-    fnobackfilled = open('not_backfilled_fronts.json', 'w')
-    fnobackfilled.write(no_backfilled_json)
+        no_backfilled_json = json.dumps(no_backfill_fronts, indent=4)
+        fnobackfilled = open('not_backfilled_fronts.json', 'w')
+        fnobackfilled.write(no_backfilled_json)
+    else:
+        print >> sys.stderr, "Usage: pass in the stage"
 
 run()
