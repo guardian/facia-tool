7552b9354abc64efb43a83dd2ac28fe960a4284f
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-12-20 11:49:25 +0000

    Log details of collections on fronts which are a mixture of both curated and automated fronts

--- scripts/editorial_backfills.py
+++ scripts/editorial_backfills.py
@@ -25,6 +25,9 @@ def run():
     '''
     Finds editorialfronts which are either fully automated or do not contain any automated collections.
     Writes the names of these fronts and the time they were last edited into a json file.
+    Fronts which have a mixuture of fully automated and curated collections will have the names of collections
+    in these groups recorded in a separate file.
+    Takes the stage (CODE/PROD) we are querying as the first argument
     '''
 
     print 'Starting script'
@@ -34,6 +37,7 @@ def run():
         collectionPath = stage + '/frontsapi/collection/'
         no_backfill_fronts = list()
         automated_fronts = list()
+        manual_backfill_fronts = list()
 
         s3.Object(bucket, configPath).download_file('./frontsconfig.json')
         with open('frontsconfig.json') as data_file:
@@ -48,16 +52,26 @@ def run():
             front_collections = front_config.get('collections')
 
             if not 'priority' in front_config:
+
+                #Keep track of what kind of front we are looking at
                 automated = True
                 fully_manual = True
+                manual_backfill = False
+
+                #Keep track of which collections are either curated or fully automated
+                curated_collections = list()
+                automated_collections = list()
+
                 last_updates = list()
 
                 for collection_id in front_collections:
                     collection = collections.get(collection_id)
                     edited_collection = get_collection_json(collection_id, collectionPath)
+                    curated_collection = False
+
+                    name = collection.get('displayName')
 
                     if edited_collection:
-                        name = edited_collection.get('displayName')
                         update = edited_collection.get('lastUpdated')
                         if isinstance(update, int):
                             last_updates.append(update)
@@ -73,16 +87,23 @@ def run():
                         if edited_collection:
                             live = edited_collection.get('live')
                             draft = edited_collection.get('draft')
-                            if name == 'new':
-                                if live and len(live) > 0:
-                                    if any("snap" not in item.get('id') for item in live):
-                                        automated = False
+                            if live and len(live) > 0:
+                                if any("snap" not in item.get('id') for item in live):
+                                    automated = False
+                                    curated_collection = True
 
-                                if draft and len(draft) > 0:
-                                    if any("snap" not in item.get('id') for item in draft):
-                                        automated = False
+                            if draft and len(draft) > 0:
+                                if any("snap" not in item.get('id') for item in draft):
+                                    automated = False
+                                    curated_collection = True
+                        if curated_collection:
+                            manual_backfill = True
+                            curated_collections.append(name)
+                        else:
+                            automated_collections.append(name)
                     else:
                         automated = False
+                        curated_collections.append(name)
 
                 if len(last_updates) > 0:
                     last_front_update = max(last_updates)
@@ -94,6 +115,9 @@ def run():
                     automated_fronts.append(front_name + ', ' + date)
                 if fully_manual:
                     no_backfill_fronts.append(front_name + ', ' + date)
+                if manual_backfill:
+                    front_details = { front_name: { 'curated': curated_collections, 'automated': automated_collections} }
+                    manual_backfill_fronts.append(front_details)
 
         automated_json = json.dumps(automated_fronts, indent=4)
         fautomated = open('automated_fronts.json', 'w')
@@ -102,6 +126,10 @@ def run():
         no_backfilled_json = json.dumps(no_backfill_fronts, indent=4)
         fnobackfilled = open('not_backfilled_fronts.json', 'w')
         fnobackfilled.write(no_backfilled_json)
+
+        manual_backfill_fronts_json = json.dumps(manual_backfill_fronts, indent=4)
+        fmanualbackfilled = open('manual_backfilled_fronts.json', 'w')
+        fmanualbackfilled.write(manual_backfill_fronts_json)
     else:
         print >> sys.stderr, "Usage: pass in the stage"
 
