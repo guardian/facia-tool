24cc6dd9c04196573ce66cc671a5e38cfc810d95
vlbee <32312712+vlbee@users.noreply.github.com>
2019-02-08 13:19:41 +0000

    Add styling to Collapse All button

--- client-v2/src/shared/components/input/ButtonCircularCaret.tsx
+++ client-v2/src/shared/components/input/ButtonCircularCaret.tsx
@@ -6,12 +6,15 @@ import ButtonCircular from './ButtonCircular';
 
 export const ButtonCircularWithTransition = ButtonCircular.extend<{
   highlight?: boolean;
+  small?: boolean;
 }>`
   transition: transform 0.15s;
   display: inline-block;
   text-align: center;
   padding: 0;
   transform: rotate(0deg);
+  height: ${({ small }) => (small ? '18px' : undefined)};
+  width: ${({ small }) => (small ? '18px' : undefined)};
 
   ${({ highlight, theme }) =>
     highlight
@@ -19,21 +22,29 @@ export const ButtonCircularWithTransition = ButtonCircular.extend<{
       : ``};
 `;
 
-const CaretImg = styled('img')`
-  width: 18px;
+const CaretImg = styled('img')<{
+  small?: boolean;
+}>`
+  width: ${({ small }) => (small ? '14px' : '18px')};
   display: inline-block;
-  vertical-align: middle;
 `;
+
+interface ButtonCircularCaretWithTransitionProps {
+  active: boolean;
+  preActive: boolean;
+  small?: boolean;
+}
 
 export default ({
   active,
   preActive,
+  small,
   ...props
-}: { active: boolean; preActive: boolean } & React.HTMLAttributes<
-  HTMLButtonElement
->) => (
+}: ButtonCircularCaretWithTransitionProps &
+  React.HTMLAttributes<HTMLButtonElement>) => (
   <ButtonCircularWithTransition
     {...props}
+    small={small}
     highlight={preActive}
     style={{
       transform: active
@@ -43,6 +54,6 @@ export default ({
         : undefined
     }}
   >
-    <CaretImg src={caretIcon} alt="" />
+    <CaretImg src={caretIcon} alt="" small={small} />
   </ButtonCircularWithTransition>
 );
