cf899b5d569649edd9a09f225aaba78d1a1c0b1c
vlbee <32312712+vlbee@users.noreply.github.com>
2019-02-19 16:49:40 +0000

    WIP update v2 /collections route handler

--- app/controllers/FaciaToolV2Controller.scala
+++ app/controllers/FaciaToolV2Controller.scala
@@ -9,33 +9,70 @@ import services._
 import updates._
 import util.Acl
 import permissions.ModifyCollectionsPermissionsCheck
+import org.joda.time.{DateTime}
 import play.api.mvc.{Action, AnyContent}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 
+object GetCollectionRequest {
+  implicit val jsonFormat = Json.format[GetCollectionRequest]
+}
+
+case class GetCollectionRequest(
+                                 collectionSpecs: Seq[CollectionSpec]
+                               )
+
+case class CollectionSpec(id: String, `type`: String, lastUpdated: Option[Int])
+
 class FaciaToolV2Controller(
-                           val acl: Acl,
-                           val structuredLogger: StructuredLogger,
-                           val faciaPress: FaciaPress,
-                           val updateActions: UpdateActions,
-                           val configAgent: ConfigAgent,
-                           val collectionService: CollectionService,
-                           val deps: BaseFaciaControllerComponents
-                         )(implicit ec: ExecutionContext)
+                             val acl: Acl,
+                             val structuredLogger: StructuredLogger,
+                             val faciaPress: FaciaPress,
+                             val updateActions: UpdateActions,
+                             val configAgent: ConfigAgent,
+                             val collectionService: CollectionService,
+                             val deps: BaseFaciaControllerComponents
+                           )(implicit ec: ExecutionContext)
   extends BaseFaciaController(deps) with ModifyCollectionsPermissionsCheck with Logging {
 
-  def getCollections(): Action[AnyContent] = AccessAPIAuthAction.async { implicit request =>
-    val collectionIds = request.queryString.getOrElse("ids", Seq()).toList
+  def getCollections() = AccessAPIAuthAction(parse.json[GetCollectionRequest]).async { implicit request =>
+
+    val collectionSpecs = request.body.collectionSpecs.toList
+    val collectionIds = collectionSpecs.map(_.id)
+
     val collectionPriorities = collectionIds.flatMap(configAgent.getFrontsPermissionsPriorityByCollectionId(_)).toSet
+
+
+    //TODO write tests
 
     withModifyGroupPermissionForCollections(collectionPriorities, Set()) {
       val collections = collectionService.fetchCollectionsAndStoriesVisible(collectionIds)
 
-      collections.map(c => NoCache {
-        Ok(Json.toJson(c)).as("application/json")
-      })
+      // if spec includes lastUpdated timestamp, then this will only return the server data if it is more recent than the spec timestamp
+      def getNewerServerData(collectionAndStoriesResponse: CollectionAndStoriesResponse) = {
+        val id = collectionAndStoriesResponse.id
+        val lastUpdated = collectionAndStoriesResponse.collection.lastUpdated
+        val spec = collectionSpecs.find(spec => spec.id == id)
+
+        // .getOrElse ensures that if lastUpdated does not exist, it will always be less, and the server data will always be returned.
+        spec.flatMap(s => if (s.lastUpdated.getOrElse(-1) < lastUpdated.getMillis()) Some(collectionAndStoriesResponse) else None)
+      }
+      collections.map(c =>
+        c.map(_.flatMap(response => getNewerServerData(response)))).map(c =>
+        NoCache {
+          Ok(Json.toJson(c)).as("application/json")
+        })
+
+//      collections.map(c => NoCache {
+//        c.map(collectionAndStoriesResponse => collectionAndStoriesResponse match {
+//          case Some(response) => getNewerServerData(response)
+//          case None => None
+//        })
+//        Ok(Json.toJson(c)).as("application/json")
+//      })
     }
+
   }
 
   def collectionEdits() = AccessAPIAuthAction.async { implicit request =>
