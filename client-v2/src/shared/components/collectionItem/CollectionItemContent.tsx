b5ddff52fb6433373a53f5cbcee315a2ac155d38
Reetta Vaahtoranta <rvaahtoranta@gmail.com>
2019-03-25 15:52:00 +0000

    Longer headlines on trail text

--- client-v2/src/shared/components/collectionItem/CollectionItemContent.tsx
+++ client-v2/src/shared/components/collectionItem/CollectionItemContent.tsx
@@ -1,16 +1,28 @@
 import styled, { css } from 'styled-components';
-import { CollectionItemDisplayTypes } from 'shared/types/Collection';
+import {
+  CollectionItemDisplayTypes,
+  CollectionItemSizes
+} from 'shared/types/Collection';
 
 const CollectionItemContent = styled('div')<{
   displayType?: CollectionItemDisplayTypes;
+  displaySize?: CollectionItemSizes;
 }>`
   position: relative;
-  ${({ displayType }) =>
-    displayType === 'default' &&
-    css`
-      width: calc(100% - 210px);
-      padding: 0 8px;
-    `};
+  ${({ displayType, displaySize }) => {
+    if (displayType === 'default') {
+      if (displaySize !== 'small') {
+        return css`
+          width: calc(100% - 210px);
+          padding: 0 8px;
+        `;
+      }
+      return css`
+        width: calc(100% - 100px);
+        padding: 0 8px;
+      `;
+    }
+  }};
 `;
 
 CollectionItemContent.defaultProps = {
