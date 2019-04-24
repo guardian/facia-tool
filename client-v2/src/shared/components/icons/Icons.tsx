56da3ea89ec302989b0f9978ee3cc9f8aca14ce4
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-04-24 08:49:41 +0100

    Be kind, rewind! (Revert article meta to CAPI value) (#696)
    * Added originalValue prop to standard inputs -- if supplied, user can revert to this value with rewind icon
    
    * Feedback

--- client-v2/src/shared/components/icons/Icons.tsx
+++ client-v2/src/shared/components/icons/Icons.tsx
@@ -36,6 +36,26 @@ const DownCaretIcon = ({ fill, size = 'm', title = null }: IconProps) => (
   </svg>
 );
 
+const RewindIcon = ({ fill = '#333', size = 'xxs' }: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width={mapSize(size)}
+    height={mapSize(size)}
+    fill={fill}
+    viewBox="0 0 330.002 330.002"
+    xmlSpace="preserve"
+  >
+    <path
+      d="M320.741,1.143c-5.602-2.322-12.057-1.039-16.347,3.251L180.001,128.787V15.001
+	c0-6.067-3.654-11.537-9.26-13.858c-5.605-2.324-12.059-1.039-16.347,3.251l-150,149.999c-2.813,2.813-4.394,6.628-4.394,10.606
+	c0,3.978,1.58,7.794,4.394,10.607l150,150.001c2.87,2.87,6.706,4.394,10.61,4.394c1.932,0,3.881-0.374,5.736-1.142
+	c5.605-2.322,9.26-7.792,9.26-13.858V201.213l124.394,124.394c2.869,2.87,6.706,4.394,10.609,4.394c1.933,0,3.882-0.374,5.737-1.142
+	c5.605-2.322,9.26-7.792,9.26-13.858v-300C330.001,8.934,326.347,3.465,320.741,1.143z"
+    />
+  </svg>
+);
+
 const StarIcon = ({
   fill = theme.colors.white,
   outline = theme.colors.white,
@@ -203,6 +223,7 @@ const AddImageIcon = ({
 export {
   DownCaretIcon,
   RubbishBinIcon,
+  RewindIcon,
   CloseIcon, // block x
   MoreIcon, // tapered +
   ClearIcon, // tapered x
