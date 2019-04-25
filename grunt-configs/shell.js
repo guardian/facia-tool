2ca0da517e9963975b9d750e6ec3ae98ca4af37a
Richard Beddington <richard.beddington@gmail.com>
2019-04-25 14:56:47 +0100

    Rahb/fix jspm (#710)
    * Use package.json jspm
    
    * Commit package lock and install earlier
    
    * Fix unconverted jspm call

--- grunt-configs/shell.js
+++ grunt-configs/shell.js
@@ -2,29 +2,37 @@
 module.exports = function() {
     return {
         collections: {
-            command: 'jspm bundle ' + [
-                'setup', 'main',
-                'facia-tool/widgets/columns/fronts',
-                'facia-tool/widgets/columns/latest',
-                'facia-tool/widgets/clipboard',
-                'facia-tool/widgets/columns/fronts-standalone-clipboard',
-                'facia-tool/widgets/autocomplete',
-                'facia-tool/widgets/presser-detect-stale',
-                'facia-tool/widgets/copy-paste-articles',
-                'facia-tool/widgets/sparklines-trails',
-                'facia-tool/widgets/front-article-indicator'
-            ].join(' + ') + ' tmp/bundles/collections.js'
+            command:
+                'npm run jspm bundle ' +
+                [
+                    'setup',
+                    'main',
+                    'facia-tool/widgets/columns/fronts',
+                    'facia-tool/widgets/columns/latest',
+                    'facia-tool/widgets/clipboard',
+                    'facia-tool/widgets/columns/fronts-standalone-clipboard',
+                    'facia-tool/widgets/autocomplete',
+                    'facia-tool/widgets/presser-detect-stale',
+                    'facia-tool/widgets/copy-paste-articles',
+                    'facia-tool/widgets/sparklines-trails',
+                    'facia-tool/widgets/front-article-indicator'
+                ].join(' + ') +
+                ' tmp/bundles/collections.js'
         },
         config: {
-            command: 'jspm bundle ' + [
-                'setup', 'main',
-                'facia-tool/widgets/columns/fronts-search',
-                'facia-tool/widgets/columns/fronts-config',
-                'facia-tool/widgets/config-collection-backfill',
-                'facia-tool/widgets/config-collection-tags',
-                'facia-tool/widgets/config-card-types',
-                'facia-tool/widgets/config-nav-sections'
-            ].join(' + ') + ' tmp/bundles/config.js'
+            command:
+                'npm run jspm bundle ' +
+                [
+                    'setup',
+                    'main',
+                    'facia-tool/widgets/columns/fronts-search',
+                    'facia-tool/widgets/columns/fronts-config',
+                    'facia-tool/widgets/config-collection-backfill',
+                    'facia-tool/widgets/config-collection-tags',
+                    'facia-tool/widgets/config-card-types',
+                    'facia-tool/widgets/config-nav-sections'
+                ].join(' + ') +
+                ' tmp/bundles/config.js'
         }
     };
 };
