2faa69dbc74c27ab79e508dbde6dbdbca682d228
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-04-25 16:49:14 +0100

    The clipboard dropzone was small, which made me sad; now it is big, and I am happy (#711)

--- client-v2/src/components/Clipboard.tsx
+++ client-v2/src/components/Clipboard.tsx
@@ -66,7 +66,7 @@ const ClipboardTitle = styled.h2`
 
 const ClipboardBody = styled.div`
   padding: 10px;
-  flex-basis: 100%;
+  flex: 1;
   display: flex;
 `;
 
@@ -175,7 +175,7 @@ class Clipboard extends React.Component<ClipboardProps> {
                 style={{
                   display: 'flex',
                   flexDirection: 'column',
-                  height: '100%',
+                  flex: '1',
                   width: '140px'
                 }}
               >
