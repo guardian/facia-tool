50ee5f565dfd52e5e9c89b912eda32bab8eedef5
Reetta Vaahtoranta <rvaahtoranta@gmail.com>
2019-05-15 09:40:15 +0100

    Open Clipboard when dragging

--- client-v2/src/components/ClipboardHeader.tsx
+++ client-v2/src/components/ClipboardHeader.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Dispatch } from 'types/Store';
 import { connect } from 'react-redux';
-import {
-  selectIsClipboardOpen,
-  editorOpenClipboard,
-  editorCloseClipboard
-} from 'bundles/frontsUIBundle';
+import { selectIsClipboardOpen, editorOpenClipboard, editorCloseClipboard } from 'bundles/frontsUIBundle';
 import { State } from 'types/State';
 import { styled } from 'constants/theme';
 import ButtonCircularCaret from 'shared/components/input/ButtonCircularCaret';
+import DragIntentContainer from 'shared/components/DragIntentContainer';
+
 
 interface ClipboardHeaderProps {
   isClipboardOpen: boolean;
   toggleClipboard: (open: boolean) => void;
 }
+
+const StyledDragIntentContainer = styled(DragIntentContainer)`
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+`;
 
 const Header = styled.div`
   align-items: center;
@@ -30,28 +34,37 @@ const ClipboardTitle = styled.h2`
   margin: 0;
 `;
 
+
 class ClipboardHeader extends React.Component<ClipboardHeaderProps> {
   public state = {
     preActive: false
   };
   public render() {
     return (
-      <Header>
-        {this.props.isClipboardOpen && (
-          <ClipboardTitle>Clipboard</ClipboardTitle>
-        )}
-        <ButtonCircularCaret
-          tabIndex={-1}
-          openDir="right"
-          active={this.props.isClipboardOpen}
-          preActive={this.state.preActive}
-          onClick={() =>
-            this.props.toggleClipboard(!this.props.isClipboardOpen)
-          }
-        />
-      </Header>
+      <StyledDragIntentContainer
+          active={!this.props.isClipboardOpen}
+          delay={300}
+          onDragIntentStart={() => this.setState({ preActive: true })}
+          onDragIntentEnd={() => this.setState({ preActive: false })}
+          onIntentConfirm={() => this.props.toggleClipboard(true)}
+      >
+        <Header>
+          {this.props.isClipboardOpen && (
+            <ClipboardTitle>Clipboard</ClipboardTitle>
+          )}
+          <ButtonCircularCaret
+            tabIndex={-1}
+            openDir="right"
+            active={this.props.isClipboardOpen}
+            preActive={this.state.preActive}
+            onClick={() =>
+              this.props.toggleClipboard(!this.props.isClipboardOpen)
+            }
+          />
+        </Header>
+      </StyledDragIntentContainer>
     );
-  }
+  };
 }
 
 const mapStateToProps = (state: State) => ({
@@ -60,10 +73,8 @@ const mapStateToProps = (state: State) => ({
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   toggleClipboard: (open: boolean) =>
-    dispatch(open ? editorOpenClipboard() : editorCloseClipboard())
+    dispatch(open ? editorOpenClipboard() : editorCloseClipboard()),
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ClipboardHeader);
+export default connect(mapStateToProps, mapDispatchToProps)(ClipboardHeader);
+
