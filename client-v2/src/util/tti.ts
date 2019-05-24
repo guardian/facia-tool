5920b36a4e6a1d2385463ea9b4ac0e69072dec44
Richard Beddington <richard.beddington@guardian.co.uk>
2019-05-24 10:52:49 +0100

    Add TTI to GA (#740)

---
+++ client-v2/src/util/tti.ts
@@ -0,0 +1,11 @@
+import ttiPolyfill from 'tti-polyfill';
+import { gtag } from 'services/GA';
+
+ttiPolyfill.getFirstConsistentlyInteractive().then((tti: any) => {
+  gtag('send', 'event', {
+    eventCategory: 'Performance Metrics',
+    eventAction: 'TTI',
+    eventValue: tti,
+    nonInteraction: true
+  });
+});
