0962ffba47b5f95459f1593f1a42d921abdfc0b6
Michael Barton <michael.barton@guardian.co.uk>
2019-06-05 15:02:38 +0100

    Use a single traverse for sending all breaking news payloads

--- app/util/BreakingNewsClient.scala
+++ app/util/BreakingNewsClient.scala
@@ -7,6 +7,8 @@ import com.gu.mobile.notifications.client.models.{BreakingNewsPayload, Notificat
 import play.api.libs.json.{JsError, JsSuccess, Json, Reads}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success}
 
 case class BreakingNewsResponse(id: String)
 
@@ -23,35 +25,23 @@ class BreakingNewsClient(
 
   private val url = s"$host/push/topic"
 
-  def send(notificationPayload: NotificationPayload)(implicit ec: ExecutionContext): Future[Either[ApiClientError, Unit]] = {
-
-    def doSend(payload: NotificationPayload): Future[Either[ApiClientError, Unit]] = {
-      val json = Json.stringify(Json.toJson(payload))
-      postJson(url, json) map {
-        case error: HttpError => Left(ApiHttpError(error.status, Some(error.body)))
-        case HttpOk(201, body) => validateFormat[BreakingNewsResponse](body)
-        case HttpOk(code, body) => Left(UnexpectedApiResponseError(s"Server returned status code $code and body:$body"))
-      } recover {
-        case e: Exception => Left(HttpProviderError(e))
-      }
+  def send(notificationPayload: BreakingNewsPayload)(implicit ec: ExecutionContext): Future[List[Either[ApiClientError, BreakingNewsResponse]]] = {
+    Future.traverse(notificationPayload.topic) { topic =>
+      // We send a Global notification as multiple notifications to each requested topic
+      // This is because there is a limit of 3 topics at once in mobile-n10n and we currently have 4 global editions
+      val payload = notificationPayload.copy(topic = List(topic), id = UUID.randomUUID())
+      doSend(payload, topic)
     }
+  }
 
-    def doSendOncePerTopic(payload: BreakingNewsPayload): Future[Either[ApiClientError, Unit]] = {
-      val results = payload.topic
-        .map(topic => payload.copy(topic = List(topic), id = UUID.randomUUID()))
-        .map(doSend)
-      Future.sequence(results).map { responses =>
-        responses.collect { case Left(error) => error } match {
-          case Nil => Right(())
-          case onlyOneError :: Nil => Left(onlyOneError)
-          case errors: List[ApiClientError] => Left(UnexpectedApiResponseError(errors.map(_.description).mkString(", ")))
-        }
-      }
-    }
-
-    notificationPayload match {
-      case breakingNews: BreakingNewsPayload if breakingNews.topic.size > 1 => doSendOncePerTopic(breakingNews)
-      case _ => doSend(notificationPayload)
+  private def doSend(payload: BreakingNewsPayload, topic: Topic)(implicit ec: ExecutionContext): Future[Either[ApiClientError, BreakingNewsResponse]] = {
+    val json = Json.stringify(Json.toJson(payload))
+    postJson(url, json) map {
+      case error: HttpError => Left(ApiHttpError(error.status, Some(error.body)))
+      case HttpOk(201, body) => validateFormat[BreakingNewsResponse](body)
+      case HttpOk(code, body) => Left(UnexpectedApiResponseError(s"Server returned status code $code and body:$body"))
+    } recover {
+      case NonFatal(e) => Left(HttpProviderError(e))
     }
   }
 
@@ -64,15 +54,15 @@ class BreakingNewsClient(
     )
   }
 
-  private def validateFormat[T](jsonBody: String)(implicit jr: Reads[T]): Either[ApiClientError, Unit] = {
+  private def validateFormat[T](jsonBody: String)(implicit jr: Reads[T]): Either[ApiClientError, T] = {
     try {
       Json.parse(jsonBody).validate[T] match {
-        case _: JsSuccess[T] => Right(())
+        case JsSuccess(r, _) => Right(r)
         case _: JsError => Left(UnexpectedApiResponseError(jsonBody))
       }
     }
     catch {
-      case _: Exception => Left(UnexpectedApiResponseError(jsonBody))
+      case NonFatal(_) => Left(UnexpectedApiResponseError(jsonBody))
     }
   }
 }
