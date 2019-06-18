8ec48778f434fb4922080ea69138e4d3c0418eff
RichieAHB <richard.beddington@guardian.co.uk>
2019-06-18 15:45:50 +0100

    Add temp API endpoint

--- client-v2/src/services/faciaApi.ts
+++ client-v2/src/services/faciaApi.ts
@@ -20,7 +20,7 @@ import { CapiArticle } from 'types/Capi';
 import chunk from 'lodash/chunk';
 import { CAPISearchQueryResponse, checkIsResults } from './capiQuery';
 import flatMap from 'lodash/flatMap';
-import { EditionsIssue } from 'types/Edition';
+import { EditionsIssue, EditionsCollection } from 'types/Edition';
 
 function fetchEditionsIssueAsConfig(editionId: string): Promise<FrontsConfig> {
   return pandaFetch(`/editions-api/${editionId}`, {
@@ -176,12 +176,12 @@ async function publishCollection(collectionId: string): Promise<void> {
   }
 }
 
-async function updateCollection(
+const createUpdateCollection = <T>(path: string) => async (
   id: string,
-  collection: CollectionWithNestedArticles
-): Promise<CollectionWithNestedArticles> {
+  collection: T
+): Promise<void> => {
   try {
-    const response = await pandaFetch(`/v2Edits`, {
+    const response = await pandaFetch(path, {
       method: 'post',
       headers: {
         'Content-Type': 'application/json'
@@ -197,7 +197,14 @@ async function updateCollection(
       }: ${response.body}`
     );
   }
-}
+};
+
+const updateCollection = createUpdateCollection<CollectionWithNestedArticles>(
+  '/v2Edits'
+);
+const updateEditionsCollection = createUpdateCollection<EditionsCollection>(
+  '/editions-api/collections'
+);
 
 async function saveClipboard(
   clipboardContent: NestedArticleFragment[]
@@ -421,6 +428,7 @@ export {
   fetchLastPressed,
   publishCollection,
   updateCollection,
+  updateEditionsCollection,
   saveClipboard,
   saveOpenFrontIds,
   saveFavouriteFrontIds,
