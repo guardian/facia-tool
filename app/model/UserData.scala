c43f4067562cc53b157883b8040ac350011c2a4f
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2019-07-18 16:31:31 +0100

    Amendments to enable feature toggling by server

--- app/model/UserData.scala
+++ app/model/UserData.scala
@@ -31,10 +31,7 @@ object UserData {
 case class UserData(
                      email: String,
                      clipboardArticles: Option[List[Trail]],
-<<<<<<< HEAD
                      editionsClipboardArticles: Option[List[Trail]],
-=======
->>>>>>> Add feature switches to outgoing app defaults; modifications to feature switch model
                      frontIds: Option[List[String]],
                      frontIdsByPriority: Option[Map[String, List[String]]],
                      favouriteFrontIdsByPriority: Option[Map[String, List[String]]],
@@ -44,7 +41,6 @@ case class UserData(
 object UserDataForDefaults {
   implicit val jsonFormat = Json.format[UserDataForDefaults]
 
-<<<<<<< HEAD
   def fromUserData(userData: UserData, clipboardArticles: Option[List[Trail]]): UserDataForDefaults = {
     val featureSwitches = userData.featureSwitches.fold(FeatureSwitches.all) { userFeatureSwitches =>
       val unsetFeatureSwitches = FeatureSwitches.all.diff(userFeatureSwitches)
@@ -52,15 +48,6 @@ object UserDataForDefaults {
     }
     UserDataForDefaults(
       clipboardArticles,
-=======
-  def fromUserData(userData: UserData): UserDataForDefaults = {
-    val featureSwitches = userData.featureSwitches.fold(FeatureSwitches.all) { userFeatureSwitches =>
-      val unsetFeatureSwitches = userFeatureSwitches.diff(FeatureSwitches.all)
-      unsetFeatureSwitches ++ userFeatureSwitches
-    }
-    UserDataForDefaults(
-      userData.clipboardArticles,
->>>>>>> Add feature switches to outgoing app defaults; modifications to feature switch model
       userData.frontIds,
       userData.frontIdsByPriority,
       userData.favouriteFrontIdsByPriority,
