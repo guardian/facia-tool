4f8a410d851e671f65a2e742407d6a8886f2184d
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-07-18 15:06:46 +0100

    Add feature switches to outgoing app defaults; modifications to feature switch model

--- app/model/UserData.scala
+++ app/model/UserData.scala
@@ -31,7 +31,10 @@ object UserData {
 case class UserData(
                      email: String,
                      clipboardArticles: Option[List[Trail]],
+<<<<<<< HEAD
                      editionsClipboardArticles: Option[List[Trail]],
+=======
+>>>>>>> Add feature switches to outgoing app defaults; modifications to feature switch model
                      frontIds: Option[List[String]],
                      frontIdsByPriority: Option[Map[String, List[String]]],
                      favouriteFrontIdsByPriority: Option[Map[String, List[String]]],
@@ -41,6 +44,7 @@ case class UserData(
 object UserDataForDefaults {
   implicit val jsonFormat = Json.format[UserDataForDefaults]
 
+<<<<<<< HEAD
   def fromUserData(userData: UserData, clipboardArticles: Option[List[Trail]]): UserDataForDefaults = {
     val featureSwitches = userData.featureSwitches.fold(FeatureSwitches.all) { userFeatureSwitches =>
       val unsetFeatureSwitches = FeatureSwitches.all.diff(userFeatureSwitches)
@@ -48,6 +52,15 @@ object UserDataForDefaults {
     }
     UserDataForDefaults(
       clipboardArticles,
+=======
+  def fromUserData(userData: UserData): UserDataForDefaults = {
+    val featureSwitches = userData.featureSwitches.fold(FeatureSwitches.all) { userFeatureSwitches =>
+      val unsetFeatureSwitches = userFeatureSwitches.diff(FeatureSwitches.all)
+      unsetFeatureSwitches ++ userFeatureSwitches
+    }
+    UserDataForDefaults(
+      userData.clipboardArticles,
+>>>>>>> Add feature switches to outgoing app defaults; modifications to feature switch model
       userData.frontIds,
       userData.frontIdsByPriority,
       userData.favouriteFrontIdsByPriority,
