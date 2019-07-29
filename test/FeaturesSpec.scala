11bb2f41c649a0e7933cfa81766d2ea57add5bbe
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-07-29 19:35:50 +0100

    Update tests

--- test/FeaturesSpec.scala
+++ test/FeaturesSpec.scala
@@ -1,4 +1,4 @@
-import model.{FeatureSwitch, FeatureSwitches, InlineForm }
+import model.{FeatureSwitch, FeatureSwitches, InlineForm, ObscureFeed }
 import org.scalatest.{Matchers, WordSpec}
 
 class FeaturesSpec extends WordSpec with Matchers {
@@ -9,15 +9,15 @@ class FeaturesSpec extends WordSpec with Matchers {
 
       "return default feature switches when user data contains no pre-existing switches" in {
         val testSwitch = FeatureSwitch("test","test", enabled = true)
-        FeatureSwitches.updateFeatureSwitchesForUser(None, testSwitch) should contain theSameElementsAs List(InlineForm, testSwitch)
+        FeatureSwitches.updateFeatureSwitchesForUser(None, testSwitch) should contain theSameElementsAs List(testSwitch) ++ FeatureSwitches.all
       }
 
       "return an updated list of feature switches" in {
         val unchangedSwitch = FeatureSwitch("test","test", enabled = true)
         val switchToChange = InlineForm
         val userDataSwitches: Option[List[FeatureSwitch]] = Some(List(switchToChange, unchangedSwitch))
-
-        FeatureSwitches.updateFeatureSwitchesForUser(userDataSwitches, switchToChange) should contain theSameElementsAs List(switchToChange, unchangedSwitch)
+        val remainingSwitches = FeatureSwitches.all diff List(switchToChange)
+        FeatureSwitches.updateFeatureSwitchesForUser(userDataSwitches, switchToChange) should contain theSameElementsAs List(switchToChange, unchangedSwitch) ++ remainingSwitches
       }
 
     }
