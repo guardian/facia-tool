ce200ccfb17f37f918fb0114ed89f1822627c15e
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2019-08-08 16:35:09 +0100

    Change data source for client articlemetadata elements

--- app/model/editions/client/ClientArticleMetadata.scala
+++ app/model/editions/client/ClientArticleMetadata.scala
@@ -79,9 +79,9 @@ object ClientArticleMetadata {
       articleMetadata.byline,
       articleMetadata.sportScore,
 
-      articleMetadata.mediaType.collect{ case MediaType.Hide => true },
+      articleMetadata.mediaType.map(_ == MediaType.Hide),
 
-      articleMetadata.replaceImage.map(_ => mediaType == MediaType.Image),
+      articleMetadata.mediaType.map(_ == MediaType.Image),
       articleMetadata.replaceImage.map(_.src),
       articleMetadata.replaceImage.flatMap(_.height).map(_.toString),
       articleMetadata.replaceImage.flatMap(_.width).map(_.toString),
