e053373f968eb8446672eb8176c3c006ece019d0
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-08-09 14:11:42 +0100

    Add selector for priority

--- client-v2/src/selectors/pathSelectors.ts
+++ client-v2/src/selectors/pathSelectors.ts
@@ -1,14 +1,14 @@
 import { State } from 'types/State';
-import { matchIssuePath } from 'routes/routes';
+import { matchIssuePath, matchFrontsEditPath } from 'routes/routes';
 import urls from 'constants/urls';
 import { EditMode } from 'types/EditMode';
 
 const matchRootPath = new RegExp(`^\/${urls.appRoot}`);
 const maybeRemoveV2Prefix = (path: string) => path.replace(matchRootPath, '');
-
 const selectFullPath = (state: State) => state.path;
 const selectV2SubPath = (state: State) =>
   maybeRemoveV2Prefix(selectFullPath(state));
+
 const selectEditMode = (state: State): EditMode => {
   if (!!matchIssuePath(selectV2SubPath(state))) {
     return 'editions';
@@ -17,4 +17,13 @@ const selectEditMode = (state: State): EditMode => {
   }
 };
 
-export { selectFullPath, selectV2SubPath, selectEditMode };
+const selectPriority = (state: State) => {
+  const path = selectV2SubPath(state);
+  const match = matchFrontsEditPath(path) || matchIssuePath(path);
+  if (!match || !match.params.priority) {
+    return;
+  }
+  return match.params.priority;
+};
+
+export { selectFullPath, selectV2SubPath, selectEditMode, selectPriority };
