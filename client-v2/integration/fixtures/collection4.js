e777acbc61b6a74c5ff8103cac7cc6481aa2586a
Anna Leach <anna.leach@theguardian.com>
2019-08-14 17:49:48 +0100

    Fix routing and hide on config/breakingnews

---
+++ client-v2/integration/fixtures/collection4.js
@@ -0,0 +1,31 @@
+module.exports = {
+  live: [],
+  draft: [
+    {
+      id: 'internal-code/page/2309422',
+      frontPublicationDate: 1540379258808,
+      meta: {
+        trailText:
+          'Social media gives people a megaphone. But if companies keep giving that megaphone to trolls intending to hurt people, they start to look complicit',
+        headline: 'Test3',
+        isBreaking: false,
+        isBoosted: false,
+        imageHide: false,
+        showKickerCustom: false,
+        showByline: false,
+        showQuotedHeadline: false,
+        imageSlideshowReplace: false,
+        supporting: [],
+        byline: 'Jess Zimmerman',
+        imageReplace: false,
+        imageCutoutReplace: false,
+        showLargeHeadline: false,
+        customKicker: ''
+      }
+    }
+  ],
+  lastUpdated: 1540558153650,
+  updatedBy: 'Richard Beddington',
+  updatedEmail: 'richard.beddington@guardian.co.uk',
+  displayName: 'Second Collection 2'
+};
