1bc6fcadcbd8df331c0b12b5ecc1c74bbe943b16
Akash Askoolum <akash1810@gmail.com>
2019-08-23 16:08:15 +0100

    improving play evolution and adding unit tests

--- conf/evolutions/default/7.sql
+++ conf/evolutions/default/7.sql
@@ -1,7 +1,8 @@
 # --- !Ups
 
 ALTER TABLE collections ADD COLUMN path_type TEXT;
-UPDATE  collections  SET path_type = 'print-sent' where prefill IS NOT NULL;
+UPDATE  collections  SET path_type = 'printSent' where prefill IS NOT NULL;
+UPDATE  collections  SET path_type = 'search' where prefill = '?tag=type/crossword';
 
 # --- !Downs
 
