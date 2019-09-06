ccae9cfbe5451a6d401c2c577b0fc295f87b127c
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-09-06 18:34:38 +0100

    Add tests for new selectors

--- client-v2/src/selectors/__tests__/collectionSelectors.spec.ts
+++ client-v2/src/selectors/__tests__/collectionSelectors.spec.ts
@@ -1,7 +1,4 @@
-import {
-  selectIsCollectionLocked,
-  createSelectCollectionsInOpenFronts
-} from 'selectors/collectionSelectors';
+import { selectIsCollectionLocked } from 'selectors/collectionSelectors';
 import { frontsConfig } from 'fixtures/frontsConfig';
 
 describe('Validating Front Collection configuration metadata', () => {
@@ -26,50 +23,5 @@ describe('Validating Front Collection configuration metadata', () => {
         'collection2'
       )
     ).toEqual(false);
-  });
-});
-
-describe('Selecting collections on all open Fronts', () => {
-  const selectCollectionsInOpenFronts = createSelectCollectionsInOpenFronts();
-  it('return correct collections for one open Front', () => {
-    expect(
-      selectCollectionsInOpenFronts({
-        fronts: {
-          frontsConfig
-        },
-        editor: {
-          frontIdsByPriority: { editorial: ['editorialFront'] }
-        },
-        path: 'v2/editorial'
-      } as any)
-    ).toEqual(['collection1']);
-  });
-  it('return correct collections for multiple open Fronts', () => {
-    expect(
-      selectCollectionsInOpenFronts({
-        fronts: {
-          frontsConfig
-        },
-        editor: {
-          frontIdsByPriority: {
-            editorial: ['editorialFront', 'editorialFront2']
-          }
-        },
-        path: 'v2/editorial'
-      } as any)
-    ).toEqual(['collection1', 'collection6']);
-  });
-  it('return enpty array for no open Fronts', () => {
-    expect(
-      selectCollectionsInOpenFronts({
-        fronts: {
-          frontsConfig
-        },
-        editor: {
-          frontIdsByPriority: {}
-        },
-        path: 'v2/editorial'
-      } as any)
-    ).toEqual([]);
   });
 });
