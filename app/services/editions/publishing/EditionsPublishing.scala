4c173447aa1b80ca47079c22b1179204e1d74595
Akash Askoolum <akash1810@gmail.com>
2019-09-17 17:19:33 +0100

    logging

--- app/services/editions/publishing/EditionsPublishing.scala
+++ app/services/editions/publishing/EditionsPublishing.scala
@@ -1,11 +1,14 @@
 package services.editions.publishing
 
 import java.time.OffsetDateTime
-import java.time.format.DateTimeFormatter
 
 import com.gu.pandomainauth.model.User
 import model.editions.EditionsIssue
+import net.logstash.logback.marker.Markers
+import play.api.Logger
 import services.editions.db.EditionsDB
+
+import scala.collection.JavaConverters._
 
 class EditionsPublishing(publishedBucket: EditionsBucket, previewBucket: EditionsBucket, db: EditionsDB) {
 
@@ -17,6 +20,17 @@ class EditionsPublishing(publishedBucket: EditionsBucket, previewBucket: Edition
   def publish(issue: EditionsIssue, user: User, now: OffsetDateTime) = {
     // Bump the recently published counters
     val publicationEventId = db.publishIssue(issue.id, user, now)
+
+    val markers = Markers.appendEntries(
+      Map (
+        "issue-id" -> issue.id,
+        "issue-date" -> issue.issueDate,
+        "publication-event-id" -> publicationEventId,
+        "user" -> user.email
+      ).asJava
+    )
+
+    Logger.info(s"Publishing issue ${issue.id}")(markers)
 
     val publishedIssue = issue.toPublishedIssue(Some(publicationEventId))
 
