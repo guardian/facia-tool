f14d963c2b26c18161759d4c8acb5567aa3deeb1
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-09-18 13:50:41 +0100

    Add selector to detect stale external articles

--- client-v2/src/shared/bundles/externalArticlesBundle.ts
+++ client-v2/src/shared/bundles/externalArticlesBundle.ts
@@ -1,5 +1,9 @@
+import isAfter from 'date-fns/is_after'
+import isValid from 'date-fns/is_valid';
 import createAsyncResourceBundle from 'lib/createAsyncResourceBundle';
 import { ExternalArticle } from 'shared/types/ExternalArticle';
+import { State } from 'shared/reducers/sharedReducer';
+
 
 export const {
   actions,
@@ -10,3 +14,20 @@ export const {
 } = createAsyncResourceBundle<ExternalArticle>('externalArticles', {
   indexById: true
 });
+
+/**
+ * Is the external article last modified field older than the given date?
+ * This function is conservative -- if either date is invalid/missing, it returns `true`.
+ */
+export const selectIsExternalArticleStale = (state: State, id: string, dateStr: string) => {
+  const article = selectors.selectById(state, id);
+  if (!article || !article.fields.lastModified) {
+    return true;
+  }
+  const articleDate = new Date(article.fields.lastModified);
+  const incomingDate = new Date(dateStr);
+  if (!isValid(articleDate) || !isValid(incomingDate)) {
+    return true;
+  }
+  return isAfter(articleDate, incomingDate);
+}
