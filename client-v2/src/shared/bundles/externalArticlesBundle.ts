2d86a4fa948f7c17bd22b14252d5f695bad92a58
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-09-20 16:59:31 +0100

    Modify stale selector to work across different bundles

--- client-v2/src/shared/bundles/externalArticlesBundle.ts
+++ client-v2/src/shared/bundles/externalArticlesBundle.ts
@@ -1,8 +1,7 @@
-import isAfter from 'date-fns/is_after';
-import isValid from 'date-fns/is_valid';
 import createAsyncResourceBundle from 'lib/createAsyncResourceBundle';
 import { ExternalArticle } from 'shared/types/ExternalArticle';
-import { State } from 'shared/reducers/sharedReducer';
+
+import { createSelectIsArticleStale } from 'shared/util/externalArticle';
 
 export const {
   actions,
@@ -14,23 +13,4 @@ export const {
   indexById: true
 });
 
-/**
- * Is the external article last modified field older than the given date?
- * This function is liberal in what it accepts -- if either date is invalid/missing, it returns `true`.
- */
-export const selectIsExternalArticleStale = (
-  state: State,
-  id: string,
-  dateStr: string | undefined
-) => {
-  const article = selectors.selectById(state, id);
-  if (!article || !article.fields.lastModified || !dateStr) {
-    return true;
-  }
-  const articleDate = new Date(article.fields.lastModified);
-  const incomingDate = new Date(dateStr);
-  if (!isValid(articleDate) || !isValid(incomingDate)) {
-    return true;
-  }
-  return isAfter(incomingDate, articleDate);
-};
+export const selectIsExternalArticleStale = createSelectIsArticleStale(selectors.selectById);
