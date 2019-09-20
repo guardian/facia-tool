8592b18cc2c2911cf1d467391aad3530d9b9c9eb
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-09-20 17:00:42 +0100

    Add component to force periodic refreshes

---
+++ client-v2/src/components/util/RefreshPeriodically.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+const refreshPeriodically = ({
+  timeMs,
+  children: Child
+}: {
+  timeMs: number;
+  children: React.ComponentType;
+}) => {
+  const [count, setCount] = useState(0);
+  useEffect(
+    () => {
+      const interval = setInterval(() => {
+        setCount(count + 1);
+      }, timeMs);
+      return () => clearInterval(interval);
+    },
+    [count]
+  );
+
+  return <Child />
+};
+
+export default refreshPeriodically;
