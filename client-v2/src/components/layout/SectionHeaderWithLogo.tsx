3ad5d200aa881833224d7b80922b07761c180b86
Akash Askoolum <akash1810@gmail.com>
2019-09-23 16:42:46 +0100

    The logo takes you home!

--- client-v2/src/components/layout/SectionHeaderWithLogo.tsx
+++ client-v2/src/components/layout/SectionHeaderWithLogo.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { styled, theme } from 'constants/theme';
 import { SectionHeaderUnpadded } from './SectionHeader';
+import { Link } from 'react-router-dom';
 
 const SectionHeader = styled(SectionHeaderUnpadded)`
   display: flex;
 `;
 
-const LogoTypeContainer = styled.div`
+const LogoTypeContainer = styled(Link)`
   background-color: ${theme.shared.colors.blackDark};
   display: inline-block;
   text-align: center;
   height: 60px;
   width: 60px;
   line-height: 60px;
+  color: white;
+  text-decoration: none;
 `;
 
 export default ({
@@ -25,7 +28,7 @@ export default ({
   greyHeader?: boolean;
 }) => (
   <SectionHeader greyHeader={greyHeader} includeBorder={includeBorder}>
-    <LogoTypeContainer>F</LogoTypeContainer>
+    <LogoTypeContainer to="/">F</LogoTypeContainer>
     {children}
   </SectionHeader>
 );
