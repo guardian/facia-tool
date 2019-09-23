c149419101a8df3edc76a7c951378df5b76ce6ee
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-09-23 08:41:40 +0100

    Reset poll interval

--- client-v2/src/constants/polling.ts
+++ client-v2/src/constants/polling.ts
@@ -1,4 +1,4 @@
 export const collectionsPollInterval = 10000;
 export const collectionArticlesPollInterval = 30000;
-export const feedArticlesPollInterval = 3000;
+export const feedArticlesPollInterval = 30000;
 export const ophanPollInterval = 30000;
