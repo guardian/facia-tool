7a49347fa5714dd9e383f1b202c6b9ae5fe78ceb
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2019-09-24 13:55:36 +0100

    lint

--- client-v2/src/components/Home.tsx
+++ client-v2/src/components/Home.tsx
@@ -11,7 +11,9 @@ const renderPriority = (priority: string) => (
 );
 const renderEditionPriority = (editionPriority: string) => (
   <li key={editionPriority}>
-    <Link to={`/manage-editions/${editionPriorities[editionPriority].address}`}>{editionPriorities[editionPriority].description}</Link>
+    <Link to={`/manage-editions/${editionPriorities[editionPriority].address}`}>
+      {editionPriorities[editionPriority].description}
+    </Link>
   </li>
 );
 const Home = () => (
