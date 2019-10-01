9fd40c0a6dde8be2528d1a849d86f7fb943dcf94
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2019-10-01 15:50:20 +0100

    Remove Australian and American editions pending a different PR

--- app/model/editions/EditionsTemplates.scala
+++ app/model/editions/EditionsTemplates.scala
@@ -6,15 +6,13 @@ import java.time.{LocalDate, ZoneId}
 import enumeratum.EnumEntry.Uncapitalised
 import enumeratum.{EnumEntry, PlayEnum}
 import model.editions.PathType.{PrintSent, Search}
-import model.editions.templates.{AmericanEdition, AustralianEdition, DailyEdition, TrainingEdition}
+import model.editions.templates.{DailyEdition, TrainingEdition}
 import org.postgresql.util.PGobject
 import play.api.libs.json.Json
 
 object EditionsTemplates {
   val templates: Map[String, EditionTemplate] = Map(
     "daily-edition" -> DailyEdition.template,
-    "american-edition" -> AmericanEdition.template,
-    "australian-edition" -> AustralianEdition.template,
     "training-edition" -> TrainingEdition.template
   )
 
