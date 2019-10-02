87d0cddc38efd7c998119a3e7ce3d9c91e2ac5e7
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2019-10-02 09:28:24 +0100

    Add client side references to non-uk editions

--- client-v2/src/constants/priorities.ts
+++ client-v2/src/constants/priorities.ts
@@ -1,8 +1,23 @@
-import { Priorities } from 'types/Priority';
+import { EditionPriority, Priorities } from 'types/Priority';
 
 export const priorities: Priorities = {
   editorial: {},
   commercial: {},
   training: {},
   email: {}
+};
+
+export const editionPriorities: { [index: string]: EditionPriority } = {
+  dailyEdition: {
+    description: 'Daily Edition',
+    address: 'daily-edition'
+  },
+  americanEdition: {
+    description: 'American Edition',
+    address: 'american-edition'
+  },
+  australianEdition: {
+    description: 'Australian Edition',
+    address: 'australian-edition'
+  }
 };
