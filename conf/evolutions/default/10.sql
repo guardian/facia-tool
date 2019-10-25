617364b7fb4d1f5f44978f40260ece5542ed4201
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2019-10-25 10:33:21 +0100

    refining sql migration script

---
+++ conf/evolutions/default/10.sql
@@ -0,0 +1,12 @@
+# --- !Ups
+
+-- refining changes from 9.sql script
+-- it turns out time concat with 00:00:00.00 UTC time is more appropriate in actual environments
+
+UPDATE  collections  SET content_prefill_window_start = CONCAT(cast((select issue_date from edition_issues where edition_issues.id = (select issue_id from fronts where fronts.id = front_id)) as text), ' 00:00:00.00 UTC')::TIMESTAMP where prefill IS NOT NULL;
+UPDATE  collections  SET content_prefill_window_end = CONCAT(cast((select issue_date from edition_issues where edition_issues.id = (select issue_id from fronts where fronts.id = front_id)) as text), ' 00:00:00.00 UTC')::TIMESTAMP where prefill IS NOT NULL;
+
+# --- !Downs
+
+ALTER TABLE collections DROP COLUMN content_prefill_window_start;
+ALTER TABLE collections DROP COLUMN content_prefill_window_end;
