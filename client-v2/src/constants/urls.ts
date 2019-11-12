cedbe21d2fffd12dbeb23cde1aa18a4f61ab5864
Akash Askoolum <akash1810@gmail.com>
2019-11-12 17:51:07 +0000

    improve styling of cover card field
    - text positioned below fields
    - field labels
    - link to card builder tool

--- client-v2/src/constants/urls.ts
+++ client-v2/src/constants/urls.ts
@@ -2,5 +2,6 @@ export default {
   capiLiveUrl: '/api/live',
   capiPreviewUrl: '/api/preview',
   manageEditions: '/manage-editions/daily-edition',
-  appRoot: 'v2'
+  appRoot: 'v2',
+  editionsCardBuilder: 'https://editions-card-builder.gutools.co.uk'
 };
