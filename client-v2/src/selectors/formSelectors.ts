65a70e2595458e52c5ccf682f28291fac8f51c13
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-11-18 21:30:00 +0000

    Lint

--- client-v2/src/selectors/formSelectors.ts
+++ client-v2/src/selectors/formSelectors.ts
@@ -42,8 +42,8 @@ export const htmlSnapFields = [
   'headline',
   'imageHide',
   'imageReplace',
-  'primaryImage',
-]
+  'primaryImage'
+];
 
 export const emailFieldsToExclude = [
   'isBreaking',
