9c056acd51f47ec660001e5e86040d99f26909b2
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2019-11-22 14:13:14 +0000

    Lint

--- client-v2/src/util/delete.ts
+++ client-v2/src/util/delete.ts
@@ -2,9 +2,9 @@ export const deleteIssue = () => {
    const issue=window.location.href.substring(window.location.href.lastIndexOf('/') + 1)
    const result = window.confirm((`Are you sure you wish to delete issue ${issue}? This cannot be undone.`))
    if (result) {
-      fetch("/editions-api/issues/" + issue , {"credentials": "include", "method": "DELETE"});
-      return "Deleted"
+      fetch(`/editions-api/issues/${issue}` , {'credentials': 'include', 'method': 'DELETE'});
+      return 'Deleted'
    } else {
-      return "Cancelled"
+      return 'Cancelled'
    }
 }
