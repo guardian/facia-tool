305078fac4dc59e4e94c6d0589ff091e4e1be243
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2019-12-17 12:09:36 +0000

    Link back to correct edition for date-picker

--- client-v2/src/constants/urls.ts
+++ client-v2/src/constants/urls.ts
@@ -1,7 +1,7 @@
 export default {
   capiLiveUrl: '/api/live',
   capiPreviewUrl: '/api/preview',
-  manageEditions: '/manage-editions/daily-edition',
+  manageEditions: '/manage-editions/',
   appRoot: 'v2',
   editionsCardBuilder: 'https://editions-card-builder.gutools.co.uk'
 };
