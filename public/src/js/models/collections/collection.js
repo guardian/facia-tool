90d3f8c981ec4fcabe18db43f7b8693f3be38ba7
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-01-15 13:29:42 +0000

    Add error styling for breaking news

--- public/src/js/models/collections/collection.js
+++ public/src/js/models/collections/collection.js
@@ -268,10 +268,11 @@ export default class Collection extends BaseClass {
             reportErrors(new Error(message)); //report to sentry
         })
         .catch(() => {
+            const breakingNewsMsg = 'breaking news alert. The alert has not been sent. Please contact Central Production.';
             const isBreakingNewsAlert = this.front.confirmSendingAlert();
-            const message = `Failed ${action}ing the ${isBreakingNewsAlert ? 'breaking news alert' : 'collection'}`;
+            const message = `Failed ${action}ing the ${isBreakingNewsAlert ? breakingNewsMsg : 'collection'}`;
             this.setPending(false);
-            alert(message);
+            alert(message, 'error');
         });
     }
 
