57089a3437f95bbdd45cc5620e30a8705457416e
David Blishen <david.blishen@guardian.co.uk>
2020-02-14 12:39:31 +0000

    First attempt at prefills

--- app/model/editions/templates/AmericanEdition.scala
+++ app/model/editions/templates/AmericanEdition.scala
@@ -60,9 +60,18 @@ object AmericanEdition {
 
   def FrontPeopleUs = front(
     "People",
-    collection("Interviews"),
-    collection("Profiles"),
+    collection("Interviews")
+      .searchPrefill("?tag=type/article,tone/interview")
+      .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-5, 0)))
+      .withArticleItemsCap(40),
+    collection("Profiles")
+      .searchPrefill("?tag=type/article,tone/profiles")
+      .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-5, 0)))
+      .withArticleItemsCap(40),
     collection("Q&A")
+      .searchPrefill("?tag=type/article,tone/q-and-as")
+      .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-5, 0)))
+      .withArticleItemsCap(40)
   )
     .swatch(Lifestyle)
   
@@ -94,13 +103,13 @@ object AmericanEdition {
   def FrontUsNewsUs = front(
     "US News",
     collection("News Features"),
-     // .searchPrefill("?tag=type/article,(world/world|us-news/us-news|uk/uk|world/europe-news|world/africa|world/americas|world/asia-pacific|world/middleeast),(tone/features|tone/analysis|tone/explainer),-(australia-news/australia-news|australia-news/australian-politics|australia-news/business-australia|media/australia-media),-tone/news,-culture/culture,-lifestyle/lifestyle,-tone/minutebyminute")
-     // .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-5, 0)))
-     // .withArticleItemsCap(40),
+      .searchPrefill("?tag=type/article,(us-news/us-news|us-news/us-politics|business/business),(tone/features|tone/analysis|tone/explainer),-culture/culture,-lifestyle/lifestyle,-tone/news,-tone/comment,-tone/minutebyminute")
+      .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-5, 0)))
+      .withArticleItemsCap(40),
     collection("News")
-     // .searchPrefill("?tag=type/article,(world/world|us-news/us-news|uk/uk|world/europe-news|world/africa|world/americas|world/asia-pacific|world/middleeast),-(tone/features|tone/analysis|tone/explainer),-(australia-news/australia-news|australia-news/australian-politics|australia-news/business-australia|media/australia-media),tone/news,-culture/culture,-lifestyle/lifestyle,-tone/minutebyminute")
-     // .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-3, 0)))
-     // .withArticleItemsCap(40)    
+      .searchPrefill("?tag=type/article,(us-news/us-news|us-news/us-politics|business/business),-(tone/features|tone/analysis|tone/explainer),-culture/culture,-lifestyle/lifestyle,tone/news,-tone/comment,-tone/minutebyminute")
+      .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-5, 0)))
+      .withArticleItemsCap(40)    
   )
     .swatch(News)
   
@@ -112,14 +121,14 @@ object AmericanEdition {
 
   def FrontWorldNewsUs = front(
     "World News",
-    collection("News Features"),
-     // .searchPrefill("?tag=type/article,(world/world|us-news/us-news|uk/uk|world/europe-news|world/africa|world/americas|world/asia-pacific|world/middleeast),(tone/features|tone/analysis|tone/explainer),-(australia-news/australia-news|australia-news/australian-politics|australia-news/business-australia|media/australia-media),-tone/news,-culture/culture,-lifestyle/lifestyle,-tone/minutebyminute")
-     // .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-5, 0)))
-     // .withArticleItemsCap(40),
+    collection("News Features")
+      .searchPrefill("?tag=type/article,(world/world|australia-news/australia-news|uk/uk|world/europe-news|world/africa|world/americas|world/asia-pacific|world/middleeast),(tone/features|tone/analysis|tone/explainer),-(us-news/us-news|us-news/us-politics|business/business),-tone/news,-culture/culture,-lifestyle/lifestyle,-tone/minutebyminute")
+      .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-5, 0)))
+      .withArticleItemsCap(40),
     collection("News")
-     // .searchPrefill("?tag=type/article,(world/world|us-news/us-news|uk/uk|world/europe-news|world/africa|world/americas|world/asia-pacific|world/middleeast),-(tone/features|tone/analysis|tone/explainer),-(australia-news/australia-news|australia-news/australian-politics|australia-news/business-australia|media/australia-media),tone/news,-culture/culture,-lifestyle/lifestyle,-tone/minutebyminute")
-     // .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-3, 0)))
-     // .withArticleItemsCap(40)    
+      .searchPrefill("?tag=type/article,(world/world|australia-news/australia-news|uk/uk|world/europe-news|world/africa|world/americas|world/asia-pacific|world/middleeast),-(tone/features|tone/analysis|tone/explainer),-(us-news/us-news|us-news/us-politics|business/business),tone/news,-culture/culture,-lifestyle/lifestyle,-tone/minutebyminute")
+      .withTimeWindowConfig(Some(CapiTimeWindowConfigInDays(-3, 0)))
+      .withArticleItemsCap(40)    
   )
     .swatch(News)
 
@@ -128,8 +137,8 @@ object AmericanEdition {
   def FrontOpinionUs = front(
     "Opinion",
     collection("Opinion")
-     // .searchPrefill("?tag=type/article,culture/culture,(tone/features|tone/reviews|tone/interview),-tone/news,-tone/minutebyminute")
-     // .withArticleItemsCap(40),
+      .searchPrefill("?tag=type/article,tone/comment,(us-news/us-news|us-news/us-politics|business/business),-sport/sport,-tone/minutebyminute")
+      .withArticleItemsCap(40)
   )
     .swatch(Opinion)
   
@@ -138,8 +147,8 @@ object AmericanEdition {
   def FrontEnvironmentUs = front(
     "Environment",
     collection("Environment")
-     // .searchPrefill("?tag=type/article,culture/culture,(tone/features|tone/reviews|tone/interview),-tone/news,-tone/minutebyminute")
-     // .withArticleItemsCap(40),
+      .searchPrefill("?tag=type/article,(us-news/us-news,environment/environment),-sport/sport,-tone/minutebyminute")
+      .withArticleItemsCap(40)
   )
     .swatch(News)
   
@@ -148,8 +157,8 @@ object AmericanEdition {
   def FrontCultureUs = front(
     "Culture",
     collection("Culture")
-     // .searchPrefill("?tag=type/article,culture/culture,(tone/features|tone/reviews|tone/interview),-tone/news,-tone/minutebyminute")
-     // .withArticleItemsCap(40),
+      .searchPrefill("?tag=type/article,culture/culture,(tone/features|tone/reviews),-tone/news,-tone/minutebyminute")
+      .withArticleItemsCap(40)
   )
     .swatch(News)
 
@@ -158,11 +167,11 @@ object AmericanEdition {
   def FrontLifestyleUs = front(
     "Lifestyle",
     collection("Lifestyle"),
-     // .searchPrefill("?tag=type/article,news/series/the-long-read,-tone/minutebyminute")
-     // .withArticleItemsCap(40),
+      .searchPrefill("?tag=type/article,lifeandstyle/lifeandstyle,-tone/minutebyminute")
+      .withArticleItemsCap(40),
     collection("Recipes")
-    //  .searchPrefill("?tag=type/article,news/series/the-long-read,-tone/minutebyminute")
-    //  .withArticleItemsCap(40)
+      .searchPrefill("?tag=type/article,tone/recipes,food/food,-tone/minutebyminute")
+      .withArticleItemsCap(40)
   )
     .swatch(News)
 
@@ -171,8 +180,8 @@ object AmericanEdition {
   def FrontSportUs = front(
     "Sport",
     collection("Sport")
-     // .searchPrefill("?tag=type/article,sport/sport,(tone/comment|tone/features|tone/analysis),-tone/minutebyminute")
-     // .withArticleItemsCap(40)
+      .searchPrefill("?tag=type/article,sport/sport,(tone/comment|tone/features|tone/analysis),-tone/minutebyminute")
+      .withArticleItemsCap(40)
   )
     .swatch(Sport)
 
