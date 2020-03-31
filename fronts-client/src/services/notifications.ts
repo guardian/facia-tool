ef97e4812f9b6e2b8368cdac46260c492760b9bf
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-03-31 11:06:25 +0100

    Add notifications event bus

---
+++ fronts-client/src/services/notifications.ts
@@ -0,0 +1,28 @@
+import { Notification } from 'bundles/notificationsBundle';
+
+type Subscriber = (notification: Notification) => void;
+
+/**
+ * An singleton event bus to decouple parts of the app that need to
+ * create notifications from the store.
+ *
+ * This is necessary because importing the store directly results in
+ * circular dependencies.
+ */
+class NotificationService {
+  private subscribers = [] as Subscriber[];
+
+  public subscribe(subscriber: Subscriber) {
+    this.subscribers.push(subscriber);
+  }
+
+  public unsubscribe(subscriber: Subscriber) {
+    this.subscribers = this.subscribers.filter((_) => _ !== subscriber);
+  }
+
+  public notify(notification: Notification) {
+    this.subscribers.forEach((sub) => sub(notification));
+  }
+}
+
+export default new NotificationService();
