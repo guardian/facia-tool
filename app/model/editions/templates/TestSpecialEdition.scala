cd452e4669d3529b890b1fee7cfcb1c8efd47f71
David Blishen <david.blishen@guardian.co.uk>
2020-08-18 14:45:03 +0100

    Create TestSpecialEdition.scala
    A prototype special edition. Want to test what happens with the absence of `expiry` and `image`

---
+++ app/model/editions/templates/TestSpecialEdition.scala
@@ -0,0 +1,45 @@
+package model.editions.templates
+
+import java.time.ZoneId
+
+import model.editions.Swatch._
+import model.editions._
+import model.editions.templates.TemplateHelpers._
+
+//noinspection TypeAnnotation
+object TrainingEdition extends EditionDefinitionWithTemplate {
+  override val title = "Specia Edition Test"
+  override val subTitle = "Demonstration of special edition"
+  override val edition = "special-edition-test"
+  override val header = Header("Special", "Edition Test")
+  override val editionType = EditionType.Special
+  //override val expiry = ???
+  //override val image = ???
+
+  lazy val template = EditionTemplate(
+    List(
+      Front1 -> Daily(),
+      Front2 -> Daily(),
+      Front3 -> Daily()
+    )
+  )
+
+  def Front1 = front("Front 1", None,
+    collection("Collection 1"),
+    collection("Collection 2"),
+    collection("Collection 3")
+  ).swatch(News)
+
+  def Front1 = front("Front 2", None,
+    collection("Collection 1"),
+    collection("Collection 2"),
+    collection("Collection 3")
+  ).swatch(Culture)
+  
+  def Front3 = front("Front 3", None,
+    collection("Collection 1"),
+    collection("Collection 2"),
+    collection("Collection 3")
+  ).swatch(Sport)
+
+}
