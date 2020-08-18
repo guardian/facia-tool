62e0d088bf637ce943a816b22daa38d11c5313de
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-08-18 15:19:15 +0100

    Add missing fields

--- app/model/editions/templates/TestSpecialEdition.scala
+++ app/model/editions/templates/TestSpecialEdition.scala
@@ -8,11 +8,13 @@ import model.editions.templates.TemplateHelpers._
 
 //noinspection TypeAnnotation
 object TestSpecialEdition extends EditionDefinitionWithTemplate {
-  override val title = "Specia Edition Test"
+  override val title = "Special Edition Test"
   override val subTitle = "Demonstration of special edition"
   override val edition = "special-edition-test"
-  override val header = Header("Special", "Edition Test")
+  override val header = Header(title ="Special", subTitle=Some("Edition Test"))
   override val editionType = EditionType.Special
+  override val notificationUTCOffset = 3
+  override val topic = "s-e-t"
   //override val expiry = ???
   //override val image = ???
 
@@ -21,7 +23,16 @@ object TestSpecialEdition extends EditionDefinitionWithTemplate {
       Front1 -> Daily(),
       Front2 -> Daily(),
       Front3 -> Daily()
-    )
+    ),
+    timeWindowConfig = CapiTimeWindowConfigInDays(
+      startOffset = 0,
+      endOffset = 0,
+    ),
+    capiDateQueryParam = CapiDateQueryParam.Published,
+    zoneId = ZoneId.of("Europe/London"),
+    availability = Daily(),
+    maybeOphanPath = None,
+    ophanQueryPrefillParams = None
   )
 
   def Front1 = front("Front 1", None,
@@ -30,12 +41,12 @@ object TestSpecialEdition extends EditionDefinitionWithTemplate {
     collection("Collection 3")
   ).swatch(News)
 
-  def Front1 = front("Front 2", None,
+  def Front2 = front("Front 2", None,
     collection("Collection 1"),
     collection("Collection 2"),
     collection("Collection 3")
   ).swatch(Culture)
-  
+
   def Front3 = front("Front 3", None,
     collection("Collection 1"),
     collection("Collection 2"),
