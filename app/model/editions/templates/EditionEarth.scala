13b3885512d64e7bac59bf9e95b2559f14f8838a
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-09-22 18:04:39 +0100

    Update button image

--- app/model/editions/templates/EditionEarth.scala
+++ app/model/editions/templates/EditionEarth.scala
@@ -15,7 +15,7 @@ object EditionEarth extends SpecialEdition {
   override val header = Header(title ="Edition", subTitle=Some("Earth"))
   override val notificationUTCOffset = 3
   override val topic = "e-e"
-  override val buttonImageUri = Some("https://i.guim.co.uk/img/media/23e97e00b2b0c3277ea6ff8a38068709509d92da/0_0_931_934/931.png?width=120&quality=90&s=9f1e7ebe4b62e4cc5a7ffad05e92306c")
+  override val buttonImageUri = Some("https://i.guim.co.uk/img/media/23e97e00b2b0c3277ea6ff8a38068709509d92da/0_0_931_934/931.png?width=134&quality=95&s=5bf4d7e4ea9abd51017379b2dacb1e79")
   override val expiry: Option[String] = Some(
     new DateTime(2020, 11,7,23,59,DateTimeZone.UTC).toString()
   )
@@ -25,7 +25,7 @@ object EditionEarth extends SpecialEdition {
       title = EditionTextFormatting(color = "#121212", font="GHGuardianHeadline-Light", lineHeight = 34, size = 34),
       subTitle = EditionTextFormatting(color = "#121212", font="GuardianTextSans-Bold", lineHeight = 20, size = 17),
       expiry = EditionTextFormatting(color = "#121212", font="GuardianTextSans-Regular", lineHeight = 16, size = 15),
-      image = EditionImageStyle(87,134)
+      image = EditionImageStyle(134,134)
     )
   )
   override val headerStyle: Option[SpecialEditionHeaderStyles] = Some(
