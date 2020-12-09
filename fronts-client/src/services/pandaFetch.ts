6842dfba1fdcc1958ff63b88cd364582938997f1
Tom Richards <tom.richards@guardian.co.uk>
2020-12-09 17:54:03 +0000

    Revert "Revert "fix stale writes by locking collections when auth/connection issue discovered""

--- fronts-client/src/services/pandaFetch.ts
+++ fronts-client/src/services/pandaFetch.ts
@@ -1,8 +1,12 @@
 import { reEstablishSession } from 'panda-session';
 import notifications from './notifications';
+import { isError } from 'tslint/lib/error';
+import Raven from 'raven-js';
 
 const reauthUrl = '/login/status';
-const reauthErrorMessage = `We couldn't log you back in. Your changes may not be saved. Please  <a href="${window.location.href}" target="_self">log in again</a> to reauthenticate.`;
+const reauthErrorMessage =
+  "We couldn't log you back in. Your changes may not be saved. " +
+  `Please <a href="${window.location.href}" target="_self">log in again</a> to reauthenticate.`;
 
 /**
  * Make a fetch request with Panda authentication.
@@ -16,26 +20,59 @@ const pandaFetch = (
   count: number = 0
 ): Promise<Response> =>
   new Promise(
-    async (resolve: (r: Response) => any, reject: (r: Response) => any) => {
-      const res = await fetch(url, {
-        ...options,
-        credentials: 'same-origin',
-      });
+    async (resolve: (r: Response) => any, reject: (e: any) => any) => {
+      try {
+        const res = await fetch(url, {
+          ...options,
+          credentials: 'same-origin',
+        });
 
-      if ((res.status === 419 || res.status === 401) && count < 1) {
-        try {
-          await reEstablishSession(reauthUrl, 5000);
-          const res2 = await pandaFetch(url, options, count + 1);
-          return resolve(res2);
-        } catch (e) {
-          notifications.notify({ message: reauthErrorMessage, level: 'error' });
-          return reject(e);
+        if ((res.status === 419 || res.status === 401) && count < 1) {
+          try {
+            await reEstablishSession(reauthUrl, 5000);
+            const res2 = await pandaFetch(url, options, count + 1);
+            return resolve(res2);
+          } catch (e) {
+            notifications.notify({
+              message: reauthErrorMessage,
+              level: 'error',
+            });
+            Raven.captureException(e, {
+              extra: {
+                message: `'Auth issue' banner presented to user`,
+              },
+            });
+            return reject(
+              isError(e) ? `Auth Issue (${e ? e.toString() : ''})` : e
+            );
+          }
+        } else if (res.status < 200 || res.status >= 300) {
+          notifications.notify({
+            message:
+              'Request failed. Your changes may not be saved. Please wait or reload the page.',
+            level: 'error',
+          });
+          Raven.captureException(
+            `'Request failed' banner presented to user, because... ${res.status} ${res.status}`,
+            { extra: res }
+          );
+          return reject(res);
         }
-      } else if (res.status < 200 || res.status >= 300) {
-        return reject(res);
-      }
 
-      return resolve(res);
+        return resolve(res);
+      } catch (error) {
+        notifications.notify({
+          message:
+            'Connection issue occurred. Your changes may not be saved. Please wait or reload the page.',
+          level: 'error',
+        });
+        Raven.captureException(error, {
+          extra: {
+            message: `'Connection issue' banner presented to user`,
+          },
+        });
+        return reject(`Connection Issue (${error ? error.toString() : ''})`);
+      }
     }
   );
 
