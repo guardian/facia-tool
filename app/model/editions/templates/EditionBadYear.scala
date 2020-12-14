7b02702ec73cdafc8066965a63f4e5d8ca929f49
David Blishen <david.blishen@guardian.co.uk>
2020-12-14 11:43:43 +0000

    Update EditionBadYear.scala
    Fix the text colour, so black not white.

--- app/model/editions/templates/EditionBadYear.scala
+++ app/model/editions/templates/EditionBadYear.scala
@@ -22,17 +22,17 @@ object EditionBadYear extends SpecialEdition {
   override val buttonStyle: Option[SpecialEditionButtonStyles] = Some(
     SpecialEditionButtonStyles(
       backgroundColor = "#c1d8fc",
-      title = EditionTextFormatting(color = "#FFFFFF", font="GHGuardianHeadline-Light", lineHeight = 34, size = 34),
-      subTitle = EditionTextFormatting(color = "#FFFFFF", font="GuardianTextSans-Bold", lineHeight = 20, size = 17),
-      expiry = EditionTextFormatting(color = "#FFFFFF", font="GuardianTextSans-Regular", lineHeight = 16, size = 15),
+      title = EditionTextFormatting(color = "#000000", font="GHGuardianHeadline-Light", lineHeight = 34, size = 34),
+      subTitle = EditionTextFormatting(color = "#000000", font="GuardianTextSans-Bold", lineHeight = 20, size = 17),
+      expiry = EditionTextFormatting(color = "#000000", font="GuardianTextSans-Regular", lineHeight = 16, size = 15),
       image = EditionImageStyle(67,134)
     )
   )
   override val headerStyle: Option[SpecialEditionHeaderStyles] = Some(
     SpecialEditionHeaderStyles(
       backgroundColor = "#c1d8fc",
-      textColorPrimary = "#FFFFFF",
-      textColorSecondary = "#FFFFFF"
+      textColorPrimary = "#000000",
+      textColorSecondary = "#000000"
     )
   )
 
