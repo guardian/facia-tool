35369e83e4f310c1802b4c173660b9381d33eab0
David Blishen <david.blishen@guardian.co.uk>
2021-06-02 12:05:53 +0100

    Making a copy of the last special for the next one.

---
+++ app/model/editions/templates/EurosSpecial.scala
@@ -0,0 +1,66 @@
+
+  def Special06 = front("Front6", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(News)
+
+  def Special07 = front("Front7", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Neutral)
+
+  def Special08 = front("Front8", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Culture)
+  
+  def Special09 = front("Front9", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Neutral)
+
+  def Special10 = front("Front10", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(News)
+
+  def Special11 = front("Front11", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Neutral)
+
+  def Special12 = front("Front12", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(News)
+  
+  def Special13 = front("Front13", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Neutral)
+
+}
