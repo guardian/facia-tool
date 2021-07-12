483a95ce176de3f0da1763b51a36d777cef7f6ec
David Blishen <david.blishen@guardian.co.uk>
2021-07-12 13:33:34 +0100

    Basic set up for Olympic Legends special edition

--- app/model/editions/templates/EditionOlympicLegends.scala
+++ app/model/editions/templates/EditionOlympicLegends.scala
@@ -8,11 +8,11 @@ import model.editions.templates.TemplateHelpers._
 import org.joda.time.{DateTime, DateTimeZone}
 
 //noinspection TypeAnnotation
-object EditionEurosSpecial extends SpecialEdition {
-  override val title = "Euros\nspecial"
-  override val subTitle = "A special edition of the Guardian on the European championships to kick off a great summer of sport"
-  override val edition = "edition-euros-special"
-  override val header = Header(title ="Euros", subTitle=Some("special"))
+object EditionOlympicLegends extends SpecialEdition {
+  override val title = "Olympic\nlegends"
+  override val subTitle = "To mark the 32nd Olympiad, our summer of sport special edition looks at 30 of the greatest Olympic moments"
+  override val edition = "edition-olympic-legends"
+  override val header = Header(title ="Olympic", subTitle=Some("legends"))
   override val notificationUTCOffset = 3
   override val topic = "e-es"
   override val buttonImageUri = Some("https://i.guim.co.uk/img/media/c2789bcfc339f8a42f8dbfd6384e910a596b2836/0_0_1000_2000/1000.png?width=80&quality=85&s=5b82671dfe610a22e6ccbfc54cba8895")
@@ -46,14 +46,7 @@ object EditionEurosSpecial extends SpecialEdition {
       Special05 -> Daily(),
       Special06 -> Daily(),
       Special07 -> Daily(),
-      Special08 -> Daily(),
-      Special09 -> Daily(),
-      Special10 -> Daily(),
-      Special11 -> Daily(),
-      Special12 -> Daily(),
-      Special13 -> Daily(),
-      Special14 -> Daily(),
-      Special15 -> Daily()
+      Special08 -> Daily()
       
     ),
     timeWindowConfig = CapiTimeWindowConfigInDays(
@@ -130,61 +123,5 @@ object EditionEurosSpecial extends SpecialEdition {
     collection("Special"),
     collection("Special")
   ).swatch(News)
-  
-  def Special09 = front("Front9", None,
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special")
-  ).swatch(Sport)
-
-  def Special10 = front("Front10", None,
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special")
-  ).swatch(Sport)
-
-  def Special11 = front("Front11", None,
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special")
-  ).swatch(Sport)
-
-  def Special12 = front("Front12", None,
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special")
-  ).swatch(Sport)
-  
-  def Special13 = front("Front13", None,
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special")
-  ).swatch(Sport)
-  
-  def Special14 = front("Front14", None,
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special")
-  ).swatch(Sport)
-  
-  def Special15 = front("Front15", None,
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special"),
-    collection("Special")
-  ).swatch(Sport)
 
 }
