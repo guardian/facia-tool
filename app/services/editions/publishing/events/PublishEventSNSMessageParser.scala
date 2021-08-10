99f4622f2b0fbd27d1c241f9fa50962922f00783
Sam Hession <samhession@live.co.uk>
2021-08-10 10:52:00 +0100

    Includes invalid JSON when logging an issue with parsing

--- app/services/editions/publishing/events/PublishEventSNSMessageParser.scala
+++ app/services/editions/publishing/events/PublishEventSNSMessageParser.scala
@@ -10,19 +10,20 @@ private[events] object PublishEventSNSMessageParser extends Logging {
 
   def parseToEvent(snsNotificationFromSQS: Message): Option[PublishEventMessage] = {
     logger.info("read new publish events")
-    val messageBody = (parse(snsNotificationFromSQS.getBody) \ "Message").validate[String]
-    messageBody match {
+    val messageBody = snsNotificationFromSQS.getBody
+    val validatedMessage = (parse(messageBody) \ "Message").validate[String]
+    validatedMessage match {
       case JsSuccess(value, _) =>
         (parse(value) \ "event").validate[PublishEvent] match {
           case JsSuccess(parsedEvent, _) =>
             val messageID = snsNotificationFromSQS.getReceiptHandle
             Some(PublishEventMessage(messageID, parsedEvent))
           case JsError(errors) =>
-            logger.error(s"errors while parsing SQS message $errors")
+            logger.error(s"errors while parsing SQS message: $value, errors: $errors")
             None
         }
       case JsError(errors) => {
-        logger.error(s"errors while parsing SQS message $errors")
+        logger.error(s"errors while parsing SQS message: $messageBody, errors $errors")
         None
       }
     }
