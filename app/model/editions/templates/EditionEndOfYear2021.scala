e5023fa7ce5c2eeab0d11ad08b7f40a0c89a58e4
David Blishen <david.blishen@guardian.co.uk>
2021-12-10 15:11:43 +0000

    Create EditionEndOfYear2021.scala

---
+++ app/model/editions/templates/EditionEndOfYear2021.scala
@@ -0,0 +1,145 @@
+package model.editions.templates
+
+import java.time.ZoneId
+
+import model.editions.Swatch._
+import model.editions._
+import model.editions.templates.TemplateHelpers._
+import org.joda.time.{DateTime, DateTimeZone}
+
+//noinspection TypeAnnotation
+object EditionEarth extends SpecialEdition {
+  override val title = "Edition Earth"
+  override val subTitle = "Cop26: A special edition of the Guardian's best recent environmental journalism"
+  override val edition = "edition-earth"
+  override val header = Header(title ="Edition", subTitle=Some("Earth"))
+  override val notificationUTCOffset = 3
+  override val topic = "e-e"
+  override val buttonImageUri = Some("https://i.guim.co.uk/img/media/31aee0e66af047a39063131af0889c87aa66151f/0_0_225_450/225.png?width=80&height=160&quality=85&s=00bd9546e8048abc5705f6e4c19dde91")
+  override val expiry: Option[String] = Some(
+    new DateTime(2021, 12,12,23,59,DateTimeZone.UTC).toString()
+  )
+  override val buttonStyle: Option[SpecialEditionButtonStyles] = Some(
+    SpecialEditionButtonStyles(
+      backgroundColor = "#0F70B7",
+      title = EditionTextFormatting(color = "#121212", font="GHGuardianHeadline-Medium", lineHeight = 34, size = 32),
+      subTitle = EditionTextFormatting(color = "#121212", font="GuardianTextSans-Regular", lineHeight = 20, size = 17),
+      expiry = EditionTextFormatting(color = "#121212", font="GuardianTextSans-Regular", lineHeight = 16, size = 15),
+      image = EditionImageStyle(67,134)
+    )
+  )
+  override val headerStyle: Option[SpecialEditionHeaderStyles] = Some(
+    SpecialEditionHeaderStyles(
+      backgroundColor = "#0F70B7",
+      textColorPrimary = "#FFFFFF",
+      textColorSecondary = "#FFFFFF"
+    )
+  )
+
+
+  lazy val template = EditionTemplate(
+    List(
+      Special01 -> Daily(),
+      Special02 -> Daily(),
+      Special03 -> Daily(),
+      Special04 -> Daily(),
+      Special05 -> Daily(),
+      Special06 -> Daily(),
+      Special07 -> Daily(),
+      Special08 -> Daily(),
+      Special09 -> Daily(),
+      Special10 -> Daily()
+    ),
+    timeWindowConfig = CapiTimeWindowConfigInDays(
+      startOffset = 0,
+      endOffset = 0,
+    ),
+    capiDateQueryParam = CapiDateQueryParam.Published,
+    zoneId = ZoneId.of("Europe/London"),
+    availability = Daily(),
+    maybeOphanPath = None,
+    ophanQueryPrefillParams = None
+  )
+
+  def Special01 = front("Introduction", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Sport)
+
+  def Special02 = front("Science", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Culture)
+
+  def Special03 = front("Transport", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Sport)
+
+  def Special04 = front("Health", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Culture)
+
+  def Special05 = front("Food", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Sport)
+
+  def Special06 = front("Fashion", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Culture)
+
+  def Special07 = front("Sport", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Sport)
+
+  def Special08 = front("Business", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Culture)
+
+  def Special09 = front("Awareness", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Sport)
+
+  def Special10 = front("Solutions", None,
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special"),
+    collection("Special")
+  ).swatch(Culture)
+
+
+}
