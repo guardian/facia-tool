d97467ae578f29a12d1a0844459d264bff38a63f
Grahame Oakland <grahame.oakland@guardian.co.uk>
2021-12-20 18:14:30 +0000

    Updated colors and menu icon

--- app/model/editions/templates/EditionEndOfYear.scala
+++ app/model/editions/templates/EditionEndOfYear.scala
@@ -10,18 +10,18 @@ import org.joda.time.{DateTime, DateTimeZone}
 //noinspection TypeAnnotation
 object EditionEndOfYear extends SpecialEdition {
   override val title = "Best of 2021"
-  override val subTitle = "A special edition of the Guardian's best journalism from 2021"
+  override val subTitle = "A lookback at our readers' and editors' favourite pieces of the year"
   override val edition = "edition-end-of-year"
   override val header = Header(title ="Best of", subTitle=Some("2021"))
   override val notificationUTCOffset = 3
   override val topic = "e-eoy"
-  override val buttonImageUri = Some("https://i.guim.co.uk/img/media/31aee0e66af047a39063131af0889c87aa66151f/0_0_225_450/225.png?width=80&height=160&quality=85&s=00bd9546e8048abc5705f6e4c19dde91")
+  override val buttonImageUri = Some("https://media.guim.co.uk/04682faa0ac6d5665ce31f4a7c4bf8d8c0a5e54e/0_0_86_163/86.png?width=86&height=163")
   override val expiry: Option[String] = Some(
     new DateTime(2022, 1,31,23,59,DateTimeZone.UTC).toString()
   )
   override val buttonStyle: Option[SpecialEditionButtonStyles] = Some(
     SpecialEditionButtonStyles(
-      backgroundColor = "#0F70B7",
+      backgroundColor = "#BB3B80",
       title = EditionTextFormatting(color = "#121212", font="GHGuardianHeadline-Medium", lineHeight = 34, size = 32),
       subTitle = EditionTextFormatting(color = "#121212", font="GuardianTextSans-Regular", lineHeight = 20, size = 17),
       expiry = EditionTextFormatting(color = "#121212", font="GuardianTextSans-Regular", lineHeight = 16, size = 15),
@@ -30,7 +30,7 @@ object EditionEndOfYear extends SpecialEdition {
   )
   override val headerStyle: Option[SpecialEditionHeaderStyles] = Some(
     SpecialEditionHeaderStyles(
-      backgroundColor = "#0F70B7",
+      backgroundColor = "#7D0068",
       textColorPrimary = "#FFFFFF",
       textColorSecondary = "#FFFFFF"
     )
@@ -146,7 +146,7 @@ object EditionEndOfYear extends SpecialEdition {
     collection("Special"),
     collection("Special")
   ).swatch(Culture)
-  
+
   def Special11 = front("Books", None,
     collection("Special"),
     collection("Special"),
@@ -154,7 +154,7 @@ object EditionEndOfYear extends SpecialEdition {
     collection("Special"),
     collection("Special")
   ).swatch(Culture)
-  
+
   def Special12 = front("TV", None,
     collection("Special"),
     collection("Special"),
@@ -162,7 +162,7 @@ object EditionEndOfYear extends SpecialEdition {
     collection("Special"),
     collection("Special")
   ).swatch(Culture)
-  
+
   def Special13 = front("Humour", None,
     collection("Special"),
     collection("Special"),
@@ -170,7 +170,7 @@ object EditionEndOfYear extends SpecialEdition {
     collection("Special"),
     collection("Special")
   ).swatch(Sport)
-  
+
   def Special14 = front("Sport", None,
     collection("Special"),
     collection("Special"),
@@ -178,7 +178,7 @@ object EditionEndOfYear extends SpecialEdition {
     collection("Special"),
     collection("Special")
   ).swatch(Sport)
-  
+
   def Special15 = front("Looking ahead", None,
     collection("Special"),
     collection("Special"),
@@ -186,7 +186,7 @@ object EditionEndOfYear extends SpecialEdition {
     collection("Special"),
     collection("Special")
   ).swatch(Sport)
-  
+
   def Special16 = front("Spare3", None,
     collection("Special"),
     collection("Special"),
@@ -194,5 +194,5 @@ object EditionEndOfYear extends SpecialEdition {
     collection("Special"),
     collection("Special")
   ).swatch(Sport)
-  
+
 }
