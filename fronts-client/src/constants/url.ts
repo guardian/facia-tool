f9d9f32098a2643a4fbdf0679bf9fbd5f38261ab
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-03-15 09:22:27 +0000

    Choose CODE preview URL when in CODE or DEV state

--- fronts-client/src/constants/url.ts
+++ fronts-client/src/constants/url.ts
@@ -1,12 +1,16 @@
 import pageConfig from 'util/extractConfigFromPage';
+
+const previewDomain = 'preview.gutools.co.uk'
+
 export default {
   base: {
     mainDomain: 'www.theguardian.com',
     mainDomainShort: 'theguardian.com',
     frontendDomain: 'frontend.gutools.co.uk',
-    previewDomain: 'preview.gutools.co.uk',
+    previewDomain,
     shortDomain: 'gu.com',
     capi: 'content.guardianapis.com',
+
   },
   media: {
     apiBaseUrl: pageConfig.apiBaseUrl,
@@ -22,4 +26,6 @@ export default {
   manageEditions: '/manage-editions/',
   appRoot: 'v2',
   editionsCardBuilder: 'https://editions-card-builder.gutools.co.uk',
+  previewUrlPROD: `https://${previewDomain}/responsive-viewer/${previewDomain}/`,
+  previewUrlCODE: 'https://m.code.dev-theguardian.com/',
 };
