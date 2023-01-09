e7d21272a725f4f8a8a3bc1d5c7e5b56da8b2d4f
David Blishen <david.blishen@guardian.co.uk>
2023-01-09 14:08:59 +0000

    Adds What's On as a front
    I've assumed that  single quotes don't need escaping:  https://stackoverflow.com/questions/30079501/why-does-scala-allow-single-quotes-in-strings-to-be-escaped

--- app/model/editions/templates/DailyEdition.scala
+++ app/model/editions/templates/DailyEdition.scala
@@ -40,6 +40,8 @@ object DailyEdition extends RegionalEdition {
       FrontOpinionSpecial -> Daily(),
       // Culture fronts and special
       FrontCulture -> WeekDays(List(WeekDay.Mon, WeekDay.Tues, WeekDay.Wed, WeekDay.Thurs)),
+      // What's on
+      FrontWhatsOn -> WeekDays(List(WeekDay.Sat)),
       FrontCultureFilmMusic -> WeekDays(List(WeekDay.Fri)),
       // New for Saturday Magazine
       FrontLifeSaturdayMagazineFeatures -> WeekDays(List(WeekDay.Sat)),
@@ -224,6 +226,16 @@ object DailyEdition extends RegionalEdition {
     collection("Culture").hide,
     collection("TV & radio").printSentAnyTag("theguardian/g2/tvandradio"),
     collection("Culture").hide
+  )
+    .swatch(Culture)
+  
+  def FrontWhatsOn = front(
+    "What's On",
+    collection("What's On").printSentAnyTag("theguardian/whatson/whatson"),
+    collection("What's On"),
+    collection("What's On"),
+    collection("What's On"),
+    collection("What's On").hide
   )
     .swatch(Culture)
 
