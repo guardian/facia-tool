6a577c37ffac40e08deb4c9f6e3661fec02489b1
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-03-01 10:29:57 +0000

    Add test

--- fronts-client/src/selectors/collectionSelectors.ts
+++ fronts-client/src/selectors/collectionSelectors.ts
@@ -37,8 +37,7 @@ const selectCollectionParams = (
     []
   );
 
-const selectIsCollectionLocked = (state: State, id: string): boolean =>
-  !!selectCollectionConfig(state, id).uneditable;
+const selectIsCollectionLocked = (state: State, id: string): boolean => !!selectCollectionConfig(state, id)?.uneditable;
 
 const selectWillCollectionHitCollectionCap = (
   state: State,
