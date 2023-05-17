18819b0f8db1af16e5adcdbd0610bfb06dd66280
David Furey <david.furey@guardian.co.uk>
2023-05-17 16:49:15 +0100

    Rename UncachedAssets to V1Assets
    Since this is a more accurate name, and arguably some of these assets should be cached

--- app/controllers/V1Assets.scala
+++ app/controllers/V1Assets.scala
@@ -2,7 +2,7 @@ package controllers
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class UncachedAssets(val assets: Assets, deps: BaseFaciaControllerComponents) extends BaseFaciaController(deps) {
+class V1Assets(val assets: Assets, deps: BaseFaciaControllerComponents) extends BaseFaciaController(deps) {
   def at(file: String, relativePath: String = "") = model.NoCache {
     assets.at("/public/src", relativePath + file)
   }
