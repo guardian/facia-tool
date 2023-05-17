7868bcd98c75b3cb03f6b5eefd66268b96e235d9
David Furey <david.furey@guardian.co.uk>
2023-05-17 16:52:31 +0100

    Serve fronts-client-v1 bundles

--- app/controllers/V1Assets.scala
+++ app/controllers/V1Assets.scala
@@ -4,6 +4,10 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class V1Assets(val assets: Assets, deps: BaseFaciaControllerComponents) extends BaseFaciaController(deps) {
   def at(file: String, relativePath: String = "") = model.NoCache {
-    assets.at("/public/src", relativePath + file)
+    if (file.startsWith("fronts-client-v1/bundles")) {
+      assets.at("/public", relativePath + file)
+    } else {
+      assets.at("/public/src", relativePath + file)
+    }
   }
 }
