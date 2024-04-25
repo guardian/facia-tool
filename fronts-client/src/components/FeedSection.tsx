d47e4335d2fb786fe97b553e1c14de2865e28e95
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-04-25 10:26:36 +0100

    Pass platform into client, using platform to conditionally display Feast search UI

--- fronts-client/src/components/FeedSection.tsx
+++ fronts-client/src/components/FeedSection.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { styled, theme } from 'constants/theme';
 
+import { selectors as editionsIssueSelectors } from '../bundles/editionsIssueBundle';
 import SectionContent from './layout/SectionContent';
 import FeedContainer from './FeedsContainer';
 import Clipboard from './Clipboard';
 import FeedSectionHeader from './FeedSectionHeader';
 import { media } from 'util/mediaQueries';
 import { connect } from 'react-redux';
-import { isMode } from 'selectors/pathSelectors';
 import { State } from 'types/State';
 import { FeastSearchContainer } from './feast/FeastSearchContainer';
 
 interface Props {
   isClipboardOpen: boolean;
-  isEditions: boolean;
+  isFeast: boolean;
 }
 
 const FeedSectionContainer = styled.div`
@@ -32,12 +32,12 @@ const FeedWrapper = styled.div<{ isClipboardOpen: boolean }>`
     isClipboardOpen ? `solid 1px ${theme.base.colors.borderColor}` : null};
 `;
 
-const FeedSection = ({ isClipboardOpen, isEditions }: Props) => (
+const FeedSection = ({ isClipboardOpen, isFeast }: Props) => (
   <FeedSectionContainer>
     <FeedSectionHeader />
     <FeedSectionContent>
       <FeedWrapper isClipboardOpen={isClipboardOpen}>
-        {isEditions ? <FeastSearchContainer /> : <FeedContainer />}
+        {isFeast ? <FeastSearchContainer /> : <FeedContainer />}
       </FeedWrapper>
       <Clipboard />
     </FeedSectionContent>
@@ -45,7 +45,7 @@ const FeedSection = ({ isClipboardOpen, isEditions }: Props) => (
 );
 
 const mapStateToProps = (state: State) => ({
-  isEditions: isMode(state, 'editions'),
+  isFeast: editionsIssueSelectors.selectAll(state)?.platform === 'feast',
 });
 
 export default connect(mapStateToProps)(FeedSection);
