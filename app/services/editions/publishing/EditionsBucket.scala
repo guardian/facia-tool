e0897bcee4534327207b251a235b3b73c0335388
Andy Gallagher <fredex42@gmail.com>
2024-05-02 14:56:47 +0100

    fix truncated JSON issue

--- app/services/editions/publishing/EditionsBucket.scala
+++ app/services/editions/publishing/EditionsBucket.scala
@@ -7,11 +7,8 @@ import model.editions.PublishableIssue
 import play.api.libs.json.{Json, Writes}
 import PublishedIssueFormatters._
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.commons.io.output.ByteArrayOutputStream
 import org.apache.commons.lang3.builder.{ReflectionToStringBuilder, ToStringStyle}
-import services.editions.publishing.EditionsBucket.logger
 
-import java.io.ObjectOutputStream
 import java.nio.charset.StandardCharsets
 
 object EditionsBucket extends LazyLogging {
@@ -22,7 +19,7 @@ object EditionsBucket extends LazyLogging {
 
   def createKey(issue: PublishableIssue): String = s"${createIssuePrefix(issue)}/${createIssueFilename(issue)}"
 
-  val objectMetadata: ObjectMetadata = {
+  val baseMetadata: ObjectMetadata = {
     val metadata = new ObjectMetadata()
     metadata.setContentType("application/json")
     metadata
@@ -30,10 +27,14 @@ object EditionsBucket extends LazyLogging {
 
   def createPutObjectRequest[T:Writes](bucketName: String, key: String, issue: T): PutObjectRequest = {
     val issueJson = Json.stringify(Json.toJson(issue))
-    val baseMd = EditionsBucket.objectMetadata
-    baseMd.setContentLength(issueJson.length)
-    logger.info(s"createPutObjectRequest: sending ${issueJson.length} chars to $bucketName")
-    new PutObjectRequest(bucketName, key, new StringInputStream(issueJson), baseMd)
+
+    //Why do we do this? Well, because if we are sending a streaming PutObjectRequest then S3 requires the length of the stream
+    //If it's not explicitly set in ObjectMetadata, then the AWS SDK will consume the entire string into memory just to measure the length.
+    //This is pointless as we already _have_ it in memory here (and it creates un-necessary log noise).  So we need to put the BYTE length into the header.
+    //Note that the byte length of a UTF-8 string can easily be greater than the character length, which is why we need to use getBytes here.
+    val metadata = baseMetadata
+    metadata.setContentLength(issueJson.getBytes(StandardCharsets.UTF_8).length)
+    new PutObjectRequest(bucketName, key, new StringInputStream(issueJson), metadata)
   }
 }
 
@@ -50,7 +51,9 @@ class EditionsBucket(s3Client: AmazonS3, bucketName: String) extends Publication
   }
 
   def putEditionsList(rawJson: String): Unit = {
-    val request = new PutObjectRequest(bucketName, "editionsList", new StringInputStream(rawJson), EditionsBucket.objectMetadata)
+    val metadata = EditionsBucket.baseMetadata
+    metadata.setContentLength(rawJson.getBytes(StandardCharsets.UTF_8).length)
+    val request = new PutObjectRequest(bucketName, "editionsList", new StringInputStream(rawJson), metadata)
     s3Client.putObject(request)
   }
 }
