561699d544242955966ac506f20aec7931326dbb
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-05-15 17:16:11 +0100

    Do not poll every second for switchboard changes

--- app/switchboard/Switchboard.scala
+++ app/switchboard/Switchboard.scala
@@ -21,7 +21,6 @@ class Lifecycle(conf: SwitchboardConfiguration, scheduler: Scheduler) extends Lo
 
   logger.info("Starting switchboard cache")
   scheduler.scheduleWithFixedDelay(0.seconds, 1.minute) { () => refreshSwitches() }
-  scheduler.scheduleWithFixedDelay(0.seconds, 1.seconds) { () => refreshSwitches() }
 
   def refreshSwitches(): Unit = {
     logger.info("Refreshing switches from switchboard")
