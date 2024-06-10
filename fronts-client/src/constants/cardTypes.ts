ee10adf237259cf6bde3b30cbbf1fde20e7890f9
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2024-06-10 17:36:16 +0100

    Add recipe-collection card type to relevant enums

--- fronts-client/src/constants/cardTypes.ts
+++ fronts-client/src/constants/cardTypes.ts
@@ -3,6 +3,7 @@ export const CardTypesMap = {
   ARTICLE: 'article',
   RECIPE: 'recipe',
   CHEF: 'chef',
+  RECIPE_COLLECTION: 'recipe-collection',
 } as const;
 
 export type CardTypes = (typeof CardTypesMap)[keyof typeof CardTypesMap];
