604bc0242e38284105bddedcc69202aed6dfe090
Divya Bhatt <divya.bhatt@guardian.co.uk>
2024-06-12 07:35:13 +0100

    Make changes for fetching chef data from contributors tag

--- fronts-client/src/bundles/chefsBundle.ts
+++ fronts-client/src/bundles/chefsBundle.ts
@@ -7,6 +7,10 @@ import { selectCard } from 'selectors/shared';
 import { State } from 'types/State';
 import { createSelector } from 'reselect';
 import { stripHtml } from 'util/sanitizeHTML';
+//import { createSelectIsArticleStale } from 'util/externalArticle';
+import { ThunkResult } from 'types/Store';
+import { previewCapi, liveCapi } from 'services/capiQuery';
+import { createSelectIsArticleStale } from '../util/externalArticle';
 
 const sanitizeChef = (chef: Chef) => ({
   ...chef,
@@ -15,13 +19,100 @@ const sanitizeChef = (chef: Chef) => ({
 
 const bundle = createAsyncResourceBundle<Chef>('chefs', {
   indexById: true,
-  initialData: {
-    // Add stub data in the absence of proper search data.
-    [chefOttolenghi.id]: sanitizeChef(chefOttolenghi),
-    [chefStein.id]: sanitizeChef(chefStein),
-    [chefCloake.id]: sanitizeChef(chefCloake),
-  },
+  selectLocalState: (state) => state.chefs,
+  // initialData: {
+  //   // Add stub data in the absence of proper search data.
+  //   [chefOttolenghi.id]: sanitizeChef(chefOttolenghi),
+  //   [chefStein.id]: sanitizeChef(chefStein),
+  //   [chefCloake.id]: sanitizeChef(chefCloake),
+  // },
 });
+
+/*const isNonCommercialArticle = (article: CapiArticle | undefined): boolean => {
+  if (!article) {
+    return true;
+  }
+
+  if (article.isHosted) {
+    return false;
+  }
+
+  if (!article.tags) {
+    return true;
+  }
+
+  return article.tags.every((tag) => tag.type !== 'paid-content');
+};
+
+ */
+
+const fetchResourceOrResults = async (
+  capiService: typeof liveCapi,
+  params: object,
+  isResource: boolean,
+  fetchFromPreview: boolean = false
+) => {
+  //const capiEndpoint = fetchFromPreview ? capiService.tags : capiService.search;
+  const capiEndpoint = capiService.chefs;
+  const { response } = await capiEndpoint(params);
+
+  return {
+    results: response.results,
+    pagination: {
+      totalPages: response.pages,
+      currentPage: response.currentPage,
+      pageSize: response.pageSize,
+    },
+  };
+};
+
+export const createFetch =
+  (
+    actions: typeof bundle.actions,
+    //selectIsArticleStale: ReturnType<typeof createSelectIsArticleStale>,
+    isPreview: boolean = false
+  ) =>
+  (params: object, isResource: boolean): ThunkResult<void> =>
+  async (dispatch, getState) => {
+    dispatch(actions.fetchStart());
+    try {
+      const resultData = await fetchResourceOrResults(
+        isPreview ? previewCapi : liveCapi,
+        params,
+        isResource,
+        isPreview
+      );
+      if (resultData) {
+        /*const nonCommercialResults = resultData.results.filter((article) =>
+              isNonCommercialArticle(article)
+            );
+            const updatedResults = nonCommercialResults.filter((article) =>
+              selectIsArticleStale(
+                getState(),
+                article.id,
+                article.fields.lastModified
+              )
+            );
+
+             */
+        dispatch(
+          actions.fetchSuccess(resultData, {
+            pagination: resultData.pagination || undefined,
+            order: resultData.results.map((_) => _.id),
+          })
+        );
+      } else {
+        dispatch(actions.fetchSuccessIgnore([]));
+      }
+    } catch (e) {
+      dispatch(actions.fetchError(e));
+    }
+  };
+
+export const fetchLive = createFetch(
+  bundle.actions
+  //createSelectIsArticleStale(bundle.selectors.selectById)
+);
 
 const selectChefDataFromCardId = (
   state: State,
