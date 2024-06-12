9ec95fb5fb1a4fb54642293a8c52d278314a6ac6
Divya Bhatt <divya.bhatt@guardian.co.uk>
2024-06-12 07:36:02 +0100

    Add polling to get chef data

--- fronts-client/src/components/feed/RecipeSearchContainer.tsx
+++ fronts-client/src/components/feed/RecipeSearchContainer.tsx
@@ -2,10 +2,10 @@ import ClipboardHeader from 'components/ClipboardHeader';
 import TextInput from 'components/inputs/TextInput';
 import ShortVerticalPinline from 'components/layout/ShortVerticalPinline';
 import { styled } from 'constants/theme';
-import React, { useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { connect } from 'react-redux';
 import { selectors as recipeSelectors } from 'bundles/recipesBundle';
-import { selectors as chefSelectors } from 'bundles/chefsBundle';
+import { fetchLive, selectors as chefSelectors } from 'bundles/chefsBundle';
 import { State } from 'types/State';
 import { Recipe } from 'types/Recipe';
 import { Chef } from 'types/Chef';
@@ -14,6 +14,8 @@ import { SearchTitle } from './SearchTitle';
 import { RecipeFeedItem } from './RecipeFeedItem';
 import { ChefFeedItem } from './ChefFeedItem';
 import { RadioButton, RadioGroup } from '../inputs/RadioButtons';
+import { getIdFromURL } from '../../util/CAPIUtils';
+import { Dispatch } from '../../types/Store';
 
 const InputContainer = styled.div`
   margin-bottom: 10px;
@@ -46,9 +48,58 @@ const FeastSearchContainerComponent = ({
   chefs,
 }: Props) => {
   const [selectedOption, setSelectedOption] = useState(FeedType.recipes);
+  const [intervalId, setIntervalId] = useState(null);
+  const [inputState, setInputState] = useState({ query: '', chefs: [] });
+
   const handleOptionChange = (optionName: FeedType) => {
     setSelectedOption(optionName);
   };
+
+  const getParams = (query: string) => ({
+    q: query,
+    'page-size': '20',
+    'show-elements': 'image',
+    'show-fields': 'all',
+  });
+
+  // Function to stop polling
+  const stopPolling = useCallback(() => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  }, [intervalId]);
+
+  const runSearch = useCallback(
+    (page = 1) => {
+      const id = getIdFromURL(inputState.query);
+      const searchTerm = id ? id : inputState.query;
+      //const isLive = capiFeedIndex === 0;
+      //const fetch = isLive ? this.props.fetchLive : this.props.fetchPreview;
+      const fetch = selectedOption === FeedType.chefs ? chefs : recipes;
+      fetch(
+        {
+          ...getParams(searchTerm),
+          page,
+        },
+        !!id
+      );
+    },
+    [inputState]
+  );
+
+  const runSearchAndRestartPolling = useCallback(() => {
+    stopPolling();
+    const newIntervalId = setInterval(() => {
+      runSearch();
+    }, 30000);
+    setIntervalId(newIntervalId);
+    runSearch();
+  }, [stopPolling, runSearch]);
+  //
+  // useEffect(() => {
+  //   return () => stopPolling();
+  // }, [stopPolling]);
 
   const renderTheFeed = () => {
     switch (selectedOption) {
@@ -68,6 +119,10 @@ const FeastSearchContainerComponent = ({
       <InputContainer>
         <TextInputContainer>
           <TextInput placeholder="Search recipes" displaySearchIcon />
+          <div>
+            <button onClick={runSearchAndRestartPolling}>Start Search</button>
+            <button onClick={stopPolling}>Stop Search</button>
+          </div>
         </TextInputContainer>
         <ClipboardHeader />
       </InputContainer>
@@ -100,10 +155,13 @@ const FeastSearchContainerComponent = ({
   );
 };
 
-const mapStateToProps = (state: State) => ({
+const mapStateToProps = (state: State, dispatch: Dispatch) => ({
   recipes: recipeSelectors.selectAll(state),
-  chefs: chefSelectors.selectAll(state),
+  chefs: (params: object, isResource: boolean) =>
+    dispatch(fetchLive(params, isResource)),
 });
+
+//chefs: chefSelectors.selectAll(state),
 
 export const RecipeSearchContainer = connect(mapStateToProps)(
   FeastSearchContainerComponent
