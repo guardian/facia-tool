5bd6645ba1a62eb6abb9410a8b7ebb21f1f9da08
Divya Bhatt <divya.bhatt@guardian.co.uk>
2024-08-22 10:43:21 +0100

    Update fixture and testcase to fix failing test. And also apply yarn lint fix.
    Co-authored-by: Jonathon Herbert <jonathon.herbert@guardian.co.uk>

--- fronts-client/src/actions/__tests__/Editions.spec.ts
+++ fronts-client/src/actions/__tests__/Editions.spec.ts
@@ -1,20 +1,37 @@
-import {apiResponse, finalState, initialState} from "./fixtures/Editions.fixture";
-import {addFrontCollection} from "../Editions";
-import configureStore from "../../util/configureStore";
-import {Dispatch} from "../../types/Store";
-import fetchMock from "fetch-mock";
+import {
+  apiResponse,
+  finalState,
+  initialState,
+} from './fixtures/Editions.fixture';
+import { addFrontCollection } from '../Editions';
+import configureStore from '../../util/configureStore';
+import { Dispatch } from '../../types/Store';
+import fetchMock from 'fetch-mock';
 
 jest.mock('uuid/v4', () => () => 'uuid');
-it('reducers', async () => {
-  const store = configureStore(initialState);
-  const frontId = "48bbbb7d-357f-4a07-9dac-646d6965cec2";
-
-  fetchMock.once(`/editions-api/fronts/${frontId}/collection`, apiResponse, {
-    //headers: {'Content-type': 'application/json'},
-    method: 'PUT',
+describe('Add new collection feature', () => {
+  const { now } = Date;
+  afterEach(fetchMock.restore);
+  beforeAll(() => {
+    (Date as any).now = () => 1337;
   });
-  await (store.dispatch as Dispatch)(addFrontCollection(frontId))
+  afterAll(() => {
+    (Date as any).now = now;
+  });
 
-  const state = store.getState()
-  expect(state).toEqual(finalState);
+  it('should add new collection in the front', async () => {
+    const store = configureStore(
+      initialState,
+      '/v2/issues/ae2035fa-7864-4c73-aabd-70ab70526bf7'
+    );
+    const frontId = '48bbbb7d-357f-4a07-9dac-646d6965cec2';
+
+    fetchMock.once(`/editions-api/fronts/${frontId}/collection`, apiResponse, {
+      method: 'PUT',
+    });
+    await (store.dispatch as Dispatch)(addFrontCollection(frontId));
+
+    const state = store.getState();
+    expect(state).toEqual(finalState);
+  });
 });
