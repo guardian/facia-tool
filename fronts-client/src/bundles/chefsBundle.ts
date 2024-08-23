8f21205edcc74846d411923b2b47ea77ab647562
Andy Gallagher <fredex42@gmail.com>
2024-08-23 14:29:16 +0100

    Wire in chef search and pull up full profile information

--- fronts-client/src/bundles/chefsBundle.ts
+++ fronts-client/src/bundles/chefsBundle.ts
@@ -1,4 +1,4 @@
-import createAsyncResourceBundle from '../lib/createAsyncResourceBundle';
+import createAsyncResourceBundle, { IPagination } from '../lib/createAsyncResourceBundle';
 import { Chef } from '../types/Chef';
 import { selectCard } from 'selectors/shared';
 import { State } from 'types/State';
@@ -7,6 +7,7 @@ import { stripHtml } from 'util/sanitizeHTML';
 import { ThunkResult } from 'types/Store';
 import { liveCapi } from 'services/capiQuery';
 import { Tag } from '../types/Capi';
+import { ChefSearchParams, liveRecipes } from '../services/recipeQuery';
 
 const sanitizeTag = (tag: Tag) => ({
   ...tag,
@@ -18,27 +19,27 @@ const bundle = createAsyncResourceBundle<Chef>('chefs', {
   selectLocalState: (state) => state.chefs,
 });
 
-const fetchResourceOrResults = async (
-  capiService: typeof liveCapi,
-  params: Record<string, string[] | string | number>
-) => {
-  const capiEndpoint = capiService.chefs;
-  const { response } = await capiEndpoint(params);
-
-  return {
-    results: response.results,
-    pagination: {
-      totalPages: response.pages,
-      currentPage: response.currentPage,
-      pageSize: response.pageSize,
-    },
-  };
-};
+// const fetchResourceOrResults = async (
+//   capiService: typeof liveCapi,
+//   params: Record<string, string[] | string | number>
+// ) => {
+//   const capiEndpoint = capiService.chefs;
+//   const { response } = await capiEndpoint(params);
+//
+//   return {
+//     results: response.results,
+//     pagination: {
+//       totalPages: response.pages,
+//       currentPage: response.currentPage,
+//       pageSize: response.pageSize,
+//     },
+//   };
+// };
 
 export const fetchChefs =
   (
     // The params to include in the request
-    params: Record<string, string[] | string | number>,
+    params: ChefSearchParams,
     // The ids of the chefs being fetched, if known.
     // If we know which IDs we're searching for, we do not include their order
     // in our state. This lets us keep `lastFetchOrder` for order-sensitive
@@ -48,21 +49,18 @@ export const fetchChefs =
   async (dispatch) => {
     dispatch(actions.fetchStart(ids));
     try {
-      const resultData = await fetchResourceOrResults(liveCapi, params);
-      if (resultData) {
-        const payload = ids
-          ? { ignoreOrder: true }
-          : {
-              pagination: resultData.pagination || undefined,
-              order: resultData.results.map((_) => _.id),
-            };
-
+      const chefs = await liveRecipes.chefs(params);
+      console.log(`Got a total of ${chefs.results.length} hits`);
+      const chefsWithTags = chefs.results.filter(chef=>chef.contributorType==="Profile");
+      console.log(`${chefsWithTags.length} had associated contributor tags`);
+      if(chefsWithTags.length==0) {
         dispatch(
-          actions.fetchSuccess(resultData.results.map(sanitizeTag), payload)
+          actions.fetchSuccessIgnore([])
         );
-      } else {
-        dispatch(actions.fetchSuccessIgnore([]));
+        return;
       }
+
+      dispatch(fetchChefsById(chefsWithTags.map(chef =>chef.nameOrId)));
     } catch (e) {
       dispatch(actions.fetchError(e));
     }
@@ -72,14 +70,36 @@ export const fetchChefsById = (
   tagIds: string[],
   page = 1,
   pageSize = 20
-): ThunkResult<void> => {
-  const params = {
-    ids: tagIds,
-    page,
-    'page-size': pageSize,
+): ThunkResult<void> =>
+  async (dispatch) => {
+
+  try {
+    console.log(`tags to look up: ${tagIds.join(",")}`)
+    const chefTags = await liveCapi.chefs({
+      'ids': tagIds.join(","),
+      'show-elements': 'image',
+      'show-fields': 'all',
+    });
+
+    chefTags.response.results.forEach((t)=>
+      console.log(JSON.stringify(t))
+    );
+    const payload: { ignoreOrder?: undefined; pagination?: IPagination; order?: string[] } = {
+      pagination: {
+        pageSize: chefTags.response.pageSize,
+        totalPages: chefTags.response.pages,
+        currentPage: chefTags.response.currentPage
+      },
+      order: tagIds
+    };
+
+    dispatch(
+      actions.fetchSuccess(chefTags.response.results.map(sanitizeTag), payload)
+    )
+  } catch(err) {
+    dispatch(actions.fetchError(err))
+  }
   };
-  return fetchChefs(params, tagIds);
-};
 
 const selectChefDataFromCardId = (
   state: State,
