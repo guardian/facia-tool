cb4b7382d76bfbb513bf1165e5b4fbd8eb5dc650
Divya Bhatt <divya.bhatt@guardian.co.uk>
2024-09-30 13:48:03 +0100

    Increase pageSize to 100 to accomodate more chefs to get loaded always.
    Solution suggested by: Andy Gallagher <fredex42@gmail.com>

--- fronts-client/src/bundles/chefsBundle.ts
+++ fronts-client/src/bundles/chefsBundle.ts
@@ -1,4 +1,6 @@
-import createAsyncResourceBundle, { IPagination } from '../lib/createAsyncResourceBundle';
+import createAsyncResourceBundle, {
+  IPagination,
+} from '../lib/createAsyncResourceBundle';
 import { Chef } from '../types/Chef';
 import { selectCard } from 'selectors/shared';
 import { State } from 'types/State';
@@ -33,52 +35,69 @@ export const fetchChefs =
     dispatch(actions.fetchStart(ids));
     try {
       const chefs = await liveRecipes.chefs(params);
-      const chefsWithTags = chefs.results.filter(chef=>chef.contributorType==="Profile");
-      if(chefsWithTags.length==0) {
+      const chefsWithTags = chefs.results.filter(
+        (chef) => chef.contributorType === 'Profile'
+      );
+      if (chefsWithTags.length == 0) {
         dispatch(actions.fetchSuccess([]));
         return;
       }
 
-      dispatch(fetchChefsById(chefsWithTags.map(chef =>chef.nameOrId), 1,20,true));
+      dispatch(
+        fetchChefsById(
+          chefsWithTags.map((chef) => chef.nameOrId),
+          1,
+          20,
+          true
+        )
+      );
     } catch (e) {
       dispatch(actions.fetchError(e));
     }
   };
 
-export const fetchChefsById = (
-  tagIds: string[],
-  page = 1,
-  pageSize = 20,
-  alreadyStarted:boolean = false
-): ThunkResult<void> =>
+export const fetchChefsById =
+  (
+    tagIds: string[],
+    page = 1,
+    pageSize = 20,
+    alreadyStarted: boolean = false
+  ): ThunkResult<void> =>
   async (dispatch) => {
+    //we could be called as the second part of a two-stage fetch, OR we could be called directly.
+    //if called directly, indicate that we started a fetch. Otherwise, the first part should
+    if (!alreadyStarted) dispatch(actions.fetchStart(tagIds));
 
-  //we could be called as the second part of a two-stage fetch, OR we could be called directly.
-  //if called directly, indicate that we started a fetch. Otherwise, the first part should
-  if(!alreadyStarted) dispatch(actions.fetchStart(tagIds));
+    try {
+      const chefTags = await liveCapi.chefs({
+        ids: tagIds.join(','),
+        'show-elements': 'image',
+        'show-fields': 'all',
+        'page-size': 100,
+      });
 
-  try {
-    const chefTags = await liveCapi.chefs({
-      'ids': tagIds.join(","),
-      'show-elements': 'image',
-      'show-fields': 'all',
-    });
+      const payload: {
+        ignoreOrder?: undefined;
+        pagination?: IPagination;
+        order?: string[];
+      } = {
+        pagination: {
+          pageSize: chefTags.response.pageSize,
+          totalPages: chefTags.response.pages,
+          currentPage: chefTags.response.currentPage,
+        },
+        order: tagIds,
+      };
 
-    const payload: { ignoreOrder?: undefined; pagination?: IPagination; order?: string[] } = {
-      pagination: {
-        pageSize: chefTags.response.pageSize,
-        totalPages: chefTags.response.pages,
-        currentPage: chefTags.response.currentPage
-      },
-      order: tagIds
-    };
-
-    dispatch(
-      actions.fetchSuccess(chefTags.response.results.map(sanitizeTag), payload)
-    )
-  } catch(err) {
-    dispatch(actions.fetchError(err))
-  }
+      dispatch(
+        actions.fetchSuccess(
+          chefTags.response.results.map(sanitizeTag),
+          payload
+        )
+      );
+    } catch (err) {
+      dispatch(actions.fetchError(err));
+    }
   };
 
 const selectChefDataFromCardId = (
