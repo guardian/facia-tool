824ab293bb15087955600e07c24a6b895a7bf66f
Tom Richards <tom.richards@guardian.co.uk>
2024-10-14 15:52:52 +0100

    remove seemingly unnecessary margin-bottom from the article form

--- fronts-client/src/components/form/FormContent.tsx
+++ fronts-client/src/components/form/FormContent.tsx
@@ -1,10 +1,14 @@
 import { styled } from 'constants/theme';
 import { CardSizes } from 'types/Collection';
 
+interface FormContentProps {
+	size?: CardSizes;
+	marginBottom?: string;
+}
 export const FormContent = styled.div`
 	flex: 3;
 	display: flex;
-	flex-direction: ${(props: { size?: CardSizes }) =>
+	flex-direction: ${(props: FormContentProps) =>
 		props.size !== 'wide' ? 'column' : 'row'};
-	margin-bottom: 40px;
+	margin-bottom: ${(props: FormContentProps) => props.marginBottom ?? 0};
 `;
