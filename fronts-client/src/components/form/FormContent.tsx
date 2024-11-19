c42abf464ef5e0706e8783a624cbe4dafc9bc265
Georges Lebreton <102960844+Georges-GNM@users.noreply.github.com>
2024-11-19 10:20:43 +0000

    Move 60px margin bottom from form to caption input (#1722)

--- fronts-client/src/components/form/FormContent.tsx
+++ fronts-client/src/components/form/FormContent.tsx
@@ -9,5 +9,5 @@ export const FormContent = styled.div<FormContentProps>`
 	flex: 3;
 	display: flex;
 	flex-direction: ${({ size }) => (size !== 'wide' ? 'column' : 'row')};
-	margin-bottom: ${({ marginBottom }) => marginBottom ?? '60px'};
+	margin-bottom: ${({ marginBottom }) => marginBottom ?? 0};
 `;
