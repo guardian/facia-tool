f7c5aea40b80b0c307347edb55b96e98f39cb7fc
Charlotte Emms <43961396+cemms1@users.noreply.github.com>
2024-11-21 09:54:08 +0000

    allow portrait crops in production and reword feature switch description (#1733)

--- fronts-client/src/components/Features/FeaturesForm.tsx
+++ fronts-client/src/components/Features/FeaturesForm.tsx
@@ -5,7 +5,6 @@ import type { State } from 'types/State';
 import { selectAllFeatures } from 'selectors/featureSwitchesSelectors';
 import { FeatureSwitch } from 'types/Features';
 import { Dispatch } from 'types/Store';
-import pageConfig from 'util/extractConfigFromPage';
 import { actionSetFeatureValue } from 'actions/FeatureSwitches';
 import { saveFeatureSwitch } from 'services/userDataApi';
 
@@ -14,24 +13,13 @@ interface Props {
 	setFeatureValue: (featureSwitch: FeatureSwitch) => void;
 }
 
-const STAGE = pageConfig.env;
-
-// We don't yet have any collectionTypes that use portrait crops
-// but even when we do, we might not want to show the option on PROD
-// it might lead to some broken visuals if used before implemented
-// on platforms.
-const SWITCHES_TO_HIDE_ON_PROD = ['support-portrait-crops'];
-
-const filterSwitchesByStage = (featureSwitch: FeatureSwitch): boolean =>
-	STAGE === 'code' || !SWITCHES_TO_HIDE_ON_PROD.includes(featureSwitch.key);
-
 class FeaturesForm extends React.Component<Props> {
 	public render() {
 		const { featureSwitches } = this.props;
 		console.log(featureSwitches);
 		return (
 			<form>
-				{featureSwitches.filter(filterSwitchesByStage).map((featureSwitch) => (
+				{featureSwitches.map((featureSwitch) => (
 					<InputCheckboxToggle
 						key={featureSwitch.key}
 						label={featureSwitch.title}
