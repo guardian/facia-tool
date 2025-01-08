4726c0a596134a2b37f5504722021281143c09d2
David Furey <david.furey@guardian.co.uk>
2025-01-08 11:50:01 +0000

    Resolve AWS deprecation warnings
    Also removing unused endpoints

--- app/services/AwsEndpoints.scala
+++ app/services/AwsEndpoints.scala
@@ -1,16 +1,14 @@
 package services
 
-import com.amazonaws.regions.ServiceAbbreviations.{S3 => S3Endpoint, _}
-import com.amazonaws.regions.{Region, Regions}
+import com.amazonaws.services.s3.AmazonS3.{ENDPOINT_PREFIX => S3Endpoint}
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch.{ENDPOINT_PREFIX => CloudWatch}
+import com.amazonaws.regions.RegionUtils
 import conf.ApplicationConfiguration
 
 class AwsEndpoints(val config: ApplicationConfiguration) {
   private lazy val region =
-    Region.getRegion(Regions.fromName(config.aws.region))
+    RegionUtils.getRegion(config.aws.region)
 
-  lazy val sns: String = region.getServiceEndpoint(SNS)
-  lazy val elb: String = region.getServiceEndpoint(ElasticLoadbalancing)
   lazy val monitoring: String = region.getServiceEndpoint(CloudWatch)
-  lazy val dynamoDb: String = region.getServiceEndpoint(Dynamodb)
   lazy val s3: String = region.getServiceEndpoint(S3Endpoint)
 }
