9b1c84d7763f36ba9ae4ac7db9c197a3162fce0f
Emily Bourke <emily.bourke@guardian.co.uk>
2025-01-22 23:40:36 +0000

    Provide scripts to run the apps
    This is cool! This easily gives scripts like `fronts-client` within the
    nix shell that run the fronts-client commands with the right
    dependencies.
    
    Next step: provide a few run-all commands, e.g. one that runs all of
    them in tmux.
    
    Eventual goal: every app works the same way and I don’t need to read a
    dev-start.sh or setup.sh script again.

--- shell.nix
+++ shell.nix
@@ -3,7 +3,42 @@ let pkgs = import sources.nixpkgs { };
 in pkgs.mkShellNoCC {
   nativeBuildInputs = with pkgs;
     let
-      sbtWithJava21 = sbt.override { jre = corretto21; };
+      sbtWithJava11 = sbt.override { jre = corretto11; };
       yarnWithNode20 = yarn.override { nodejs = nodejs_20; };
-    in [ sbtWithJava21 metals yarnWithNode20 nodejs_18 ];
+      frontsClient = writeShellApplication {
+        name = "fronts-client";
+        runtimeInputs = [ yarnWithNode20 ];
+        text = ''
+          cd fronts-client
+          yarn install
+          yarn build
+          yarn watch
+        '';
+      };
+      startPlay = writeShellApplication {
+        name = "start-play";
+        runtimeInputs = [ sbtWithJava11 ];
+        text = ''
+          export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -Xmx4G -Xss4m"
+          sbt "run"
+        '';
+      };
+      buildV1 = writeShellApplication {
+        name = "build-v1";
+        runtimeInputs = [ nodejs_18 ];
+        text = ''
+          npm install
+          npm run jspm install
+        '';
+      };
+
+    in [
+      sbtWithJava11
+      metals
+      yarnWithNode20
+      nodejs_18
+      frontsClient
+      startPlay
+      buildV1
+    ];
 }
