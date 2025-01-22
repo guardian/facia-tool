a175183c238376fe359169817fc3a9635144a430
Emily Bourke <emily.bourke@guardian.co.uk>
2025-01-22 14:56:19 +0000

    Just provide yarn with specific node version
    This approach gives a nix shell with a yarn available which has the
    right (major) node version for fronts-client (20), and a different
    version available with node and npm (for the v1 app).
    
    Unfortunately, nix refuses to allow node 16 as it’s EOL, so I’ve just
    stuck with 18 for the moment in order to demonstrate that differences
    are possible. Presumably there’s some way to get around the EOL warning,
    but I’m not sure how right now.
    
    With this much, it should be possible to write a script that runs each
    of the commands in separate tmux terminals, for example. (Except for the
    docker-compose bit, which I haven’t looked at yet.)

--- shell.nix
+++ shell.nix
@@ -4,12 +4,6 @@ in pkgs.mkShellNoCC {
   nativeBuildInputs = with pkgs;
     let
       sbtWithJava21 = sbt.override { jre = corretto21; };
-      fronts-client = mkYarnPackage {
-        name = "fronts-client";
-        src = ./fronts-client;
-        packageJSON = ./fronts-client/package.json;
-        yarnLock = ./fronts-client/yarn.lock;
-        yarnNix = ./fronts-client/fronts-client.nix;
-      };
-    in [ sbtWithJava21 metals fronts-client ];
+      yarnWithNode20 = yarn.override { nodejs = nodejs_20; };
+    in [ sbtWithJava21 metals yarnWithNode20 nodejs_18 ];
 }
