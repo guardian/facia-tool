8a9b103d7a3768e9e1a3a9c9e2bcb82c90593a82
Anna Beddow <anna.beddow@gmail.com>
2025-03-25 17:16:30 +0000

    Make toggling boost levels or the immersive field dependent on the other
    Co-authored-by: Dominik Lander <dominik.lander@guardian.co.uk>

--- fronts-client/src/components/form/__tests__/BoostToggles.spec.tsx
+++ fronts-client/src/components/form/__tests__/BoostToggles.spec.tsx
@@ -44,7 +44,12 @@ describe('renderBoostToggles', () => {
 	groupToggles.forEach(({ group, collectionType, expectedLabels }) => {
 		it(`returns the correct toggles for ${collectionType} group ${group}`, () => {
 			// ✅ Get the raw output instead of rendering in a form
-			const toggles = renderBoostToggles(group, mockCardId, collectionType);
+			const toggles = renderBoostToggles(
+				group,
+				mockCardId,
+				() => {},
+				collectionType,
+			);
 
 			// ✅ Render only the toggles, skipping redux-form & form
 			const tree = renderer.create(<>{toggles}</>).toJSON();
@@ -58,7 +63,7 @@ describe('renderBoostToggles', () => {
 	});
 
 	it('returns an empty fragment if collectionType is invalid', () => {
-		const toggles = renderBoostToggles(0, mockCardId, 'invalid/type');
+		const toggles = renderBoostToggles(0, mockCardId, () => {}, 'invalid/type');
 		const tree = renderer.create(<>{toggles}</>).toJSON();
 
 		expect(tree).toMatchSnapshot();
@@ -66,7 +71,7 @@ describe('renderBoostToggles', () => {
 	});
 
 	it('returns an empty fragment if collectionType is missing', () => {
-		const toggles = renderBoostToggles(0, mockCardId);
+		const toggles = renderBoostToggles(0, mockCardId, () => {});
 		const tree = renderer.create(<>{toggles}</>).toJSON();
 
 		expect(tree).toMatchSnapshot();
