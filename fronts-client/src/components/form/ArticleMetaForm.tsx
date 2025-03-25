dd1f4e7f5aafe35eb89a443be88ade85efd257e8
fweddi <freddiepreece@outlook.com>
2025-03-25 12:29:23 +0000

    Move video inside the radio button

--- fronts-client/src/components/form/ArticleMetaForm.tsx
+++ fronts-client/src/components/form/ArticleMetaForm.tsx
@@ -847,6 +847,25 @@ class FormComponent extends React.Component<Props, FormComponentState> {
 												editableFields.indexOf('videoReplace') === -1
 											}
 											icon={<SelectVideoIcon />}
+											contents={<VideoControls
+												atomId={this.props.atomId}
+												active={(this.props.showMainVideo || this.props.videoReplace)}
+												onChange={(e: any) => {
+													const value = e.currentTarget.value;
+
+													if (
+														value !== undefined &&
+														value !== null &&
+														value !== ''
+													) {
+														this.changeMediaField('videoReplace');
+													} else {
+														this.changeMediaField('showMainVideo');
+													}
+
+													this.props.change('atomId', extractAtomId(value));
+												}}
+											/>}
 											usesBlockStyling={true}
 											name="media-select"
 											type="radio"
@@ -861,30 +880,7 @@ class FormComponent extends React.Component<Props, FormComponentState> {
 											}
 										/>
 									</InputGroup>
-									{(this.props.showMainVideo || this.props.videoReplace) && <VideoControls atomId={this.props.atomId} />}
-									{(this.props.showMainVideo || this.props.videoReplace) && (
-										<Field
-											component={InputText}
-											name="replaceVideoUri"
-											type="text"
-											onChange={(e: any) => {
-												const value = e.currentTarget.value;
 
-												if (
-													value !== undefined &&
-													value !== null &&
-													value !== ''
-												) {
-													this.changeMediaField('videoReplace');
-												} else {
-													this.changeMediaField('showMainVideo');
-												}
-
-												this.props.change('atomId', extractAtomId(value));
-											}}
-											placeholder="Paste video url"
-										/>
-									)}
 									<InputGroup>
 										<Field
 											component={InputRadio}
