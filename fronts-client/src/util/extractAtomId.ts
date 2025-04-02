c2c2be0b13e9effa6ac0777161dbe3e685b8245e
fweddi <freddiepreece@outlook.com>
2025-04-02 11:51:11 +0100

    Refactor state management

--- fronts-client/src/util/extractAtomId.ts
+++ fronts-client/src/util/extractAtomId.ts
@@ -27,4 +27,52 @@ const extractAtomId = (src: string | undefined): string => {
 	return '';
 };
 
-export { extractAtomId };
+const extractAssetId = (atom: any): string | undefined => {
+	const assets = atom?.data?.media?.assets;
+	if (
+		assets === undefined ||
+		assets.length === 0 ||
+		assets[0] === undefined ||
+		assets[0].id === undefined
+	) {
+		throw new Error(`No assets found for atom ${atom.id}`);
+	} else {
+		return assets[0].id;
+	}
+};
+
+const extractVideoTrailImage = (atom: any): string | undefined => {
+	const imageAssets = atom?.data?.media?.trailImage?.assets;
+
+	if (
+		imageAssets === undefined ||
+		imageAssets.length === 0 ||
+		imageAssets[0] === undefined ||
+		imageAssets[0].file === undefined
+	) {
+		throw new Error(`No trail image found for atom ${atom.id}`);
+	} else {
+		return imageAssets[0].file;
+	}
+};
+
+const extractAtomProperties = (
+	atom: any
+) => {
+	if(atom === undefined) {
+		return {
+			assetId: undefined,
+			trailImage: undefined
+		};
+	}
+
+	const assetId = extractAssetId(atom);
+	const trailImage = extractVideoTrailImage(atom);
+
+	return {
+		assetId,
+		trailImage
+	}
+}
+
+export { extractAtomId, extractAtomProperties };
