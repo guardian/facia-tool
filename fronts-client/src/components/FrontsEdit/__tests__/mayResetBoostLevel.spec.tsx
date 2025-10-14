3e9ac29a0d12bbd29347cbff6f84640fafcb7e69
Andy Noton <andy.noton.contractor@guardian.co.uk>
2025-10-14 17:26:23 +0100

    update tests

--- fronts-client/src/components/FrontsEdit/__tests__/mayResetBoostLevel.spec.tsx
+++ fronts-client/src/components/FrontsEdit/__tests__/mayResetBoostLevel.spec.tsx
@@ -4,23 +4,31 @@ import { mayResetBoostLevel } from '../../../actions/Cards';
 import { baseTo, baseFrom } from './fixtures/groups.fixture';
 
 describe('mayResetBoostLevel', () => {
+	const state: any = {};
+
 	it('should early exit if to is not a group', () => {
 		const card = {} as Card;
 		const result = mayResetBoostLevel(
-			baseFrom,
-			{ ...baseTo, type: 'not-a-group' },
-			card,
-			'collection',
+			{
+				from: baseFrom,
+				to: {...baseTo, type: 'not-a-group'},
+				card,
+				persistTo: 'collection',
+				state
+			}
 		);
 		expect(result).toBe(undefined);
 	});
 	it('should early exit if the card is being persisted to the clipboard', () => {
 		const card = {} as Card;
 		const result = mayResetBoostLevel(
-			baseFrom,
-			{ ...baseTo, type: 'not-a-group' },
-			card,
-			'clipboard',
+			{
+				from: baseFrom,
+				to: {...baseTo, type: 'not-a-group'},
+				card,
+				persistTo: 'clipboard',
+				state
+			}
 		);
 		expect(result).toBe(undefined);
 	});
@@ -28,50 +36,65 @@ describe('mayResetBoostLevel', () => {
 	it('should not reset the boost level if the card is moving inside the same group', () => {
 		const card = {} as Card;
 		const result = mayResetBoostLevel(
-			{ ...baseFrom, id: 'group-1' },
-			{ ...baseTo, id: 'group-1' },
-			card,
-			'collection',
+			{
+				from: { ...baseFrom, id: 'group-1' },
+				to: { ...baseTo, id: 'group-1' },
+				card,
+				persistTo: 'collection',
+				state
+			}
 		);
 		expect(result).toBe(undefined);
 	});
 	it('should reset the boost level to `default` if the card is moving to a standard group', () => {
 		const card = {} as Card;
 		const result = mayResetBoostLevel(
-			baseFrom,
-			{ ...baseTo, groupName: 'standard' },
-			card,
-			'collection',
+			{
+				from: baseFrom,
+				to: { ...baseTo, groupName: 'standard' },
+				card,
+				persistTo: 'collection',
+				state
+			}
 		);
 		expect(result?.payload.meta.boostLevel).toBe('default');
 	});
 	it('should reset the boost level to `default` if the card is moving to a splash group', () => {
 		const card = {} as Card;
 		const result = mayResetBoostLevel(
-			baseFrom,
-			{ ...baseTo, groupName: 'splash' },
-			card,
-			'collection',
+			{
+				from: baseFrom,
+				to: {...baseTo, groupName: 'splash'},
+				card,
+				persistTo: 'collection',
+				state
+			}
 		);
 		expect(result?.payload.meta.boostLevel).toBe('default');
 	});
 	it('should reset the boost level to `boost` if the card is moving to a big group', () => {
 		const card = {} as Card;
 		const result = mayResetBoostLevel(
-			baseFrom,
-			{ ...baseTo, groupName: 'big' },
-			card,
-			'collection',
+			{
+				from: baseFrom,
+				to: {...baseTo, groupName: 'big'},
+				card,
+				persistTo: 'collection',
+				state
+			}
 		);
 		expect(result?.payload.meta.boostLevel).toBe('boost');
 	});
 	it('should reset the boost level to `megaboost` if the card is moving to a very big group', () => {
 		const card = {} as Card;
 		const result = mayResetBoostLevel(
-			baseFrom,
-			{ ...baseTo, groupName: 'very big' },
-			card,
-			'collection',
+			{
+				from: baseFrom,
+				to: {...baseTo, groupName: 'very big'},
+				card,
+				persistTo: 'collection',
+				state
+			}
 		);
 		expect(result?.payload.meta.boostLevel).toBe('megaboost');
 	});
--- fronts-client/src/components/FrontsEdit/__tests__/mayResetImmersive.spec.tsx
+++ fronts-client/src/components/FrontsEdit/__tests__/mayResetImmersive.spec.tsx
@@ -1,18 +1,25 @@
 import type { Card } from 'types/Collection';
 
-import { mayResetImageReplace } from '../../../actions/Cards';
+import { mayResetImmersive } from '../../../actions/Cards';
 import { baseTo, baseFrom } from './fixtures/groups.fixture';
 
-describe('mayResetImageReplace', () => {
-	const replacedImage_4_5_Card: Card = {
+describe('mayResetImmersive', () => {
+	const immersiveCard: Card = {
 		id: 'internal-code/page/15334368',
 		frontPublicationDate: 1741879217277,
 		meta: {
-			imageReplace: true,
-			imageSrcHeight: '1000',
-			imageSrcWidth: '800',
+			isImmersive: true,
 		},
 		uuid: 'abcd-1234',
+	};
+
+	const nonImmersiveCard: Card = {
+		id: 'internal-code/page/43681533',
+		frontPublicationDate: 1741879219999,
+		meta: {
+			isImmersive: false,
+		},
+		uuid: 'wxyz-5678',
 	};
 
 	const state: any = {
@@ -31,11 +38,11 @@ describe('mayResetImageReplace', () => {
 	};
 
 	it('should early exit if to is not a group', () => {
-		const result = mayResetImageReplace(
+		const result = mayResetImmersive(
 			{
 				from: baseFrom,
 				to: { ...baseTo, type: 'not-a-group' },
-				card: replacedImage_4_5_Card,
+				card: immersiveCard,
 				persistTo: 'collection',
 				state
 			}
@@ -43,11 +50,11 @@ describe('mayResetImageReplace', () => {
 		expect(result).toBe(undefined);
 	});
 	it('should early exit if the card is being persisted to the clipboard', () => {
-		const result = mayResetImageReplace(
+		const result = mayResetImmersive(
 			{
 				from: baseFrom,
 				to: baseTo,
-				card: replacedImage_4_5_Card,
+				card: immersiveCard,
 				persistTo: 'clipboard',
 				state,
 			}
@@ -55,63 +62,51 @@ describe('mayResetImageReplace', () => {
 		expect(result).toBe(undefined);
 	});
 	it('should early exit if the card is being moved within the same group', () => {
-		const result = mayResetImageReplace(
+		const result = mayResetImmersive(
 			{
 				from: { ...baseFrom, id: 'group-1' },
 				to: { ...baseTo, id: 'group-1' },
-				card: replacedImage_4_5_Card,
+				card: immersiveCard,
 				persistTo: 'collection',
 				state,
 			}
 		);
 		expect(result).toBe(undefined);
 	});
-	it('should not reset the imageReplace flag if moving from clipboard to a 4:5 collection', () => {
-		const result = mayResetImageReplace(
-			{
-				from: null,
-				to: { ...baseTo, collectionId: 'f4c5e687-0fc8-4456-b895-fd6e7237fa02' },
-				card: replacedImage_4_5_Card,
-				persistTo: 'collection',
-				state,
-			}
-		);
-		expect(result).toBe(undefined);
-	});
-	it('should reset the imageReplace flag if moving from clipboard to a 5:4 collection', () => {
-		const result = mayResetImageReplace(
+	it('should not reset the immersive flag if card is not immersive', () => {
+		const result = mayResetImmersive(
 			{
 				from: null,
 				to: baseTo,
-				card: replacedImage_4_5_Card,
-				persistTo: 'collection',
-				state,
-			}
-		);
-		expect(result?.payload.meta.imageReplace).toBe(false);
-	});
-	it('should not reset the imageReplace flag if moving 4:5 card to a 4:5 collection', () => {
-		const result = mayResetImageReplace(
-			{
-				from: baseFrom,
-				to: { ...baseTo, collectionId: 'f4c5e687-0fc8-4456-b895-fd6e7237fa02' },
-				card: replacedImage_4_5_Card,
+				card: nonImmersiveCard,
 				persistTo: 'collection',
 				state,
 			}
 		);
 		expect(result).toBe(undefined);
 	});
-	it('should reset the imageReplace flag if moving 4:5 card to a 5:4 collection', () => {
-		const result = mayResetImageReplace(
+	it('should reset the immersive flag if moving to a scrollable container', () => {
+		const result = mayResetImmersive(
 			{
-				from: baseFrom,
-				to: baseTo,
-				card: replacedImage_4_5_Card,
+				from: null,
+				to: { ...baseTo, collectionId: 'f4c5e687-0fc8-4456-b895-fd6e7237fa02' },
+				card: immersiveCard,
 				persistTo: 'collection',
 				state,
 			}
 		);
-		expect(result?.payload.meta.imageReplace).toBe(false);
+		expect(result?.payload.meta.isImmersive).toBe(false);
+	});
+	it('should not reset the immersive flag if to a flexible/general card', () => {
+		const result = mayResetImmersive(
+			{
+				from: baseFrom,
+				to: { ...baseTo, collectionId: 'da9953a2-6116-4e8d-9d62-f245fe65f399' },
+				card: immersiveCard,
+				persistTo: 'collection',
+				state,
+			}
+		);
+		expect(result).toBe(undefined);
 	});
 });
