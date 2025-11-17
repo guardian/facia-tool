57dcd7d39bcecf62c50ed5cae1ac0b9e8d02aa5b
Sam Hession <samhession@live.co.uk>
2025-11-17 08:59:01 +0000

    Increase config front changeImageUrl wait time 500 -> 1000

--- public/test/spec/config.front.spec.js
+++ public/test/spec/config.front.spec.js
@@ -238,7 +238,7 @@ describe('Config Front', function () {
             var imageUrl = images.path('square.png');
             dom.type('.metadata--provisionalImage', imageUrl);
 
-            return wait.ms(500).then(() => {
+            return wait.ms(1000).then(() => {
                 var front = frontWidget.pinnedFront();
                 expect(persistence.front.update).toHaveBeenCalledWith(front);
                 expect(front.props.webTitle()).toBe('Nicer title');
