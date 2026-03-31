adeadb7cb3f3cecb71fe3e5f88bfcc7f9834cd6e
fweddi <freddiepreece@outlook.com>
2026-03-31 12:06:57 +0100

    Increase timeout to fix flaky test

--- public/test/spec/config.front.spec.js
+++ public/test/spec/config.front.spec.js
@@ -238,7 +238,7 @@ describe('Config Front', function () {
             var imageUrl = images.path('square.png');
             dom.type('.metadata--provisionalImage', imageUrl);
 
-            return wait.ms(1000).then(() => {
+            return wait.ms(3000).then(() => {
                 var front = frontWidget.pinnedFront();
                 expect(persistence.front.update).toHaveBeenCalledWith(front);
                 expect(front.props.webTitle()).toBe('Nicer title');
