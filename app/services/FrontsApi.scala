149e0b97c5bd8effa7a82b9d51df814b1b43eb6c
David Furey <david.furey@guardian.co.uk>
2026-05-26 16:25:48 +0100

    Allow localstack S3 for local development
    While working on the config tool, I often corrupt some of the CODE fronts config files. Versioning is enabled, so it is easy to fix this and no one has noticed (yet).
    
    But it would be nice not to break the shared CODE environment.

--- app/services/FrontsApi.scala
+++ app/services/FrontsApi.scala
@@ -3,9 +3,14 @@ package services
 import com.gu.etagcaching.aws.sdkv2.s3.S3ObjectFetching
 import com.gu.facia.client.{ApiClient, Environment}
 import conf.ApplicationConfiguration
+import software.amazon.awssdk.auth.credentials.{
+  AwsBasicCredentials,
+  StaticCredentialsProvider
+}
 import software.amazon.awssdk.regions.Region.EU_WEST_1
 import software.amazon.awssdk.services.s3.S3AsyncClient
 
+import java.net.URI
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class FrontsApi(
@@ -16,11 +21,26 @@ class FrontsApi(
     val bucket = config.aws.frontsBucket
     val stage = config.facia.stage.toUpperCase
 
-    val s3AsyncClient = S3AsyncClient
-      .builder()
-      .region(EU_WEST_1)
-      .credentialsProvider(config.aws.newStyleCmsFrontsAccountCredentials)
-      .build()
+    val s3AsyncClient = config.aws.localS3Endpoint match {
+      case Some(endpoint) =>
+        S3AsyncClient
+          .builder()
+          .region(EU_WEST_1)
+          .endpointOverride(URI.create(endpoint))
+          .forcePathStyle(true)
+          .credentialsProvider(
+            StaticCredentialsProvider.create(
+              AwsBasicCredentials.create("test", "test")
+            )
+          )
+          .build()
+      case None =>
+        S3AsyncClient
+          .builder()
+          .region(EU_WEST_1)
+          .credentialsProvider(config.aws.newStyleCmsFrontsAccountCredentials)
+          .build()
+    }
 
     ApiClient.withCaching(
       bucket,
