77d26697c31825c8d35051450914e6dfe97a2b14
Emma Imber <emma-jo.imber@guardian.co.uk>
2026-08-04 16:40:14 +0100

    Add more abTest methods for form functionality
    Co-authored-by: fweddi <freddiepreece@outlook.com>
    Co-authored-by: Anna Beddow <anna.beddow@guardian.co.uk>

--- fronts-client/src/util/abTests.ts
+++ fronts-client/src/util/abTests.ts
@@ -1,4 +1,4 @@
-import { Card } from '../types/Collection';
+import { Card, VariantId, Test } from '../types/Collection';
 
 // TODO: add unit test
 export const hasAbTestOnCard = (card: Card | undefined) => {
@@ -15,4 +15,27 @@ export const hasAbTestOnCard = (card: Card | undefined) => {
 			test.expiryDate &&
 			test.expiryDate > Date.now(),
 	);
+};
+
+// TODO: add unit test
+export const findActiveOrDraftTest = (card: Card) => {
+	if (!card.tests) {
+		return undefined;
+	}
+
+	return card.tests.find(
+		(test) =>
+			!test.hasManuallyEndedOnThisTrail &&
+			((test.expiryDate && test.expiryDate > Date.now()) ||
+				typeof test.expiryDate === 'undefined'),
+	);
+};
+
+// we can extend this in future to 'getVariantField'
+export const getVariantHeadline = (
+	test: Test | undefined,
+	variantId: VariantId,
+) => {
+	return test?.variantMeta.find((variant) => variant.id === variantId)?.meta
+		.headline;
 };
