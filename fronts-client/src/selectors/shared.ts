edbe99c0d113ea8edd969ff23d2d2470644784e8
fweddi <freddiepreece@outlook.com>
2026-08-13 11:46:25 +0100

    Add clarifying comment

--- fronts-client/src/selectors/shared.ts
+++ fronts-client/src/selectors/shared.ts
@@ -157,7 +157,14 @@ const createSelectArticleFromCard = () =>
 				...omit(card, 'meta'),
 				...articleMeta,
 				headline: headlineField,
-				// TODO: this is currently broken because the test doesn't exist when you first hit the toggle
+				/**
+				 * Tests (and headline variant fields) are only persisted to the card
+				 * once the card is saved.
+				 *
+				 * So, even though headlineA is set on the form level when the AB test
+				 * switch is toggled on, it won't persist to the card level until the
+				 * card is saved. This is expected behaviour.
+				 **/
 				headlineA: activeOrDraftTest ? headlineFieldVariantA : undefined,
 				headlineB: activeOrDraftTest ? headlineFieldVariantB : undefined,
 				abTestEnabled: !!activeOrDraftTest,
