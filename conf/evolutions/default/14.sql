707c9a983998c8c377a2e142e685e7b9d73cb49a
Andy Gallagher <fredex42@gmail.com>
2026-09-08 07:26:17 +0000

    add database evolutions

---
+++ conf/evolutions/default/14.sql
@@ -0,0 +1,43 @@
+-- !Ups
+
+CREATE TABLE packages (
+    id            TEXT    PRIMARY KEY DEFAULT uuid_generate_v4()::TEXT,
+
+    name          TEXT    NOT NULL,
+    is_hidden     BOOLEAN NOT NULL,
+    web_metadata      JSONB,
+    feast_metadata JSONB,
+    prefill       TEXT,
+
+    created_on    TIMESTAMPTZ    DEFAULT now(),
+    created_by    TEXT,
+    created_email TEXT,
+
+    updated_on    TIMESTAMPTZ,
+    updated_by    TEXT,
+    updated_email TEXT
+);
+
+CREATE INDEX idxPackageName on packages(name);
+
+CREATE TABLE package_cards (
+    id            TEXT    PRIMARY KEY DEFAULT uuid_generate_v4()::TEXT,
+    package_id    TEXT   REFERENCES packages(id) ON DELETE CASCADE NOT NULL,
+
+    state          TEXT        NOT NULL, -- 'live' or 'draft'
+    page_code      TEXT        NOT NULL,
+    index          INT         NOT NULL,
+
+    metadata       JSONB,
+
+    added_on       TIMESTAMPTZ NOT NULL,
+    added_by       TEXT        NOT NULL,
+    added_email    TEXT        NOT NULL,
+);
+
+CREATE INDEX idxPackageCards ON package_cards (package_id);
+
+-- !Downs
+
+DROP TABLE package_cards;
+DROP TABLE packages ON CONFLICT CASCADE;
