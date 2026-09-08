17eb5c11f880b853aea6cde46d109ac9516d03f4
Andy Gallagher <fredex42@gmail.com>
2026-09-08 07:56:40 +0000

    Add Package/PackageCard database models

---
+++ app/model/packages/Package.scala
@@ -0,0 +1,61 @@
+package model.packages
+
+import org.postgresql.util.PGobject
+import play.api.libs.json.{JsValue, Json, OFormat}
+import scalikejdbc.WrappedResultSet
+
+/**
+  * A Package is a lot like a collection, in that it represents an ordered set of Cards.
+  * The difference to a Collection is that a Collection must belong to a Front; a Package is
+  * a top-level object that exists independently of any Front, Issue or Edition
+  *
+  */
+final case class Package(
+    id: String,
+    name: String,
+    isHidden: Boolean,
+    webMetadata: Option[JsValue],
+    feastMetadata: Option[JsValue],
+    prefill: Option[String],
+    createdOn: Option[Long],
+    createdBy: Option[String],
+    createdEmail: Option[String],
+    updatedOn: Option[Long],
+    updatedBy: Option[String],
+    updatedEmail: Option[String]
+) {
+  def webMetadataPG: Option[PGobject] = webMetadata.map(toPGobject)
+
+  def feastMetadataPG: Option[PGobject] = feastMetadata.map(toPGobject)
+
+  private def toPGobject(value: JsValue): PGobject = {
+    val pgObject = new PGobject()
+    pgObject.setType("jsonb")
+    pgObject.setValue(Json.stringify(value))
+    pgObject
+  }
+}
+
+object Package {
+  implicit val format: OFormat[Package] = Json.format[Package]
+
+  def fromRow(rs: WrappedResultSet): Package =
+    Package(
+      id = rs.string("id"),
+      name = rs.string("name"),
+      isHidden = rs.boolean("is_hidden"),
+      webMetadata = rs.stringOpt("web_metadata").map(Json.parse),
+      feastMetadata = rs.stringOpt("feast_metadata").map(Json.parse),
+      prefill = rs.stringOpt("prefill"),
+      createdOn = rs
+        .zonedDateTimeOpt("created_on")
+        .map(_.toInstant.toEpochMilli),
+      createdBy = rs.stringOpt("created_by"),
+      createdEmail = rs.stringOpt("created_email"),
+      updatedOn = rs
+        .zonedDateTimeOpt("updated_on")
+        .map(_.toInstant.toEpochMilli),
+      updatedBy = rs.stringOpt("updated_by"),
+      updatedEmail = rs.stringOpt("updated_email")
+    )
+}
---
+++ app/model/packages/PackageCard.scala
@@ -0,0 +1,43 @@
+package model.packages
+
+import org.postgresql.util.PGobject
+import play.api.libs.json.{JsValue, Json, OFormat}
+import scalikejdbc.WrappedResultSet
+
+final case class PackageCard(
+		id: String,
+		packageId: String,
+		state: String,
+		pageCode: String,
+		index: Int,
+		metadata: Option[JsValue],
+		addedOn: Long,
+		addedBy: String,
+		addedEmail: String
+) {
+	def metadataPG: Option[PGobject] = metadata.map(toPGobject)
+
+	private def toPGobject(value: JsValue): PGobject = {
+		val pgObject = new PGobject()
+		pgObject.setType("jsonb")
+		pgObject.setValue(Json.stringify(value))
+		pgObject
+	}
+}
+
+object PackageCard {
+	implicit val format: OFormat[PackageCard] = Json.format[PackageCard]
+
+	def fromRow(rs: WrappedResultSet): PackageCard =
+		PackageCard(
+			id = rs.string("id"),
+			packageId = rs.string("package_id"),
+			state = rs.string("state"),
+			pageCode = rs.string("page_code"),
+			index = rs.int("index"),
+			metadata = rs.stringOpt("metadata").map(Json.parse),
+			addedOn = rs.zonedDateTime("added_on").toInstant.toEpochMilli,
+			addedBy = rs.string("added_by"),
+			addedEmail = rs.string("added_email")
+		)
+}
